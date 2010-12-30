c56e16488deeb5381ad34893ae95971db083dab3
Mikael NAVARRO <klnavarro@gmail.com>
2010-12-30 12:27:34 +0100

    Calc nb_resolutions = nb resolved + nb_delivered + nb_closed (vs/ previous period/month).

--- reports/period_stats_report.class.php
+++ reports/period_stats_report.class.php
@@ -25,7 +25,7 @@ class PeriodStatsReport {
   // Compute monthly reports for the complete year
   public function computeReport() {
   	
-  	global $periodStatsExcludedProjectList;
+    global $periodStatsExcludedProjectList;
   	
     $now = time();
 
@@ -33,7 +33,7 @@ class PeriodStatsReport {
       $startTimestamp = mktime(0, 0, 1, $start_month, $this->start_day, $this->start_year);
       $endTimestamp   = mktime(0, 0, 1, ($start_month + 1), $this->start_day, $this->start_year);
 
-      if ($startTimestamp > $now) { break;}
+      if ($startTimestamp > $now) { break; }
 
       $periodStats = new PeriodStats($startTimestamp, $endTimestamp);
       
@@ -45,14 +45,13 @@ class PeriodStatsReport {
       $result = mysql_query($query) or die("Query failed: $query");
       while($row = mysql_fetch_object($result)) {
       	
-         // remove FDL project
+        // remove FDL project
       	if (! in_array($row->project_id, $periodStatsExcludedProjectList))  {
       		$projectList[] = $row->project_id; 
       	}
       }
       
       $periodStats->projectList = $projectList;
-      
       
       $periodStats->computeStats();
 
@@ -71,11 +70,15 @@ class PeriodStatsReport {
     global $status_delivered;
     global $status_closed;
   	
+    $nb_resolved = 0;
+    $nb_delivered = 0;
+    $nb_closed = 0;
+
     echo "<table>\n";
     echo "<caption title='Bilan mensuel SAUF SuiviOp.'>Bilan mensuel (nbre de fiches / status &agrave; la fin du mois)</caption>";
     echo "<tr>\n";
     echo "<th>Date</th>\n";
-    echo "<th title='Nbre de fiches cr&eacute;&eacute;es SAUF (SuiviOp, FDL)'>Nb soumissions</th>\n";
+    echo "<th title='Nbre de fiches cr&eacute;&eacute;es SAUF SuiviOp, FDL'>Nb submissions</th>\n";
     echo "<th>New</th>\n";
     echo "<th>Acknowledge</th>\n";
     echo "<th>Feedback</th>\n";
@@ -85,23 +88,38 @@ class PeriodStatsReport {
     echo "<th>Resolved</th>\n";
     echo "<th>Delivered</th>\n";
     echo "<th>Closed</th>\n";
+    echo "<th title='Nbre de fiches r&eacute;solues SAUF SuiviOp'>Nb resolutions</th>\n";
     echo "</tr>\n";
+
     foreach ($this->periodStatsList as $date => $ps) {
+      // Calc diff vs/ previous period
+      $nb_resolved = $ps->statusCountList[$status_resolved] - $nb_resolved;
+      $nb_delivered = $ps->statusCountList[$status_delivered] - $nb_delivered;
+      $nb_closed = $ps->statusCountList[$status_closed] - $nb_closed;
+      // Nb resolutions
+      $nb_resolutions = $nb_resolved + $nb_delivered + $nb_closed;
+
+      // Disp
+      $tableLine = "<tr>\n";
+      $tableLine .= "<td class=\"right\">".date("F Y", $date)."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList["submitted"]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_new]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_ack]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_feedback]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_analyzed]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_accepted]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_openned]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_resolved]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_delivered]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$ps->statusCountList[$status_closed]."</td>\n";
+      $tableLine .= "<td class=\"right\">".$nb_resolutions."</td>\n";
+      $tableLine .= "</tr>\n";
+      echo "$tableLine";
       
-	    $tableLine = "<tr>\n";
-	    $tableLine .= "<td>".date("F Y", $ps->startTimestamp)."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList["submitted"]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_new]."</td>\n"; // TODO new
-	    $tableLine .= "<td>".$ps->statusCountList[$status_ack]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_feedback]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_analyzed]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_accepted]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_openned]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_resolved]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_delivered]."</td>\n";
-	    $tableLine .= "<td>".$ps->statusCountList[$status_closed]."</td>\n";
-	    $tableLine .= "</tr>\n";
-	    echo "$tableLine";
+      // Reinit for next period
+      $nb_resolved = $ps->statusCountList[$status_resolved];
+      $nb_delivered = $ps->statusCountList[$status_delivered];
+      $nb_closed = $ps->statusCountList[$status_closed];
     }
     echo "</table>\n";
   }
