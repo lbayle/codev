f71bff1602d996debe415034c2ec3470abe16b99
Louis BAYLE <lbayle.work@gmail.com>
2011-01-21 14:02:47 +0100

    add check warnings to home mage

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
 <?php include 'menu.inc.php'; ?>
 
 
-<div id="homepage_list"  class="left">
 
-<br/>
-<br/>
-<br/>
 
 <?php
+
+include_once 'consistency_check.class.php'; 
+include_once 'user.class.php';
+
+
 // -----------------------------
 function displayLoginForm() {
         
@@ -33,65 +34,107 @@ function displayLoginForm() {
   echo("</form>\n");
   echo "</div>\n";      
 }
-  
+
+function displayLinks() {
+
+   echo "<div id='homepage_list'  class='left'>\n";
+	echo "<br/>\n";
+   echo "<br/>\n";
+   echo "	<ul>\n";
+   echo "   <li>\n";
+   echo "        <a href='http://".$_SERVER['HTTP_HOST']."/mantis.php'>Mantis</a>";
+   echo "   </li>\n";
+   echo "   <br/>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/timetracking/time_tracking.php'>Saisie des CRA</a>";
+   echo "   </li>\n";
+   echo "   <br/>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/timetracking/holidays_report.php'>Affichage des cong&eacute;s</a>";
+   echo "   </li>\n";
+   echo "</ul>\n";
+ 
+   echo "<br/>\n";
+   echo "<br/>\n";
+   echo "<br/>\n";
+
+   echo "<ul>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/reports'>Suivi des fiches Mantis</a>";
+   echo "   </li>\n";
+   echo "   <br/>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/timetracking/week_activity_report.php'>Activit&eacute; hebdomadaire</a>";
+   echo "   </li>\n";
+   echo "<br/>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/timetracking/productivity_report.php'>Indicateurs de production</a>";
+   echo "   </li>\n";
+   echo "<br/>\n";
+   echo "   <li>\n";
+   echo "        <a href='".getServerRootURL()."/reports/issue_info.php'>Informations sur une fiche</a>";
+   echo "   </li>\n";
+   echo "</ul>\n";
+   echo "</div>\n";
+   
+}
+
+// -----------------------------
+function displayConsistencyErrors($sessionUser) {
+
+   // get projects i'm involved in (dev, Leader, Manager)
+   $devTeamList = $sessionUser->getDevTeamList();
+   $leadedTeamList = $sessionUser->getLeadedTeamList();
+   $managedTeamList = $sessionUser->getManagedTeamList();
+   $teamList = $devTeamList + $leadedTeamList + $managedTeamList; 
+   $projectList = $sessionUser->getProjectList($teamList);
+
+   $ccheck = new ConsistencyCheck($projectList);
+
+   $cerrList = $ccheck->check();
+
+   if (0 == count($cerrList)) {
+      #echo "Pas d'erreur.<br/>\n";
+   } else {
+
+      echo "<br/>\n";
+      echo "<hr/>\n";
+      echo "<br/>\n";
+      echo "<br/>\n";
+   	echo "<div align='left'>\n";
+      foreach ($cerrList as $cerr) {
+
+         if ($sessionUser->id == $cerr->userId) {
+            $user = new User($cerr->userId);
+            echo "ERREUR Mantis: <span style='color:red'>".mantisIssueURL($cerr->bugId)."&nbsp;&nbsp;".date("Y-m-d", $cerr->timestamp)."&nbsp;&nbsp;".$statusNames[$cerr->status]."&nbsp;&nbsp;$cerr->desc</span><br/>\n";
+         }
+      }
+      echo "</div>\n";
+   }
+   
+}
+
+// ================ MAIN =================
+
+
 if (!isset($_SESSION['userid'])) {
    displayLoginForm();
 } else {
-	
+
+   $link = mysql_connect($db_mantis_host, $db_mantis_user, $db_mantis_pass) 
+      or die("Impossible de se connecter");
+   mysql_select_db($db_mantis_database) or die("Could not select database");
+
+   $userid = $_SESSION['userid'];
+   $sessionUser = new User($userid);
+
+   displayLinks();
+   
+   displayConsistencyErrors($sessionUser);
 }
+
 ?>
 
-
-<br/>
-<br/>
-<ul>
-   <li>
-      <?php echo "<a href='http://".$_SERVER['HTTP_HOST']."/mantis.php'>Mantis</a>";?>
-   </li>
-   <br/>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/timetracking/time_tracking.php'>Saisie des CRA</a>";?>
-   </li>
-   <br/>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/timetracking/holidays_report.php'>Affichage des cong&eacute;s</a>";?>
-   </li>
-</ul>
- 
-<br/>
-<br/>
-<br/>
-
-<ul>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/reports'>Suivi des fiches Mantis</a>";?>
-   </li>
-   <br/>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/timetracking/week_activity_report.php'>Activit&eacute; hebdomadaire</a>";?>
-   </li>
-   <br/>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/timetracking/productivity_report.php'>Indicateurs de production</a>";?>
-   </li>
-   <br/>
-   <li>
-      <?php echo "<a href='".getServerRootURL()."/reports/issue_info.php'>Informations sur une fiche</a>";?>
-   </li>
-</ul>
-
-
-
-
-</div>
-
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
 <br/>
 <br/>
 
