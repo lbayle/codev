02114457548edba926776dee5a98a0f676c3f90d
Louis BAYLE <lbayle.work@gmail.com>
2011-01-25 10:03:45 +0100

    GETTEXT update.sh

--- i18n/update.sh
+++ i18n/update.sh
@@ -5,26 +5,163 @@ PRJ_NAME=codev
 #SRC_FILE="index.php"
 SRC_FILE="index.php login.php"
 
-TEMPLATE_FILE=codev.po.template
 
-echo "  - parse $SRC_FILE and create template: $TEMPLATE_FILE"
-#xgettext -kT_ngettext:1,2 -kT_gettext -kT_ -k_ -L PHP -o $TEMPLATE_FILE $SRC_FILE
-xgettext -kT_ngettext:1,2 -kT_gettext -kT_ -k_ -o $TEMPLATE_FILE $SRC_FILE
 
-if [ "x$1" = "x--new" ]; then
-    echo "  - compile $TEMPLATE_FILE to ${PRJ_NAME}.mo"
-    msgfmt --statistics -o ${PRJ_NAME}.mo $TEMPLATE_FILE
-else
-    if [ -f $PRJ_NAME.po ]; then
-    
-       echo "  - merge $TEMPLATE_FILE with ${PRJ_NAME}.po"
-	   msgmerge -o .${PRJ_NAME}.po.tmp ${PRJ_NAME}.po $TEMPLATE_FILE
-	   mv .${PRJ_NAME}.po.tmp $PRJ_NAME.po
-    
-       echo "  - compile to ${PRJ_NAME}.mo"
-	   msgfmt --statistics -o ${PRJ_NAME}.mo ${PRJ_NAME}.po
-    else
-	   echo "Usage: $0 --new           Create new file (WARN: delete previous translations)"
-	   echo "Usage: $0 <basename>"
-    fi
+LOCALE="fr"
+
+# --------------
+# check input params
+CheckArgs ()
+{
+   echo " "
+
+   # init variables
+   doNew="No"
+   doCompile="No"
+   doTemplate="Yes"
+
+   while [ "x" != "x$*" ]
+   do
+      case $1 in
+
+         --new | -n )
+            doNew="Yes"
+            ;;
+
+         --locale | -l )
+	    shift
+            LOCALE="$1"
+            ;;
+
+         --compile | -c )
+	    shift
+            doCompile="Yes"
+            ;;
+
+         --template | -t )
+	    shift
+            doTemplate="Yes"
+            ;;
+
+         --help | -h )
+            DisplayHelp
+            exit 0
+            ;;
+
+         * )
+            echo "WARNING: Unknown arg '$1' (skipped)"
+            
+	    ;;
+      esac
+      shift
+   done
+}
+
+DisplayHelp()
+{
+   echo "v0.1.0"
+   echo "syntax  :  $0 [options...]"
+   echo ""
+   echo "options:"
+   echo "  --template         Generate .po template file"
+   echo "  --compile          Compile .po template file to .mo"
+   echo "  --locale <locale>  Select Locale to update (fr, de, en, ...)"
+   echo " <basename>"
+   echo " "
+}
+
+
+
+# generate $SRC_FILES
+f_genFileList ()
+{
+  local dirList=$1
+  local FILE_LIST=.fileList.txt
+
+  echo "" > $FILE_LIST # clear
+
+  #for i in $dirList
+  for i in admin classes reports timetracking tools
+  do
+    find "$i" -iname "*.php" -print0 | while IFS= read -rd $'\0' f
+    do
+      #echo "$f"
+      echo "$f" >> $FILE_LIST
+    done
+  done
+
+  # ---
+  #TODO add php files from root dir
+
+
+  # ---
+  while read filename
+  do
+    SRC_FILES="$SRC_FILES $filename"
+  done < $FILE_LIST
+  
+  # ---
+  echo "  - $(cat $FILE_LIST | wc -l ) .php files found"
+
+  rm $FILE_LIST
+}
+
+
+
+# usage: f_createTemplateFile <locale>
+f_createTemplateFile ()
+{
+  local tmpPoFile="/tmp/${PRJ_NAME}.${LOCALE}.po.tmp"
+  local TEMPLATE_FILE=${PRJ_NAME}.po.template
+
+  echo "  - parse php files and create template"
+  xgettext -kT_ngettext:1,2 -kT_gettext -kT_ -k_ -L PHP -o $TEMPLATE_FILE $SRC_FILE
+  #xgettext -kT_ngettext:1,2 -kT_gettext -kT_ -k_ -o $TEMPLATE_FILE $SRC_FILES
+
+  if [ -f ${FILE_PO} ]
+  then
+      echo "  - merge with existing ${FILE_PO}"
+      msgmerge -o ${tmpPoFile} ${FILE_PO} $TEMPLATE_FILE
+      mv ${tmpPoFile} ${FILE_PO}
+  else
+      mv ${TEMPLATE_FILE} ${FILE_PO}
+  fi
+}
+
+f_compileTemplateFile ()
+{
+   echo "  - compile to ${FILE_MO}"
+   msgfmt --statistics -o ${FILE_MO} ${FILE_PO}
+}
+
+# ###############################
+# MAIN
+# ###############################
+
+# get the name of the HOST if exists in command line argument
+CheckArgs $@
+
+#DIR_LIST="admin classes reports timetracking tools"
+
+DIR_LOCALE="./i18n/locale/${LOCALE}/LC_MESSAGES"
+
+FILE_PO="${DIR_LOCALE}/${PRJ_NAME}.po"
+FILE_MO="${DIR_LOCALE}/${PRJ_NAME}.mo"
+
+  f_genFileList $DIR_LIST
+
+
+if [ "Yes" == "$doTemplate" ]
+then
+  f_createTemplateFile
+  echo "READY."
+  echo "  - Now edit $FILE_PO and run script again with option --compile"
 fi
+
+if [ "Yes" == "$doCompile" ]
+then
+  f_compileTemplateFile
+  echo "DONE."
+  echo "  - Locale file generated: $FILE_MO"
+fi
+
