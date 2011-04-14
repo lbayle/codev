f135aedb0cdab9208c723e1bc10431717351d6b5
Louis BAYLE <lbayle.work@gmail.com>
2011-04-14 16:07:00 +0200

    Remove <Mantis Reports> from main menu + i18n

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -6,42 +6,41 @@
 <div id="menu">
 
 <?php 
-echo "
-<table class='menu'>
-   <tr>
-      <td class='menu'><a href='".getServerRootURL()."/index.php'>".T_("Home")."</a></td>
+echo "<table class='menu'>\n";
+echo "   <tr>\n";
+echo "      <td class='menu'><a href='".getServerRootURL()."/index.php'>".T_("Home")."</a></td>\n";
 
-      <td><a href='http://".$_SERVER['HTTP_HOST']."/mantis' title='MantisBT'>Mantis</a></td>
+echo "      <td><a href='http://".$_SERVER['HTTP_HOST']."/mantis' title='MantisBT'>Mantis</a></td>\n";
 
-      <td>
-      <a href='".getServerRootURL()."/timetracking/time_tracking.php' title=''>".T_("Time Tracking")."</a>
-      |
-      <a href='".getServerRootURL()."/timetracking/holidays_report.php' title='".T_("Holidays Reports")."'>".T_("Holidays")."</a>
-      |
-      <a href='".getServerRootURL()."/tools/check.php' title='".T_("Consistency Check")."'>".T_("Check")."</a>
-      </td>
-      <td>
-      <a href='".getServerRootURL()."/reports/issue_info.php' title='".T_("Task Information")."'>".T_("Task info")."</a>
-      |
-      <a href='".getServerRootURL()."/timetracking/week_activity_report.php' title='".T_("Weekly activities")."'>".T_("Weekly activities")."</a>
-      |
-      <a href='".getServerRootURL()."/reports/productivity_report.php' title='".T_("Statistics")."'>".T_("Productivity Reports")."</a>
-      |
-      <a href='".getServerRootURL()."/reports/statistics.php' title=''>".T_("Statistics")."</a>
-      |
-      <a href='".getServerRootURL()."/reports/mantis_reports.php' title=''>".T_("Mantis Reports")."</a>
-      |
-      <a href='".getServerRootURL()."/reports/export_csv_weekly.php' title='".T_("Project Management Reports")."'>".T_("Export to CSV")."</a>
-      |
-      <a href='".getServerRootURL()."/reports/planning_report.php' title='".T_("Gant")."'>".T_("Planning")."</a>
-      </td>
-      <td>
-      <a href='".getServerRootURL()."/doc/index.php' title='".T_("Documentation")."'>Doc</a>
-      |
-      <a href='".getServerRootURL()."/admin/index.php' title='".T_("CoDev Administration")."'>Admin</a>
-      </td>
-   </tr>
-</table>";
+echo "      <td>\n";
+echo "      <a href='".getServerRootURL()."/timetracking/time_tracking.php' title=''>".T_("Time Tracking")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/timetracking/holidays_report.php' title='".T_("Holidays Reports")."'>".T_("Holidays")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/tools/check.php' title='".T_("Consistency Check")."'>".T_("Check")."</a>\n";
+echo "      </td>\n";
+echo "      <td>\n";
+echo "      <a href='".getServerRootURL()."/reports/issue_info.php' title='".T_("Task Information")."'>".T_("Task info")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/timetracking/week_activity_report.php' title='".T_("Team Weekly activities")."'>".T_("Weekly activities")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/reports/productivity_report.php' title='".T_("Productivity Reports")."'>".T_("Productivity")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/reports/statistics.php' title=''>".T_("Statistics")."</a>\n";
+#echo "      |\n";
+#echo "      <a href='".getServerRootURL()."/reports/mantis_reports.php' title=''>".T_("Mantis Reports")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/reports/export_csv_weekly.php' title='".T_("Generate Excel Sheet")."'>".T_("Export to CSV")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/reports/planning_report.php' title='".T_("Check DeadLines (~Gant diagram)")."'>".T_("Planning")."</a>\n";
+echo "      </td>\n";
+echo "      <td>\n";
+echo "      <a href='".getServerRootURL()."/doc/index.php' title='".T_("Documentation")."'>Doc</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/admin/index.php' title='".T_("CoDev Administration")."'>Admin</a>\n";
+echo "      </td>\n";
+echo "  </tr>\n";
+echo "</table>";
 ?>
 <br/>
 <br/>
