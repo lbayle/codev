f249d78f0ea924ba8b5fc8663edb85f32fcc786e
Louis BAYLE <lbayle.work@gmail.com>
2011-04-22 09:11:08 +0200

    code cleanup

--- timetracking/time_tracking_tools.php
+++ timetracking/time_tracking_tools.php
@@ -86,7 +86,7 @@ function displayTimetrackingTuples($userid, $startTimestamp=NULL, $endTimestamp=
       echo "<tr>\n";
       //echo "<td width=40>\n";
       echo "<td>\n";
-      echo "<a title='".T_("delete this row")."' href=\"javascript: deleteTrack('".$row->id."', '".$trackDescription."', '".$row->bugid."')\" ><img border='0' src='b_drop.png'></a>\n";
+      echo "<a title='".T_("delete this row")."' href=\"javascript: deleteTrack('".$row->id."', '".$trackDescription."', '".$row->bugid."')\" ><img border='0' src='../images/b_drop.png'></a>\n";
       echo "</td>\n";
       echo "<td width=170>".$cosmeticDate."</td>\n";
       echo "<td>".mantisIssueURL($row->bugid)."</td>\n";
