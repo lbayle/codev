c4fcb8c7307b9eb31318656a089d274d4de72260
Louis BAYLE <lbayle.trash@gmail.com>
2011-06-08 02:40:03 +0200

    cosmetic

---
+++ include/mysql_config.inc.sample.php
@@ -0,0 +1,9 @@
+<?php
+   // This file is part of CoDev-Timetracking.
+
+   // Mantis DB infomation.
+   $db_mantis_host      =  'localhost';
+   $db_mantis_user      =  'codev';
+   $db_mantis_pass      =  '';
+   $db_mantis_database  =  'bugtracker';
+?>
