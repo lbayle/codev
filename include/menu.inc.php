543b41b914d2c346c02fed972e2dbd32b817c3d3
Louis BAYLE <lbayle.work@gmail.com>
2011-07-05 12:56:03 +0200

    cosmetic: reorganize main menu items

--- include/menu.inc.php
+++ include/menu.inc.php
@@ -33,11 +33,11 @@ echo "      <a href='".getServerRootURL()."/timetracking/time_tracking.php' titl
 echo "      |\n";
 echo "      <a href='".getServerRootURL()."/timetracking/holidays_report.php' title='".T_("Holidays Reports")."'>".T_("Holidays")."</a>\n";
 echo "      |\n";
-echo "      <a href='".getServerRootURL()."/tools/check.php' title='".T_("Consistency Check")."'>".T_("Check")."</a>\n";
+echo "      <a href='".getServerRootURL()."/reports/planning_report.php' title='".T_("Check DeadLines (~Gant diagram)")."'>".T_("Planning")."</a>\n";
+echo "      |\n";
+echo "      <a href='".getServerRootURL()."/reports/issue_info.php' title='".T_("Task Information")."'>".T_("Task info")."</a>\n";
 echo "      </td>\n";
 echo "      <td>\n";
-echo "      <a href='".getServerRootURL()."/reports/issue_info.php' title='".T_("Task Information")."'>".T_("Task info")."</a>\n";
-echo "      |\n";
 echo "      <a href='".getServerRootURL()."/timetracking/team_activity_report.php' title='".T_("Team Weekly activities")."'>".T_("Weekly activities")."</a>\n";
 echo "      |\n";
 echo "      <a href='".getServerRootURL()."/reports/productivity_report.php' title='".T_("Productivity Reports")."'>".T_("Productivity")."</a>\n";
@@ -48,7 +48,7 @@ echo "      <a href='".getServerRootURL()."/reports/statistics.php' title=''>".T
 echo "      |\n";
 echo "      <a href='".getServerRootURL()."/reports/export_csv_weekly.php' title='".T_("Generate Excel Sheet")."'>".T_("Export to CSV")."</a>\n";
 echo "      |\n";
-echo "      <a href='".getServerRootURL()."/reports/planning_report.php' title='".T_("Check DeadLines (~Gant diagram)")."'>".T_("Planning")."</a>\n";
+echo "      <a href='".getServerRootURL()."/tools/check.php' title='".T_("Consistency Check")."'>".T_("Check")."</a>\n";
 echo "      </td>\n";
 echo "      <td>\n";
 echo "      <a href='".getServerRootURL()."/doc/index.php' title='".T_("Documentation")."'>Doc</a>\n";
