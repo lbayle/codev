acc27410fab84bf557b0e7b75553879737e49085
Louis BAYLE <lbayle.trash@gmail.com>
2011-12-12 01:14:17 +0100

    FIX: deleteConfigFiles

--- install/install.class.php
+++ install/install.class.php
@@ -125,19 +125,19 @@ class Install {
    public static function deleteConfigFiles() {
    
 	   if (file_exists(Install::FILENAME_CONSTANTS)) {
-	   	$retCode = unlink( FILENAME_CONSTANTS );
+	   	$retCode = unlink( Install::FILENAME_CONSTANTS );
 	   	if (true == $retCode) {
-	   		echo "DEBUG: ". Install::FILENAME_CONSTANTS . " successfully deleted.</br>";
+	   		#echo "DEBUG: ". Install::FILENAME_CONSTANTS . " successfully deleted.</br>";
 	     	} else {
 	   		echo "ERROR: Could not delete file: ". Install::FILENAME_CONSTANTS . "</br>";
 	   	}
 	   }
 	   if (file_exists(Install::FILENAME_MYSQL_CONFIG)) {
-	   	$retCode = unlink( FILENAME_MYSQL_CONFIG );
+	   	$retCode = unlink( Install::FILENAME_MYSQL_CONFIG );
 	   	if (true == $retCode) {
-	   		echo "DEBUG: ". Install::FILENAME_CONSTANTS . " successfully deleted.</br>";
+	   		#echo "DEBUG: ". Install::FILENAME_MYSQL_CONFIG . " successfully deleted.</br>";
 	   	} else {
-	      		echo "ERROR: Could not delete file: ". Install::FILENAME_CONSTANTS . "</br>";
+	      		echo "ERROR: Could not delete file: ". Install::FILENAME_MYSQL_CONFIG . "</br>";
 	      }
 	   }
    }   
