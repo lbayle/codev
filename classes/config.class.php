16e24d5f7f0753e07fc0a421006caa3acfefe331
Louis BAYLE <lbayle.trash@gmail.com>
2011-12-22 02:35:04 +0100

    FIX gantt broken because of log4php config

--- classes/config.class.php
+++ classes/config.class.php
@@ -18,6 +18,8 @@
 
 // ==============================================================
 
+require_once('Logger.php');
+Logger::configure($codevRootDir.DIRECTORY_SEPARATOR.'log4php.xml');
 
 /**
  * Constants Singleton class
@@ -103,6 +105,8 @@ class Config {
    // --------------------------------------
    private function __construct()
    {
+
+      echo "DEBUG config ".__CLASS__."</br>";
       $this->logger = Logger::getLogger(__CLASS__);
 
       self::$configVariables = array();
