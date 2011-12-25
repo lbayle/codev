c54fb351e3359b05e316c77c2c72dc37af5a9fd9
Louis BAYLE <lbayle.trash@gmail.com>
2011-12-25 21:03:49 +0100

    jquery dialog test

--- lib/jquery/jqtest.php
+++ lib/jquery/jqtest.php
@@ -1,20 +1,72 @@
 <html>
 <head>
-<script type="text/javascript" src="jquery.js"></script>
-<script type="text/javascript" src="custom_codevtt.js"></script>
+
+	<meta charset="utf-8">
+	<title>jQuery Test</title>
+
+   <style>
+   @import "./jquery.ui.base.css";
+   @import "./jquery.ui.theme.css";
+   </style>
+
+   <link type="text/css" href="./ui/css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="Stylesheet" />
+	<script src="./jquery.js"></script>
+   <script src="./ui/development-bundle/external/jquery.bgiframe-2.1.2.js"></script>
+	<script src="./jquery-ui.js"></script>
+
+
 
 <script type="text/javascript">
-// we will add our javascript code here
+
+	$(function() {
+		$( "#dialog" ).dialog({
+			autoOpen: false
+		});
+
+		$( "#opener" ).click(function() {
+			$( "#dialog" ).dialog( "open" );
+			return false;
+		});
+
+		$( "#dialog2" ).dialog({
+			autoOpen: false
+		});
+
+		$( "#opener2" ).click(function() {
+			$( "#dialog2" ).dialog( "open" );
+			return false;
+		});
+
+
+	});
+
+
+
+
+
 </script>
 
 </head>
 <body>
 
-<?php
 
-   echo "This is a jQuery popup <a id='first' href='#'><img title='help' src='../../images/help_icon.gif' /></a>";
-    
-?>
+<div class="demo">
+
+<div id="dialog" title="Basic dialog">
+	<p>This is an animated dialog which is useful for displaying information.</p>
+</div>
+
+<div id="dialog2" title="dialog on image">
+	<p>Here we go !</p>
+</div>
+
+<button id="opener">Open Dialog</button>
+</br>
+</br>
+click there: <a id='opener2' href='#'><img title='help' src='../../images/help_icon.gif' /></a>
+
+</div><!-- End demo -->
+
 
 </body>
 </html>
