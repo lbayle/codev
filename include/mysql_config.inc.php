c2665df8032e148de4c2fa8aff8d32f59edd572b
Louis BAYLE <lbayle.work@gmail.com>
2012-01-18 10:15:25 +0100

    include constants.php

---
+++ include/mysql_config.inc.php
@@ -0,0 +1,7 @@
+<?php
+   // Mantis DB infomation.
+   $db_mantis_host      =  'localhost';
+   $db_mantis_user      =  'root';
+   $db_mantis_pass      =  'secret';
+   $db_mantis_database  =  'mantis6f';
+?>
