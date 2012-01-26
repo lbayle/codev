32ca10710f0ab579c1a75fe70370d99962bc41b6
lob <lob@pavilion.(none)>
2012-01-26 23:24:36 +0100

    homepage: showIssuesInDrift

--- index.php
+++ index.php
@@ -26,6 +26,107 @@
    $logger = Logger::getLogger("homepage");
 ?>
 
+<style>
+   fieldset { padding:0; border:0; }
+   validateTips { border: 1px solid transparent; padding: 0.3em; }
+</style>
+
+<script  language="JavaScript">
+
+
+  function updateRemaining(dialogBoxTitle, bugid, remaining, description, userid, nextForm ){
+
+     $( "#desc_summary" ).text(description);
+  
+     $( "#formUpdateRemaining * #remaining" ).val(remaining);
+	 
+     $( "#formUpdateRemaining input[name=bugid]" ).val(bugid);
+     $( "#formUpdateRemaining input[name=userid]" ).val(userid);
+     $( "#formUpdateRemaining input[name=nextForm]" ).val(nextForm);
+
+     $( "#update_remaining_dialog_form" ).dialog('option', 'title', dialogBoxTitle);
+     $( "#update_remaining_dialog_form" ).dialog( "open" );
+
+  }
+   
+   // ------ JQUERY ------
+	$(function() {
+
+		var  remaining = $( "#remaining" ),
+			 allFields = $( [] ).add( remaining ),
+			 tips = $( "#validateTips" );
+
+		function updateTips( t ) {
+			tips
+				.text( t )
+				.addClass( "ui-state-highlight" );
+			setTimeout(function() {
+				tips.removeClass( "ui-state-highlight", 1500 );
+			}, 500 );
+		}
+
+		function checkRegexp( o, regexp, n ) {
+			if ( !( regexp.test( o.val() ) ) ) {
+				o.addClass( "ui-state-error" );
+				updateTips( n );
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		$( "#update_remaining_dialog_form" ).dialog({
+			autoOpen: false,
+			height: 250,
+			width: 500,
+			modal: true,
+			open: function() { 
+               // Select input field contents 
+               $( "#remaining" ).select(); 
+			},
+			buttons: {
+				"Update": function() {
+					var bValid = true;
+					allFields.removeClass( "ui-state-error" );
+					bValid = bValid && checkRegexp( remaining, /^[0-9]+(\.[0-9]5?)?$/i, "format:  '1',  '0.3'  or  '2.55'" );
+
+					if ( bValid ) {
+						// TODO use AJAX to call php func and update remaining on bugid
+						$('#formUpdateRemaining').submit();
+					}
+				},
+				Cancel: function() {
+					$( this ).dialog( "close" );
+				}
+			},
+			close: function() {
+				allFields.val( "" ).removeClass( "ui-state-error" );
+			}
+		});
+		
+	});
+</script>
+
+
+<div id="update_remaining_dialog_form" title="Task XXX - Update Remaining" style='display: none'>
+	<p id='desc' name='desc'>
+	    <label id="desc_summary" name='desc_summary'>summary</label>
+	</p>
+	<p id="validateTips"> </p>
+	<form id='formUpdateRemaining' name='formUpdateRemaining' method='post' Action='index.php' >
+	   <fieldset>
+		   <label for="remaining">Remaining: </label>
+		   <input type='text'  id='remaining' name='remaining' size='3' class='text' value='noValue' />
+	   </fieldset>
+      <input type='hidden' name='bugid'    value='0' >
+      <input type='hidden' name='userid'   value='0' >
+      <input type='hidden' name='nextForm' value='unspecifiedForm' >
+      <input type='hidden' name='action'   value='updateRemainingAction' >
+	</form>
+</div>
+
+
+
 <?php
 include 'login.inc.php';
 include 'menu.inc.php';
@@ -69,17 +170,17 @@ function displayLinks() {
    global $mantisURL;
 
    echo "<div id='homepage_list'  class='left'>\n";
-	echo "<br/>\n";
-   echo "<br/>\n";
+   #echo "<br/>\n";
+   #echo "<br/>\n";
    echo "	<ul>\n";
    echo "   <li>\n";
    echo "        <a href='".$mantisURL."'>Mantis</a>";
    echo "   </li>\n";
-   echo "   <br/>\n";
+   #echo "   <br/>\n";
    echo "   <li>\n";
    echo "        <a href='".getServerRootURL()."/timetracking/time_tracking.php'>".T_("Time Tracking")."</a>"; // Saisie des CRA
    echo "   </li>\n";
-   echo "   <br/>\n";
+   #echo "   <br/>\n";
    echo "   <li>\n";
    echo "        <a href='".getServerRootURL()."/timetracking/holidays_report.php'>".T_("Holidays")."</a>"; // Affichage des cong&eacute;s
    echo "   </li>\n";
@@ -87,18 +188,18 @@ function displayLinks() {
 
    //echo "<br/>\n";
    //echo "<br/>\n";
-   echo "<br/>\n";
+   #echo "<br/>\n";
 
    echo "<ul>\n";
-   echo "   <br/>\n";
+   #echo "   <br/>\n";
    echo "   <li>\n";
    echo "        <a href='".getServerRootURL()."/reports/planning_report.php'>".T_("Planning")."</a>"; // Affichage du planning
    echo "   </li>\n";
-   echo "<br/>\n";
+   #echo "<br/>\n";
    echo "   <li>\n";
    echo "        <a href='".getServerRootURL()."/reports/issue_info.php'>".T_("Task information")."</a>"; // Info fiche
    echo "   </li>\n";
-   echo "<br/>\n";
+   #echo "<br/>\n";
    echo "   <li>\n";
    echo "        <a href='".getServerRootURL()."/timetracking/team_activity_report.php'>".T_("Weekly activity")."</a>"; // Activit&eacute; hebdomadaire
    echo "   </li>\n";
@@ -112,6 +213,82 @@ function displayLinks() {
    echo "</div>\n";
 
 }
+
+// -----------------------------
+/**
+ * 
+ */
+function showIssuesInDrift($userid) {
+
+    
+	$user = UserCache::getInstance()->getUser($userid);
+	$allIssueList = $user->getAssignedIssues();
+
+	foreach ($allIssueList as $issue) {
+       $driftPrelEE = $issue->getDriftETA();
+	   $driftEE = $issue->getDrift();
+
+	   if (($driftPrelEE >= 1) || ($driftEE >= 1)) {
+	       $issueList[] = $issue;
+	   }
+    }	
+	if (0 == count($issueList)) {
+	   return 0;
+	}
+   echo "<br/>\n";
+   echo "<br/>\n";
+   echo "<hr/>\n";
+   echo "<br/>\n";
+   echo "<br/>\n";
+
+    echo "<table>\n";
+    echo "<caption>".T_("Tasks in drift")."</caption>\n";
+    echo "<tr>\n";
+    echo "<th>".T_("ID")."</th>\n";
+    echo "<th>".T_("Project")."</th>\n";
+    #echo "<th title=''>".T_("PrelEE Drift")."</th>\n";
+	#echo "<th title=''>".T_("EE Drift")."</th>\n";
+    echo "<th title='Derive par rapport a l estimation preliminaire'>".T_("Derive PrelEE")."</th>\n";
+	echo "<th title='Derive par rapport au BI+BS'>".T_("Derive EE")."</th>\n";
+	echo "<th>".T_("RAF")."</th>\n";
+	echo "<th>".T_("Summary")."</th>\n";
+    echo "</tr>\n";
+	
+	foreach ($issueList as $issue) {
+	
+		// TODO: check if issue in team project list ?
+
+		$driftPrelEE = $issue->getDriftETA();
+		$driftEE = $issue->getDrift();
+		
+        $formatedSummary = str_replace("'", "\'", $issue->summary);
+        $formatedSummary = str_replace('"', "\'", $formatedSummary);
+		$formatedTitle = "Task ".$issue->bugId." / ".$issue->tcId." - Update Remaining";
+		
+	   echo "<tr>\n";
+	   echo "<td>".issueInfoURL($issue->bugId)."</td>\n";
+	   echo "<td>".$issue->getProjectName()."</td>\n";
+	   $color = "";
+	   if ($driftPrelEE <= -1) { $color = "style='background-color: #61ed66;'"; }
+	   if ($driftPrelEE >= 1) { $color = "style='background-color: #fcbdbd;'"; }
+	   echo "<td $color >".$driftPrelEE."</td>\n";
+	   $color = "";
+	   if ($driftEE <= -1) { $color = "style='background-color: #61ed66;'"; }
+	   if ($driftEE >= 1) { $color = "style='background-color: #fcbdbd;'"; }
+	   echo "<td $color >".$driftEE."</td>\n";
+	   
+	   echo "<td>\n";
+	   echo "<a title='".T_("update Remaining")."' href=\"javascript: updateRemaining('".$formatedTitle."', '".$issue->bugId."', '".$issue->getRemaining()."', '".$formatedSummary."', '', '')\" >".$issue->getRemaining()."</a>\n";
+	   echo "</td>\n";
+	   
+	   echo "<td>".$issue->summary."</td>\n";
+	   echo "</tr>\n";
+	}
+   echo "</table>\n";
+   
+   
+}
+
 
 // -----------------------------
 function displayConsistencyErrors($sessionUser) {
@@ -151,6 +328,10 @@ function displayConsistencyErrors($sessionUser) {
 
 // ================ MAIN =================
 
+$bugid     = isset($_POST['bugid']) ? $_POST['bugid'] : '';
+$remaining = isset($_POST['remaining']) ? $_POST['remaining'] : '';
+$action    = isset($_POST["action"]) ? $_POST["action"] : '';
+
 
 if (!isset($_SESSION['userid'])) {
    displayLoginForm();
@@ -159,9 +340,23 @@ if (!isset($_SESSION['userid'])) {
 	$userid = $_SESSION['userid'];
    $sessionUser = UserCache::getInstance()->getUser($userid);
 
+   // --- updateRemaining DialogBox
+    if ("updateRemainingAction" == $action) {
+	   if ("0" != "$bugid") {
+	      $issue = IssueCache::getInstance()->getIssue($bugid);
+	      $issue->setRemaining($remaining);
+	      #$action = "displayBug";
+	   }
+	}   
+
+   
    #disclaimer();
 
    displayLinks();
+
+   echo "<br/>\n";
+   
+   showIssuesInDrift($userid);
 
    echo "<br/>\n";
 
