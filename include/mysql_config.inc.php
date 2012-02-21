89bf27b6b884ad540a1b0167a7a097114eeb4da5
Louis BAYLE <lbayle.work@gmail.com>
2012-02-21 14:20:26 +0100

    remove constants.php

--- include/mysql_config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-   // Mantis DB infomation.
-   $db_mantis_host      =  'localhost';
-   $db_mantis_user      =  'root';
-   $db_mantis_pass      =  'Secret.0';
-   $db_mantis_database  =  'mantis6f';
-?>
