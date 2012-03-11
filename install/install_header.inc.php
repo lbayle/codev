2e798fc88fba0dfcdc270f2b4bdadfeff2b6b94a
Nicolas Velin <nicolas_job@velin.fr>
2012-03-11 16:18:54 +0100

    Wrong encoding header

--- install/install_header.inc.php
+++ install/install_header.inc.php
@@ -35,7 +35,7 @@
 
 <head>
 <title>CoDev TimeTracking</title>
-<meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <?php
    echo "<link rel='shortcut icon' href='".getServerRootURL()."/images/favicon.ico' />\n";
