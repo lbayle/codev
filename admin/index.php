ae7543dec20aee14625e0ba097c242a4038ef0e7
Louis BAYLE <lbayle.work@gmail.com>
2012-03-28 12:12:34 +0200

    FIX PHP session

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,7 @@ if (!isset($_SESSION)) {
 	$tokens = explode('/', $_SERVER['PHP_SELF'], 3);
 	$sname = str_replace('.', '_', $tokens[1]);
 	session_name($sname); 
+	session_start(); 
 	header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"'); 
 } 
 ?>
