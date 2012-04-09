79817c9b0b352f4ae172c05a3be0931425cf7bb9
Louis BAYLE <lbayle.trash@gmail.com>
2012-04-09 18:15:35 +0200

    check Team ConsistencyErrors

--- tools/check.php
+++ tools/check.php
@@ -36,83 +36,27 @@ include_once('team.class.php');
 $logger = Logger::getLogger("check");
 
 /**
- * Get consistency errors
- * @param int User's id
+ * Get teams
+ * @param $teamList
+ * @return array
  */
-function getConsistencyErrors($userid) {
-    $sessionUser = new User($userid);
-
-    global $logger;
-    global $statusNames;
-
-    $logger->debug("getConsistencyErrors userid=$userid");
-
-    // get projects i'm involved in (dev, Leader, Manager)
-    $devTeamList = $sessionUser->getDevTeamList();
-    $leadedTeamList = $sessionUser->getLeadedTeamList();
-    $managedTeamList = $sessionUser->getManagedTeamList();
-    $oTeamList = $sessionUser->getObservedTeamList();
-
-    $teamList = $devTeamList + $leadedTeamList + $managedTeamList + $oTeamList;
-    $projectList = $sessionUser->getProjectList($teamList);
-
-    #$ccheck = new ConsistencyCheck($projectList);
-
-    $formatedProjects = implode( ', ', array_keys($projectList));
-    $query = "SELECT id AS bug_id, status, handler_id, last_updated ".
-	    "FROM `mantis_bug_table` ".
-		 "WHERE project_id IN ($formatedProjects) ";
-
-    $result = mysql_query($query);
-    if (!$result) {
-	    $logger->error("Query FAILED: $query");
-	    $logger->error(mysql_error());
-	    echo "<span style='color:red'>ERROR: Query FAILED</span>";
-	    exit;
+function getTeams($teamList) {
+    foreach ($teamList as $tid => $tname) {
+        $teams[] = array(
+            'id' => $tid,
+            'name' => $tname,
+            'selected' => $tid == $_SESSION['teamid']
+        );
     }
-    $issueList = array();
-    while($row = mysql_fetch_object($result))
-    {
-	    $issue = IssueCache::getInstance()->getIssue($row->bug_id);
-	    $issueList[$row->bug_id] = $issue;
-    }
-
-    $ccheck = new ConsistencyCheck2($issueList);
-    $cerrList = $ccheck->check();
-
-    if (count($cerrList) > 0) {
-        global $count;
-        $count = count($cerrList);
-        foreach ($cerrList as $cerr) {
-            $user = UserCache::getInstance()->getUser($cerr->userId);
-            $issue = IssueCache::getInstance()->getIssue($cerr->bugId);
-
-            // do not display users that do not belong to a team
-            $userProjList = array_keys($user->getProjectList());
-
-            if ((in_array($issue->projectId, $userProjList)) ||
-                (0 == $cerr->userId)) {
-
-               $cerrs[] = array('userName' => $user->getName(),
-                     'mantisIssueURL' => mantisIssueURL($cerr->bugId, $issue->summary),
-                     'date' => date("Y-m-d", $cerr->timestamp),
-                     'status' => $statusNames[$cerr->status],
-                     'severity' => $cerr->severity,
-                     'project' => $issue->getProjectName(),
-                     'desc' => $cerr->desc);
-            }
-        }
-
-        return $cerrs;
-    }
+    return $teams;
 }
+
 
 /**
  * Get consistency errors
  * @param int User's id
  */
 function getTeamConsistencyErrors($teamid) {
-   $sessionUser = new User($userid);
 
    global $logger;
    global $statusNames;
@@ -121,21 +65,37 @@ function getTeamConsistencyErrors($teamid) {
 
    // get team projects
    $projectList = Team::getProjectList($teamid);
+   $memberList = Team::getMemberList($teamid);
 
-   $ccheck = new ConsistencyCheck($projectList);
-   $cerrList = $ccheck->check();
+
+    $formatedProjects = implode( ', ', array_keys($projectList));
+    $formatedMembers = implode( ', ', array_keys($memberList));
+    $query = "SELECT id AS bug_id, status, handler_id, last_updated ".
+       "FROM `mantis_bug_table` ".
+       "WHERE project_id IN ($formatedProjects) ".
+       "AND   handler_id IN ($formatedMembers) ";
+
+    $result = mysql_query($query);
+    if (!$result) {
+       $logger->error("Query FAILED: $query");
+       $logger->error(mysql_error());
+       echo "<span style='color:red'>ERROR: Query FAILED</span>";
+       exit;
+    }
+    $issueList = array();
+    while($row = mysql_fetch_object($result))
+    {
+       $issue = IssueCache::getInstance()->getIssue($row->bug_id);
+       $issueList[$row->bug_id] = $issue;
+    }
+
+    $ccheck = new ConsistencyCheck2($issueList);
+    $cerrList = $ccheck->check();
 
    if (count($cerrList) > 0) {
-      global $count;
-      $count = count($cerrList);
       foreach ($cerrList as $cerr) {
          $user = UserCache::getInstance()->getUser($cerr->userId);
          $issue = IssueCache::getInstance()->getIssue($cerr->bugId);
-
-         // check if issue is assigned to a user from the selected teams
-         $userTeamList = $user->getTeamList();
-         // TODO
-
 
          $cerrs[] = array('userName' => $user->getName(),
                'mantisIssueURL' => mantisIssueURL($cerr->bugId, $issue->summary),
@@ -145,9 +105,7 @@ function getTeamConsistencyErrors($teamid) {
                'project' => $issue->getProjectName(),
                'desc' => $cerr->desc);
       }
-
-      $sortedCerrList = qsort($cerrs);
-      return $sortedCerrList;
+      return $cerrs;
    }
 }
 
@@ -161,11 +119,46 @@ $smartyHelper->assign('pageName', T_('Consistency Check'));
 
 // Consistency errors
 if (isset($_SESSION['userid'])) {
-    $consistencyErrors = getConsistencyErrors($_SESSION['userid']);
-    $smartyHelper->assign('count', $count);
-    if(isset($consistencyErrors)) {
-        $smartyHelper->assign('consistencyErrors', $consistencyErrors);
+
+    // use the teamid set in the form, if not defined (first page call) use session teamid
+    if (isset($_POST['teamid'])) {
+        $teamid = $_POST['teamid'];
+    } else {
+        $teamid = isset($_SESSION['teamid']) ? $_SESSION['teamid'] : 0;
     }
+    $_SESSION['teamid'] = $teamid;
+
+    $session_user = UserCache::getInstance()->getUser($_SESSION['userid']);
+
+    $mTeamList = $session_user->getTeamList();
+    $lTeamList = $session_user->getLeadedTeamList();
+    $oTeamList = $session_user->getObservedTeamList();
+    $managedTeamList = $session_user->getManagedTeamList();
+    $teamList = $mTeamList + $lTeamList + $oTeamList + $managedTeamList;
+
+    if (count($teamList) > 0) {
+        $smartyHelper->assign('teams', getTeams($teamList));
+
+        if ("displayPage" == $_POST['action'] && 0 != $teamid) {
+
+	        $consistencyErrors = getTeamConsistencyErrors($teamid);
+
+	        $smartyHelper->assign('count', count($consistencyErrors));
+	        if(isset($consistencyErrors)) {
+		        $smartyHelper->assign('consistencyErrors', $consistencyErrors);
+	        }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 }
 
 $smartyHelper->displayTemplate($codevVersion, $_SESSION['username'], $_SESSION['realname'],$mantisURL);
