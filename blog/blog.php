ec2d93013f96aba5ed2ac97a68698bddeb1db1a1
Louis BAYLE <lbayle.trash@gmail.com>
2012-04-21 01:36:48 +0200

    BlogManager: display blogPosts

--- blog/blog.php
+++ blog/blog.php
@@ -28,6 +28,49 @@ include_once('blog_manager.class.php');
 require('display.inc.php');
 
 
+
+function getBlogPosts($postList) {
+
+   $blogPosts = array();
+
+   foreach ($postList as $id => $bpost) {
+
+   	$srcUser = UserCache::getInstance()->getUser($bpost->src_user_id);
+
+      $item = array();
+
+      // TODO
+      $item['category'] = Config::getVariableValueFromKey(Config::id_blogCategories, $bpost->category);
+      $item['severity'] = BlogPost::getSeverityName($bpost->severity);
+      $item['summary'] = $bpost->summary;
+      $item['content'] = $bpost->content;
+      $item['date_submitted'] = date('Y-m-d',$bpost->date_submitted);
+      $item['from']    = $srcUser->getName();
+
+      // find receiver
+      if (0 != $bpost->dest_user_id) {
+      	$destUser = UserCache::getInstance()->getUser($bpost->dest_user_id);
+         $item['to'] = $destUser->getName();
+      } else if (0 != $bpost->dest_team_id) {
+      	$team = new Team($bpost->dest_team_id);
+         $item['to'] = $team->name;
+      } else if (0 != $bpost->dest_project_id) {
+      	$destProj = ProjectCache::getInstance()->getProject($bpost->dest_project_id);
+      	$item['to'] = $destProj->name;
+      } else {
+      	$item['to'] = '?';
+      }
+
+      $item['activity'] = 'activities...';
+      $item['ack'] = "<input type='button' value='".T_('Ack')."' onclick='javascript: ackPost(".$bpost->id.")' />'";
+
+      $blogPosts[$id] = $item;
+   }
+   return $blogPosts;
+}
+
+
+
 // ================ MAIN =================
 
 $logger = Logger::getLogger("blog");
@@ -41,8 +84,24 @@ if (isset($_SESSION['userid'])) {
 
    $blogManager = new BlogManager();
 
-   print_r($blogManager->getCategoryList());
+   $src_user_id  = $session_user->id;
+   $severity     = BlogPost::severity_normal;
+   $category     = 0;
+   $summary      = 'Test summary';
+   $content      = 'Hello world !<br>The quick brown fox jumps over the lazy dog';
+   $dest_team_id = 4;
+   $dest_user_id    = 0;
+   $dest_project_id = 0;
+   $date_expire     = 0;
+   $color=0;
+/*
+   $blogPost_id = BlogPost::create($src_user_id, $severity, $category, $summary, $content,
+         $dest_user_id, $dest_project_id, $dest_team_id, $date_expire, $color);
+*/
 
+   $postList = $blogManager->getPosts($session_user->id);
+   $blogPosts = getBlogPosts($postList);
+   $smartyHelper->assign('blogPosts', $blogPosts);
 
 }
 
