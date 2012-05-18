f4aa7bd847d7f05bb9836f50b2d29f1e977edcc6
NSV <nicolas_pro@velin.fr>
2012-05-18 22:28:11 +0200

    Add some top margin for the sub menu

--- include/session.inc.php
+++ include/session.inc.php
@@ -3,7 +3,10 @@ if (!isset($_SESSION)) {
    $tokens = explode('/', $_SERVER['PHP_SELF'], 3);
    $sname = str_replace('.', '_', $tokens[1]);
    session_name($sname);
+   //ini_set("session.gc_maxlifetime","86400"); // 1 day
+   //session_set_cookie_params(86400); // 1 day
    session_start();
+   //setcookie( session_name(), session_id(), time() + 86400 ); // 1 day
    header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
 }
 ?>
