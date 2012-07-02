78c6bf91a16d0dc5efd1133589bb7bd855ab36f6
NSV <nicolas_pro@velin.fr>
2012-07-03 00:06:04 +0200

    Use JQuery Calendar instead of the PHP Calendar

--- tools/date.php
+++ tools/date.php
@@ -1,134 +1,71 @@
 <?php
+
 /*
-    This file is part of CoDev-Timetracking.
+   This file is part of CoDev-Timetracking.
 
-    CoDev-Timetracking is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    CoDev-Timetracking is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   CoDev-Timetracking is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-include_once '../path.inc.php';
+require('../path.inc.php');
 
-require('super_header.inc.php');
-
-#include_once '../tools.php';
-require_once('../calendar/classes/tc_calendar.php');
-
-
-   echo "<link href='".getServerRootURL()."/calendar/calendar.css' rel='stylesheet' type='text/css' />\n";
-echo "<script language='javascript' src='".getServerRootURL()."/calendar/calendar.js'></script>\n";
-
-$page_name = "Date converstion";
-#include 'header.inc.php';
-
-#include 'menu.inc.php';
-?>
-
-<script language="JavaScript">
-  function submitForm1() {
-    document.forms["form1"].action.value = "dateToTimestamp";
-    document.forms["form1"].submit();
-  }
-
-  function submitForm2() {
-     document.forms["form2"].action.value = "timestampToDate";
-     document.forms["form2"].submit();
-   }
-
-</script>
-
-
-<?php
+require('include/super_header.inc.php');
 
 function setCalendarToDateForm($defaultDate1) {
-
-  list($defaultYear, $defaultMonth, $defaultDay) = explode('-', $defaultDate1);
-
-  $myCalendar1 = new tc_calendar("date1", true, false);
-  $myCalendar1->setIcon("../calendar/images/iconCalendar.gif");
-  $myCalendar1->setDate($defaultDay, $defaultMonth, $defaultYear);
-  $myCalendar1->setPath("../calendar/");
-  $myCalendar1->setYearInterval(2010, 2025);
-  $myCalendar1->dateAllow('2010-01-01', '2015-12-31');
-  $myCalendar1->setDateFormat('Y-m-d');
-  $myCalendar1->startMonday(true);
-
-  echo "<div class=left>";
-  // Create form
-  echo "<form id='form1' name='form1' method='post' action='date.php'>\n";
-
-  echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-  $myCalendar1->writeScript();
-
-  echo "&nbsp;<input type=button value='Convert to Timestamp' onClick='javascript: submitForm1()'>\n";
-
-  echo "<input type=hidden name=action      value=noAction>\n";
-
-  echo "</form>\n";
-  echo "</div>";
+   echo "<div class=left>";
+   // Create form
+   echo '<link type="text/css" href="lib/jquery/css/Aristo/Aristo.css" rel="Stylesheet" />';
+   echo '<script type="text/javascript" src="lib/jquery/js/jquery-1.7.1.min.js"></script>';
+   echo '<script type="text/javascript" src="lib/jquery/js/jquery.bgiframe-2.1.2.js"></script>';
+   echo '<script type="text/javascript" src="lib/jquery/js/jquery-ui-1.8.16.custom.min.js"></script>';
+   echo '<script type="text/javascript" src="js/datepicker.js"></script>';
+   echo "<form id='form1' name='form1' method='post' action='$_SERVER[PHP_SELF]'>\n";
+   echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+   echo '<script type="text/javascript">';
+   echo 'jQuery(document).ready(function() {';
+   echo 'jQuery("#datepicker").datepicker("setDate" ,"'.$defaultDate1.'");';
+   echo '});';
+   echo '</script>';
+   echo '<input type="text" id="datepicker" class="datepicker" name="date" maxlength="10" size="10" title="Date" />';
+   echo "&nbsp;<input type=submit value='Convert to Timestamp' />\n";
+   echo "</form>\n";
+   echo "</div>";
 }
 
 function setTimestampToDateForm($timestamp) {
-
-  echo "<div class=left>";
-  // Create form
-  echo "<form id='form2' name='form2' method='post' action='date.php'>\n";
-
-
-  echo("Timestamp: <input name='timestamp' type='text' id='timestamp' value='$timestamp'>\n");
-  echo "&nbsp;<input type=button value='Convert to Date' onClick='javascript: submitForm2()'>\n";
-
-  echo "<input type=hidden name=action      value=noAction>\n";
-
-  echo "</form>\n";
-  echo "</div>";
+   echo "<div class=left>";
+   // Create form
+   echo "<form id='form2' name='form2' method='post' action='$_SERVER[PHP_SELF]'>\n";
+   echo("Timestamp: <input name='timestamp' type='text' id='timestamp' value='$timestamp'>\n");
+   echo "&nbsp;<input type=submit value='Convert to Date'>\n";
+   echo "</form>\n";
+   echo "</div>";
 }
-
-
 
 // =========== MAIN ==========
-
-$date1  = isset($_REQUEST["date1"]) ? $_REQUEST["date1"] : date("Y-m-d", time());
-$timestamp    = $_POST[timestamp];
-
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
-echo "<br/>";
+echo '<html><head><base href="'.getServerRootURL().'/" /></head><body>';
+$date1 = getSecurePOSTStringValue("date",date("Y-m-d", time()));
 setCalendarToDateForm($date1);
-
-$formatedDate = isset($_REQUEST["date1"]) ? $_REQUEST["date1"] : NULL;
-
 echo "<br/>";
+
+$timestamp = getSecurePOSTIntValue("timestamp",0);
 setTimestampToDateForm($timestamp);
 
-
-$action = $_POST[action];
-if ("dateToTimestamp" == $action) {
-
-	if (NULL != $formatedDate) {
-	   $timestamp = date2timestamp($formatedDate);
-	   echo "<br/>$formatedDate => $timestamp<br/>";
-
-	   $_POST[timestamp] = $timestamp;
-	}
-
-} elseif ("timestampToDate" == $action) {
-	echo "<br/>$timestamp => ".date("Y-m-d H:i:s", $timestamp)."<br/>";
+if (isset($_POST["date"])) {
+   $timestamp = date2timestamp($date1);
+   echo "<br/>$formatedDate => $timestamp<br/>";
+} elseif (isset($_POST["timestamp"])) {
+   echo "<br/>$timestamp => ".date("Y-m-d H:i:s", $timestamp)."<br/>";
 }
-
-
-
-
-
 
 ?>
