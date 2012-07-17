c4a3a54d4497e894e5c1a11e8d9364ab04d07313
NSV <nicolas_pro@velin.fr>
2012-07-17 22:20:25 +0200

    COSMETIC Clean up Edit jobs and use Jobs class to get all the job

--- classes/jobs.class.php
+++ classes/jobs.class.php
@@ -1,19 +1,19 @@
 <?php
 /*
-    This file is part of CoDev-Timetracking.
+   This file is part of CoDev-Timetracking.
 
-    CoDev-Timetracking is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    CoDev-Timetracking is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   CoDev-Timetracking is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**
@@ -26,8 +26,8 @@ class Job {
   const type_assignedJob = 1;     // jobs specific to one or more projects are type 1
 
 
-  public static $typeNames = array(Job::type_commonJob    => "Common",
-                                   Job::type_assignedJob  => "Assigned");
+  public static $typeNames = array(self::type_commonJob    => "Common",
+                                   self::type_assignedJob  => "Assigned");
 
    var $id;
    var $name;
@@ -40,40 +40,62 @@ class Job {
       $this->type  = $type;
       $this->color = $color;
     }
+
+   /**
+    * @param Job $jobB
+    * @return int
+    */
+   function compareTo($jobB) {
+      return strcmp($this->name, $jobB->name) < 0 ? TRUE : FALSE;
+   }
 }
 
+Jobs::staticInit();
 
-// =======================================
 class Jobs {
-
-   private $logger;
 
    const JOB_NA      = 1; // REM: N/A     job_id = 1, created by SQL file at install
    const JOB_SUPPORT = 2; // REM: Support job_id = 2, created by SQL file at install
 
-   var $jobList;
+   /**
+    * @var Logger The logger
+    */
+   private static $logger;
 
-   // --------------------
+   /**
+    * @var Job[] The job list
+    */
+   private $jobList;
+
+   /**
+    * Initialize complex static variables
+    * @static
+    */
+   public static function staticInit() {
+      self::$logger = Logger::getLogger(__CLASS__);
+   }
+
    public function __construct() {
-      $this->logger = Logger::getLogger(__CLASS__);
-
-   	$this->jobList = array();
+      $this->jobList = array();
 
       $query = "SELECT * FROM `codev_job_table`";
-   	$result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-            while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
-      	$j = new Job($row->id, $row->name, $row->type, $row->color);
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+         $j = new Job($row->id, $row->name, $row->type, $row->color);
          $this->jobList[$row->id] = $j;
       }
    }
 
+   public function getJobs() {
+      return array_values($this->jobList);
+   }
+
    public function getJobName($id) {
-   	return $this->jobList[$id]->name;
+      return $this->jobList[$id]->name;
    }
 
    public function getJobColor($id) {
@@ -85,42 +107,41 @@ class Jobs {
    }
 
    public function isSupport($id) {
-   	  $job_support = Config::getInstance()->getValue(Config::id_jobSupport);
-      return ($id == $job_support);
+      return ($id == Config::getInstance()->getValue(Config::id_jobSupport));
    }
 
    /**
-    *
-    * @param unknown_type $job_name
-    * @param unknown_type $job_type
-    * @param unknown_type $job_color
+    * @param string $job_name
+    * @param string $job_type
+    * @param string $job_color
+    * @return int $job_id
     */
    public static function create($job_name, $job_type, $job_color) {
-
       $formattedName = SqlWrapper::getInstance()->sql_real_escape_string($job_name);
-   	$query = "INSERT INTO `codev_job_table`  (`name`, `type`, `color`) VALUES ('$formattedName','$job_type','$job_color');";
+      $query = "INSERT INTO `codev_job_table`  (`name`, `type`, `color`) VALUES ('$formattedName','$job_type','$job_color');";
       $result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-   	$job_id = SqlWrapper::getInstance()->sql_insert_id();
 
-   	return $job_id;
+      return SqlWrapper::getInstance()->sql_insert_id();
    }
 
    /**
-    *
+    * @static
+    * @param int $project_id
+    * @param int $job_id
     */
    public static function addJobProjectAssociation($project_id, $job_id) {
       $query = "INSERT INTO `codev_project_job_table`  (`project_id`, `job_id`) VALUES ('$project_id','$job_id');";
       $result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-
-
    }
+
 }
+
 ?>
