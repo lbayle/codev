f1ceb6651fc2e09784518da55a0449787be81875
NSV <nicolas_pro@velin.fr>
2012-07-19 11:31:33 +0200

    PERF Rework all the import issues system Remove callback method (not need to repaint) Change the strategy to find the dataRow (find by the id) Use setTimeout to avoid browser's freeze Move the scrollbar to follow the adding issues

--- import/import_issues.php
+++ import/import_issues.php
@@ -1,41 +1,43 @@
 <?php
+require('../include/session.inc.php');
 
-include_once('../include/session.inc.php');
 /*
-  This file is part of CodevTT
+   This file is part of CodevTT
 
-  CodevTT is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+   CodevTT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-  CodevTT is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+   CodevTT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
-require '../path.inc.php';
+require('../path.inc.php');
 
-require('super_header.inc.php');
+require('include/super_header.inc.php');
 
-/* INSERT INCLUDES HERE */
+require('smarty_tools.php');
 
-require_once('../smarty_tools.php');
+require('classes/smarty_helper.class.php');
 
-require_once('user.class.php');
-require_once('team.class.php');
-require_once('project.class.php');
-require_once('consistency_check2.class.php');
+require_once('classes/user.class.php');
+require_once('classes/team.class.php');
+require_once('classes/project.class.php');
+require_once('classes/consistency_check2.class.php');
 
-require('display.inc.php');
+require_once('tools.php');
 
+require_once('lib/log4php/Logger.php');
 
-/* INSERT FUNCTIONS HERE */
+$logger = Logger::getLogger("import_issues");
 
+/*
 function getFakeNewIssues() {
    $issues = array();
 
@@ -69,15 +71,18 @@ function getFakeNewIssues() {
 
    return $issues;
 }
+*/
 
 /**
- *
- * @param type $filename
+ * @param string $filename
+ * @param string $delimiter
+ * @param string $enclosure
+ * @param string $escape
+ * @return mixed[]
  */
 function getIssuesFromCSV($filename, $delimiter = ';', $enclosure = '"', $escape = '"') {
-
    $issues = array();
-   
+
    $file = new SplFileObject($filename);
    /* Can't be use with PHP 5.1
    $file->setFlags(SplFileObject::READ_CSV);
@@ -97,262 +102,125 @@ function getIssuesFromCSV($filename, $delimiter = ';', $enclosure = '"', $escape
 
             $newIssue = array();
             $newIssue['lineNum']        = $row;
-            $newIssue['summary']        = convertToUTF8($data[0]);
-            $newIssue['mgrEffortEstim'] = str_replace(",", ".", convertToUTF8($data[1])); // 3,5 => 3.5
-            $newIssue['effortEstim']    = str_replace(",", ".", convertToUTF8($data[2])); // 3,5 => 3.5
-            $newIssue['description']    = convertToUTF8($data[3]);
-            $newIssue['deadline']       = convertToUTF8($data[4]);  // YYY-MM-DD
-            $newIssue['extRef']         = convertToUTF8($data[5]);
+            $newIssue['summary']        = Tools::convertToUTF8($data[0]);
+            $newIssue['mgrEffortEstim'] = str_replace(",", ".", Tools::convertToUTF8($data[1])); // 3,5 => 3.5
+            $newIssue['effortEstim']    = str_replace(",", ".", Tools::convertToUTF8($data[2])); // 3,5 => 3.5
+            $newIssue['description']    = Tools::convertToUTF8($data[3]);
+            $newIssue['deadline']       = Tools::convertToUTF8($data[4]);  // YYY-MM-DD
+            $newIssue['extRef']         = Tools::convertToUTF8($data[5]);
             //$newIssue['summary_attr'] = "style='background-color: #FF82B4;'";
             $issues[] = $newIssue;
          }
       }
    }
-   
+
    return $issues;
 }
 
-
 /**
- * jeditable formatted CommandList
- *
- * @param int $teamid
- * @param bool $selected
- * @return string json encoded list
+ * @param Team $team
+ * @return string[]
  */
-function getJsonCommands($teamid, $selected = NULL) {
-
-   $team = TeamCache::getInstance()->getTeam($teamid);
-   $cmdList = $team->getCommands();
-
-   $commands = array();
-   foreach ($cmdList as $id => $cmd) {
-      $commands[$id] = $cmd->getName();
-   }
-
-   if ($selected) {
-      $commands['selected'] = $selected;
-   }
-
-   return array2json($commands);
-}
-
-/**
- *
- * @param type $teamid
- * @param type $selected
- * @return type
- */
-function getCommands($teamid, $selected = NULL) {
-
-   $team = TeamCache::getInstance()->getTeam($teamid);
+function getCommands(Team $team) {
    $cmdList = $team->getCommands();
 
    $items = array();
    if (0 != count($cmdList)) {
-
       foreach ($cmdList as $id => $cmd) {
-         $items[] = array(
-            'id' => $id,
-            'name' => $cmd->getName(),
-            'selected' => ($id == $selected)
-         );
+         $items[$id] = $cmd->getName();
       }
    }
    return $items;
 }
 
 /**
- *
  * @param int $projectid
- * @param bool $selected
- * @return string json encoded list
+ * @return string[]
  */
-function getJsonProjectCategories($projectid, $selected = NULL) {
-
-   $prj = ProjectCache::getInstance()->getProject($projectid);
-
-   $categories = $prj->getCategories();
-
-   if ($selected) {
-      $categories['selected'] = $selected;
-   }
-   return array2json($categories);
-}
-
-/**
- *
- * @param int $projectid
- * @return string json encoded list
- */
-function getProjectCategories($projectid, $selected = NULL) {
-
-   $items = array();
+function getProjectCategories($projectid) {
+   $catList = array();
    if (0 != $projectid) {
-
-   $prj = ProjectCache::getInstance()->getProject($projectid);
-   $catList = $prj->getCategories();
-
-      foreach ($catList as $id => $name) {
-         $items[] = array(
-            'id' => $id,
-            'name' => $name,
-            'selected' => ($id == $selected)
-         );
-      }
+      $prj = ProjectCache::getInstance()->getProject($projectid);
+      $catList = $prj->getCategories();
    }
-   return $items;
-}
-
-
-
-/**
- *
- * @param type $projectid
- * @param type $selected
- * @return type
- */
-function getJsonProjectTargetVersion($projectid, $selected = NULL) {
-
-   $prj = ProjectCache::getInstance()->getProject($projectid);
-
-   $versions = $prj->getProjectVersions();
-
-   if ($selected) {
-      $versions['selected'] = $selected;
-   }
-
-   return array2json($versions);
+   return $catList;
 }
 
 /**
- *
  * @param int $projectid
- * @return string json encoded list
+ * @return string[]
  */
-function getProjectTargetVersion($projectid, $selected = NULL) {
-
-   $items = array();
+function getProjectTargetVersion($projectid) {
+   $versions = array();
    if (0 != $projectid) {
-
-   $prj = ProjectCache::getInstance()->getProject($projectid);
-   $versions = $prj->getProjectVersions();
-
-      foreach ($versions as $id => $name) {
-         $items[] = array(
-            'id' => $id,
-            'name' => $name,
-            'selected' => ($id == $selected)
-         );
-      }
+      $prj = ProjectCache::getInstance()->getProject($projectid);
+      $versions = $prj->getProjectVersions();
    }
-   return $items;
+   return $versions;
 }
-
-
-/**
- *
- * @param type $teamid
- * @param type $selected
- * @return type
- */
-function getJsonUsers($teamid, $selected = NULL) {
-
-   $userList = Team::getActiveMemberList($teamid);
-
-   if ($selected) {
-      $userList['selected'] = $selected;
-   }
-   return array2json($userList);
-}
-
-/**
- *
- * @param type $teamid
- * @param type $selected
- * @return type
- */
-function getUsers($teamid, $selected = NULL) {
-
-   $userList = Team::getActiveMemberList($teamid);
-
-   $items = array();
-   if (0 != count($userList)) {
-
-      foreach ($userList as $id => $name) {
-         $items[] = array(
-            'id' => $id,
-            'name' => $name,
-            'selected' => ($id == $selected)
-         );
-      }
-   }
-   return $items;
-}
-
 
 // ================ MAIN =================
-
-$logger = Logger::getLogger("import_issues");
-
 $smartyHelper = new SmartyHelper();
-$smartyHelper->assign('pageName', T_("Import Mantis Issues"));
+$smartyHelper->assign('pageName', "Import Mantis Issues");
 
 if (isset($_SESSION['userid'])) {
 
    $session_user = UserCache::getInstance()->getUser($_SESSION['userid']);
 
-   $action = isset($_POST['action']) ? $_POST['action'] : '';
+   $action = Tools::getSecurePOSTStringValue('action', '');
 
    $dTeamList = $session_user->getDevTeamList();
-      $lTeamList = $session_user->getLeadedTeamList();
-      $managedTeamList = $session_user->getManagedTeamList();
-      $teamList = $dTeamList + $lTeamList + $managedTeamList;
+   $lTeamList = $session_user->getLeadedTeamList();
+   $managedTeamList = $session_user->getManagedTeamList();
+   $teamList = $dTeamList + $lTeamList + $managedTeamList;
 
-    // use the teamid set in the form, if not defined (first page call) use session teamid
-    if (isset($_POST['teamid'])) {
-        $teamid = $_POST['teamid'];
-        $_SESSION['teamid'] = $teamid;
-    } elseif(isset($_SESSION['teamid'])) {
-        $teamid = $_SESSION['teamid'];
-    } else {
-       $teamIds = array_keys($teamList);
-       $teamid = $teamIds[0];
-    }
+   // use the teamid set in the form, if not defined (first page call) use session teamid
+   if (isset($_POST['teamid'])) {
+      $teamid = Tools::getSecurePOSTIntValue($_POST['teamid']);
+      $_SESSION['teamid'] = $teamid;
+   } elseif(isset($_SESSION['teamid'])) {
+      $teamid = $_SESSION['teamid'];
+   } else {
+      $teamIds = array_keys($teamList);
+      $teamid = $teamIds[0];
+   }
 
-    $smartyHelper->assign('teamid', $teamid);
-    if (0 != $teamid) {
-      $team = TeamCache::getInstance()->getTeam($teamid);
+   $team = TeamCache::getInstance()->getTeam($teamid);
+
+   $smartyHelper->assign('teamid', $teamid);
+   if (0 != $teamid) {
       $smartyHelper->assign('teamName', $team->name);
-    }
+   }
 
-    // use the projectid set in the form, if not defined (first page call) use session projectid
-    if (isset($_POST['projectid'])) {
-        $projectid = $_POST['projectid'];
-    } else {
-        $projectid = isset($_SESSION['projectid']) ? $_SESSION['projectid'] : 0;
-    }
-    $_SESSION['projectid'] = $projectid;
-    $smartyHelper->assign('projectid', $projectid);
-    if (0 != $projectid) {
+   // use the projectid set in the form, if not defined (first page call) use session projectid
+   if (isset($_POST['projectid'])) {
+      $projectid = Tools::getSecurePOSTIntValue('projectid');
+      $_SESSION['projectid'] = $projectid;
+   } else {
+      $projectid = isset($_SESSION['projectid']) ? $_SESSION['projectid'] : 0;
+   }
+   $smartyHelper->assign('projectid', $projectid);
+   if (0 != $projectid) {
       $proj = ProjectCache::getInstance()->getProject($projectid);
       $smartyHelper->assign('projectName', $proj->name);
-    }
+   }
 
-    #if ('' == $action) {
-       // first call to the page, display FileSelector
+   #if ('' == $action) {
+   // first call to the page, display FileSelector
 
-      $smartyHelper->assign('teams', getTeams($teamList,$teamid));
-      $smartyHelper->assign('projects', getSmartyArray(Team::getProjectList($teamid, false),$projectid));
-    #}
+   $smartyHelper->assign('teams', SmartyTools::getSmartyArray($teamList,$teamid));
+   $smartyHelper->assign('projects', SmartyTools::getSmartyArray($team->getProjects(false),$projectid));
+   #}
 
    if ("uploadFile" == $action) {
 
       $filename = $_FILES['uploaded_csv']['name'];
       $tmpFilename = $_FILES['uploaded_csv']['tmp_name'];
-      
+
+      $err_msg = NULL;
+
       if ($_FILES['uploaded_csv']['error']) {
-         
+
          $err_id = $_FILES['uploaded_csv']['error'];
          switch ($err_id){
             case 1:
@@ -393,25 +261,28 @@ if (isset($_SESSION['userid'])) {
 
          $smartyHelper->assign('filename', $filename);
 
+         $commands = getCommands($team);
+         $projectCategories = getProjectCategories($projectid);
+         $projectTargetVersion = getProjectTargetVersion($projectid);
+         $activeMembers = $team->getActiveMembers();
 
-         $smartyHelper->assign('commandList', getCommands($teamid));
-         $smartyHelper->assign('categoryList', getProjectCategories($projectid));
-         $smartyHelper->assign('targetversionList', getProjectTargetVersion($projectid));
-         $smartyHelper->assign('userList', getUsers($teamid));
+         $smartyHelper->assign('commandList', SmartyTools::getSmartyArray($commands, 0));
+         $smartyHelper->assign('categoryList', SmartyTools::getSmartyArray($projectCategories, 0));
+         $smartyHelper->assign('targetversionList', SmartyTools::getSmartyArray($projectTargetVersion, 0));
+         $smartyHelper->assign('userList', SmartyTools::getSmartyArray($activeMembers, 0));
 
-         $smartyHelper->assign('jed_commandList', getJsonCommands($teamid));
-         $smartyHelper->assign('jed_categoryList', getJsonProjectCategories($projectid));
-         $smartyHelper->assign('jed_targetVersionList', getJsonProjectTargetVersion($projectid));
-         $smartyHelper->assign('jed_userList', getJsonUsers($teamid));
+         $smartyHelper->assign('jed_commandList', Tools::array2json($commands));
+         $smartyHelper->assign('jed_categoryList', Tools::array2json($projectCategories));
+         $smartyHelper->assign('jed_targetVersionList', Tools::array2json($projectTargetVersion));
+         $smartyHelper->assign('jed_userList', Tools::array2json($activeMembers));
 
       } else {
          $smartyHelper->assign('errorMsg', $err_msg);
       }
    }
 
-
-
 }
 
 $smartyHelper->displayTemplate($codevVersion, $_SESSION['username'], $_SESSION['realname'], $mantisURL);
+
 ?>
