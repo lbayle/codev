7ac16252b4487e5cd8ba9713b0ec1e8d0a0db66a
NSV <nicolas_pro@velin.fr>
2012-08-04 22:11:01 +0200

    COSMETIC Clean up PeriodStats report class

--- classes/period_stats_report.class.php
+++ classes/period_stats_report.class.php
@@ -1,23 +1,25 @@
 <?php
 /*
-    This file is part of CoDev-Timetracking.
+   This file is part of CoDev-Timetracking.
 
-    CoDev-Timetracking is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    CoDev-Timetracking is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   CoDev-Timetracking is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-include_once "period_stats.class.php";
-include_once "project.class.php";
+include_once('classes/config.class.php');
+include_once('classes/period_stats.class.php');
+include_once('classes/project.class.php');
+include_once('classes/sqlwrapper.class.php');
 
 /**
  * CALCULATE PERIOD STATS Reports
@@ -25,110 +27,104 @@ include_once "project.class.php";
  */
 class PeriodStatsReport {
 
-  var $start_year;
-  var $start_month;
-  var $start_day;
-  var $periodStatsList;
+   var $start_year;
+   var $start_month;
+   var $start_day;
+   var $periodStatsList;
 
-  var $teamid;
+   var $teamid;
 
-  // --------------------------------------------
-  public function PeriodStatsReport($start_year, $start_month, $start_say, $teamid) {
-    $this->start_year = $start_year;
-    $this->start_month = $start_month;
-    $this->start_day = $start_say;
-    $this->periodStatsList = array();
+   public function PeriodStatsReport($start_year, $start_month, $start_say, $teamid) {
+      $this->start_year = $start_year;
+      $this->start_month = $start_month;
+      $this->start_day = $start_say;
+      $this->periodStatsList = array();
 
-    $this->teamid = $teamid;
-  }
+      $this->teamid = $teamid;
+   }
 
-  // --------------------------------------------
-  // Compute monthly reports for the complete year
-  public function computeReport() {
+   /**
+    * Compute monthly reports for the complete year
+    */
+   public function computeReport() {
+      $now = time();
+      $startM = $this->start_month;
+      $startD = $this->start_day;
 
-    $now = time();
-    $startM = $this->start_month;
-    $startD = $this->start_day;
+      for ($y = $this->start_year; $y <= date('Y'); $y++) {
 
-    for ($y = $this->start_year; $y <= date('Y'); $y++) {
+         for ($month=$startM; $month<13; $month++) {
+            $startTimestamp = mktime(0, 0, 1, $month, $startD, $y);
+            $endTimestamp   = mktime(0, 0, 1, ($month + 1), $startD, $y);
 
-	    for ($month=$startM; $month<13; $month++) {
-	      $startTimestamp = mktime(0, 0, 1, $month, $startD, $y);
-	      $endTimestamp   = mktime(0, 0, 1, ($month + 1), $startD, $y);
+            if ($startTimestamp > $now) { break; }
 
-	      if ($startTimestamp > $now) { break; }
+            $periodStats = new PeriodStats($startTimestamp, $endTimestamp);
 
-	      $periodStats = new PeriodStats($startTimestamp, $endTimestamp);
+            $projectList = array();
 
+            // only projects for specified team, except excluded projects
+            $query = "SELECT project_id FROM `codev_team_project_table` ".
+               "WHERE team_id = $this->teamid ".
+               "AND codev_team_project_table.type <> ".Project::type_noStatsProject;
 
-	      $projectList = array();
+            $result = SqlWrapper::getInstance()->sql_query($query);
+            if (!$result) {
+               echo "<span style='color:red'>ERROR: Query FAILED</span>";
+               exit;
+            }
+            while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+               $projectList[] = $row->project_id;
+            }
 
-	      // only projects for specified team, except excluded projects
-	      $query = "SELECT project_id FROM `codev_team_project_table` ".
-	               "WHERE team_id = $this->teamid ".
-	               "AND codev_team_project_table.type <> ".Project::type_noStatsProject;
-
-	      $result = SqlWrapper::getInstance()->sql_query($query);
-	      if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+            $periodStats->projectList = $projectList;
+            $periodStats->computeStats();
+            $this->periodStatsList[$startTimestamp] = $periodStats;
+            $startD = 1;
          }
-	      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
-            $projectList[] = $row->project_id;
-	      }
+         $startM = 1;
+      }
+   }
 
-	      $periodStats->projectList = $projectList;
-	      $periodStats->computeStats();
-	      $this->periodStatsList[$startTimestamp] = $periodStats;
-	      $startD = 1;
-	    }
-	    $startM = 1;
-    }
-  }
-
-
-  // --------------------------------------------
-  public function getStatus($status) {
+   public function getStatus($status) {
       $sub = array();
 
       foreach ($this->periodStatsList as $date => $ps) {
-      	$sub[$date] = $ps->statusCountList[$status];
+         $sub[$date] = $ps->statusCountList[$status];
 
       }
-  	   return $sub;
-  }
+      return $sub;
+   }
 
-  // --------------------------------------------
-  function displayHTMLReport() {
+   function displayHTMLReport() {
+      $statusNames = Config::getInstance()->getValue("statusNames");
+      ksort($statusNames);
 
-    $statusNames = Config::getInstance()->getValue("statusNames");
-    ksort($statusNames);
-
-    echo "<table>\n";
-    echo "<caption title='Bilan mensuel SAUF SuiviOp.'>Bilan mensuel (nbre de fiches / status &agrave; la fin du mois)</caption>";
-    echo "<tr>\n";
-    echo "<th>Date</th>\n";
-    foreach ($statusNames as $s => $sname) {
-      echo "<th>$sname</th>\n";
-    }
-    echo "</tr>\n";
-
-    foreach ($this->periodStatsList as $date => $ps) {
-
-      // Disp
-      $tableLine = "<tr>\n";
-      $tableLine .= "<td class=\"right\">".formatDate("%B %Y", $date)."</td>\n";
-
-      foreach ($statusNames as $s => $sname) {
-         $tableLine .= "<td class=\"right\">".$ps->statusCountList[$s]."</td>\n";
+      echo "<table>\n";
+      echo "<caption title='Bilan mensuel SAUF SuiviOp.'>Bilan mensuel (nbre de fiches / status &agrave; la fin du mois)</caption>";
+      echo "<tr>\n";
+      echo "<th>Date</th>\n";
+      foreach ($statusNames as $sname) {
+         echo "<th>$sname</th>\n";
       }
-      $tableLine .= "</tr>\n";
-      echo "$tableLine";
+      echo "</tr>\n";
 
-    }
-    echo "</table>\n";
-  }
+      foreach ($this->periodStatsList as $date => $ps) {
 
-} // end class PeriodStatsReport
+         // Disp
+         $tableLine = "<tr>\n";
+         $tableLine .= "<td class=\"right\">".formatDate("%B %Y", $date)."</td>\n";
+
+         foreach ($statusNames as $s => $sname) {
+            $tableLine .= "<td class=\"right\">".$ps->statusCountList[$s]."</td>\n";
+         }
+         $tableLine .= "</tr>\n";
+         echo "$tableLine";
+
+      }
+      echo "</table>\n";
+   }
+
+}
 
 ?>
