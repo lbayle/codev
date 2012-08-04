9b1b3c465e2d269f5ef3fdc846fa380b78b2057c
NSV <nicolas_pro@velin.fr>
2012-08-04 22:09:47 +0200

    COSMETIC Clean up Holidays class

--- classes/holidays.class.php
+++ classes/holidays.class.php
@@ -16,6 +16,8 @@
    along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+include_once('classes/sqlwrapper.class.php');
+
 require_once('lib/log4php/Logger.php');
 
 /**
