5df89157bfa702ddd3a3392710039ef525a69f0c
NSV <nicolas_pro@velin.fr>
2012-08-04 22:09:12 +0200

    COSMETIC Clean up ConsistencyCheck class

--- classes/consistency_check.class.php
+++ classes/consistency_check.class.php
@@ -1,26 +1,28 @@
 <?php
 /*
-    This file is part of CoDev-Timetracking.
+   This file is part of CoDev-Timetracking.
 
-    CoDev-Timetracking is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    CoDev-Timetracking is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   CoDev-Timetracking is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+   You should have received a copy of the GNU General Public License
+   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 include_once('constants.php');
 
-include_once('classes/issue.class.php');
-include_once('classes/user.class.php');
-include_once('classes/project.class.php');
+include_once('classes/issue_cache.class.php');
+include_once('classes/project_cache.class.php');
+include_once('classes/sqlwrapper.class.php');
+
+require_once('tools.php');
 
 require_once('lib/log4php/Logger.php');
 
@@ -39,42 +41,40 @@ class ConsistencyError {
       self::$logger = Logger::getLogger(__CLASS__);
    }
 
-	public $bugId;
-	public $userId;
-	public $teamId;
-	public $desc;
+   public $bugId;
+   public $userId;
+   public $teamId;
+   public $desc;
    public $timestamp;
    public $status;
 
    public $severity; // unused
 
-	public function ConsistencyError($bugId, $userId, $status, $timestamp, $desc) {
-      $this->bugId     = $bugId;
-      $this->userId    = $userId;
+   public function ConsistencyError($bugId, $userId, $status, $timestamp, $desc) {
+      $this->bugId = $bugId;
+      $this->userId = $userId;
       $this->status = $status;
       $this->timestamp = $timestamp;
-      $this->desc      = $desc;
-	}
+      $this->desc = $desc;
+   }
 
-	// ----------------------------------------------
-	/**
-	 * QuickSort compare method.
-	 * returns true if $this has higher priority than $activityB
-	 *
-	 * @param GanttActivity $activityB the object to compare to
-	 */
-	function compareTo($cerrB) {
-
-	   // the oldest activity should be in front of the list
-	   if ($this->bugId > $cerrB->bugId) {
-	      self::$logger->debug("activity.compareTo FALSE (".$this->bugId." >  ".$cerrB->bugId.")");
-	      return false;
-	   } else {
-	      self::$logger->debug("activity.compareTo TRUE  (".$this->bugId." <= ".$cerrB->bugId.")");
-	      return true;
-	   }
-	   return true;
-	}
+   /**
+    * QuickSort compare method.
+    * returns true if $this has higher priority than $activityB
+    *
+    * @param ConsistencyError $cerrB the object to compare to
+    * @return bool
+    */
+   function compareTo($cerrB) {
+      // the oldest activity should be in front of the list
+      if ($this->bugId > $cerrB->bugId) {
+         self::$logger->debug("activity.compareTo FALSE (".$this->bugId." >  ".$cerrB->bugId.")");
+         return false;
+      } else {
+         self::$logger->debug("activity.compareTo TRUE  (".$this->bugId." <= ".$cerrB->bugId.")");
+         return true;
+      }
+   }
 
 }
 
@@ -97,21 +97,19 @@ class ConsistencyCheck {
 
    var $projectList;
 
-   // ----------------------------------------------
    public function __construct($projectList = NULL) {
       if (NULL != $projectList) {
-   		$this->projectList = $projectList;
-   	} else {
-   		 $projectList = array();
-   	}
+         $this->projectList = $projectList;
+      } else {
+         $this->projectList = array();
+      }
    }
 
-   // ----------------------------------------------
    /**
     * perform all consistency checks
+    * @return ConsistencyError[]
     */
    public function check() {
-
       #self::$logger->debug("checkResolved");
       $cerrList2 = $this->checkResolved();
 
@@ -131,67 +129,59 @@ class ConsistencyCheck {
       #$cerrList = array_merge($cerrList2, $cerrList3, $cerrList4, $cerrList5);
       $cerrList = array_merge($cerrList2, $cerrList4, $cerrList5, $cerrList6);
 
-
-      $sortedCerrList = qsort($cerrList);
+      $sortedCerrList = Tools::qsort($cerrList);
 
       return $sortedCerrList;
    }
 
-
-   // ----------------------------------------------
    /**
     * if $deliveryIssueCustomField is specified, then $deliveryDateCustomField should also be specified.
+    * @return ConsistencyError[]
     */
    public function checkDeliveryDate() {
-
-      $deliveryIdCustomField     = Config::getInstance()->getValue(Config::id_customField_deliveryId);
-      $deliveryDateCustomField   = Config::getInstance()->getValue(Config::id_customField_deliveryDate);
-
       $cerrList = array();
 
       // select all issues which current status is 'analyzed'
       $query = "SELECT * ".
-        "FROM `mantis_bug_table` ".
-        "WHERE status >= get_project_resolved_status_threshold(project_id) ";
+         "FROM `mantis_bug_table` ".
+         "WHERE status >= get_project_resolved_status_threshold(project_id) ";
 
       if (0 != count($this->projectList)) {
-      	$formatedProjects = implode( ', ', array_keys($this->projectList));
-      	$query .= "AND project_id IN ($formatedProjects) ";
+         $formatedProjects = implode( ', ', array_keys($this->projectList));
+         $query .= "AND project_id IN ($formatedProjects) ";
       }
 
-       $query .="ORDER BY last_updated DESC, bug_id DESC";
+      $query .="ORDER BY last_updated DESC, bug_id DESC";
 
-       $result = SqlWrapper::getInstance()->sql_query($query);
-	    if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
-       }
-       while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
+      }
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
          $issue = IssueCache::getInstance()->getIssue($row->id, $row);
 
          if ((NULL != $issue->deliveryId) &&
-         	 (NULL == $issue->deliveryDate)) {
-               $cerr = new ConsistencyError($row->id,
-                                              $row->handler_id,
-                                              $row->status,
-                                              $row->last_updated,
-                                              T_("Delivery date not specified: If a delivery sheet is specified, then a Delivery Date is requested."));
-               $cerr->severity = T_("Error");
-               $cerrList[] = $cerr;
-         	 }
+            (NULL == $issue->deliveryDate)) {
+            $cerr = new ConsistencyError($row->id,
+               $row->handler_id,
+               $row->status,
+               $row->last_updated,
+               T_("Delivery date not specified: If a delivery sheet is specified, then a Delivery Date is requested."));
+            $cerr->severity = T_("Error");
+
+            $cerrList[] = $cerr;
+         }
       }
       return $cerrList;
 
    }
 
-
-   // ----------------------------------------------
    /**
     * fiches resolved dont le RAE != 0
+    * @return ConsistencyError[]
     */
    public function checkResolved() {
-
       $cerrList = array();
 
       // select all issues which current status is 'analyzed'
@@ -204,39 +194,38 @@ class ConsistencyCheck {
          $query .= "AND project_id IN ($formatedProjects) ";
       }
 
-      $query .="ORDER BY last_updated DESC, bug_id DESC";
+      $query .= "ORDER BY last_updated DESC, bug_id DESC";
 
       $result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-      while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
+
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
          // check if fields correctly set
-      	$issue = IssueCache::getInstance()->getIssue($row->id, $row);
+         $issue = IssueCache::getInstance()->getIssue($row->id, $row);
 
          if (0 != $issue->backlog) {
-           $cerr = new ConsistencyError($row->id,
-                                              $row->handler_id,
-                                              $row->status,
-                                              $row->last_updated,
-                                              T_("Backlog should be 0 (not $issue->backlog)."));
+            $cerr = new ConsistencyError($row->id,
+               $row->handler_id,
+               $row->status,
+               $row->last_updated,
+               T_("Backlog should be 0 (not $issue->backlog)."));
             $cerr->severity = T_("Error");
+
             $cerrList[] = $cerr;
          }
       }
 
       return $cerrList;
-  	}
+   }
 
-
-
-   // ----------------------------------------------
    /**
     * fiches NOT resolved with RAE == 0
+    * @return ConsistencyError[]
     */
-  	public function checkBadBacklog() {
+   public function checkBadBacklog() {
       global $status_new;
       global $status_acknowledged;
 
@@ -254,25 +243,26 @@ class ConsistencyCheck {
          $query .= "AND project_id IN ($formatedProjects) ";
       }
 
-      $query .="ORDER BY last_updated DESC, bug_id DESC";
+      $query .= "ORDER BY last_updated DESC, bug_id DESC";
 
-  	   $result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-      while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
+
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
          // check if fields correctly set
          $issue = IssueCache::getInstance()->getIssue($row->id, $row);
 
          if ($issue->backlog <= $min_backlog) {
-           $cerr = new ConsistencyError($row->id,
-                                              $row->handler_id,
-                                              $row->status,
-                                              $row->last_updated,
-                                              T_("Backlog == 0: Backlog may not be up to date."));
+            $cerr = new ConsistencyError($row->id,
+               $row->handler_id,
+               $row->status,
+               $row->last_updated,
+               T_("Backlog == 0: Backlog may not be up to date."));
             $cerr->severity = T_("Warning");
+
             $cerrList[] = $cerr;
          }
       }
@@ -281,120 +271,117 @@ class ConsistencyCheck {
 
    }
 
-   // ----------------------------------------------
    /**
     * a mgrEffortEstim should be defined when creating an Issue
+    * @return ConsistencyError[]
     */
    public function checkMgrEffortEstim() {
+      $cerrList = array();
 
-   	$cerrList = array();
-
-   	// select all issues
+      // select all issues
       $query = "SELECT * ".
                "FROM `mantis_bug_table` ".
                "WHERE status < get_project_resolved_status_threshold(project_id) ";
 
       if (0 != count($this->projectList)) {
 
-      	// --- except SideTasksProjects (they don't have a MgrEffortEstim field)
-      	$prjListNoSideTasks = $this->projectList; // copy
+         // --- except SideTasksProjects (they don't have a MgrEffortEstim field)
+         $prjListNoSideTasks = $this->projectList; // copy
          foreach ($prjListNoSideTasks as $id => $name) {
-         	$p = ProjectCache::getInstance()->getProject($id);
-         	if (true == $p->isSideTasksProject()) {
+            $p = ProjectCache::getInstance()->getProject($id);
+            if (true == $p->isSideTasksProject()) {
                unset($prjListNoSideTasks[$id]);
-         	}
+            }
          }
 
          if (0 != count($prjListNoSideTasks)) {
-             $formatedProjects = implode( ', ', array_keys($prjListNoSideTasks));
-             $query .= "AND project_id IN ($formatedProjects) ";
+            $formatedProjects = implode( ', ', array_keys($prjListNoSideTasks));
+            $query .= "AND project_id IN ($formatedProjects) ";
          }
       } else {
-      	// TODO except SideTasksProjects
+         // TODO except SideTasksProjects
       }
 
-      $query .="ORDER BY last_updated DESC, bug_id DESC";
+      $query .= "ORDER BY last_updated DESC, bug_id DESC";
 
       $result = SqlWrapper::getInstance()->sql_query($query);
-	   if (!$result) {
-    	      echo "<span style='color:red'>ERROR: Query FAILED</span>";
-    	      exit;
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-      while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
+
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
          // check if fields correctly set
          $issue = IssueCache::getInstance()->getIssue($row->id, $row);
 
-	         if ((NULL   == $issue->mgrEffortEstim) ||
-	             ('' == $issue->mgrEffortEstim)     ||
-	             ('0' == $issue->mgrEffortEstim)) {
+         if ((NULL   == $issue->mgrEffortEstim) ||
+            ('' == $issue->mgrEffortEstim)     ||
+            ('0' == $issue->mgrEffortEstim)) {
 
-	           $cerr = new ConsistencyError($row->id,
-	                                              $row->handler_id,
-	                                              $row->status,
-	                                              $row->last_updated,
-	                                              T_("MgrEffortEstim not set."));
-	            $cerr->severity = T_("Error");
-	            $cerrList[] = $cerr;
-	         }
+            $cerr = new ConsistencyError($row->id,
+               $row->handler_id,
+               $row->status,
+               $row->last_updated,
+               T_("MgrEffortEstim not set."));
+            $cerr->severity = T_("Error");
+
+            $cerrList[] = $cerr;
+         }
 
       }
 
-
-
       return $cerrList;
-
    }
 
    /**
     * if you spend some time on a task,
     * then it's status is probably 'ack' or 'open' but certainly not 'new'
+    * @return ConsistencyError[]
     */
    function checkTimeTracksOnNewIssues() {
+      global $status_new;
+      global $statusNames;
 
-    global $status_new;
-    global $statusNames;
+      $cerrList = array();
 
-    $cerrList = array();
-
-    // select all issues which current status is 'new'
+      // select all issues which current status is 'new'
       $query = "SELECT * ".
                "FROM `mantis_bug_table` ".
                "WHERE status = $status_new ";
 
       if (0 != count($this->projectList)) {
-        $formatedProjects = implode( ', ', array_keys($this->projectList));
-        $query .= "AND project_id IN ($formatedProjects) ";
+         $formatedProjects = implode( ', ', array_keys($this->projectList));
+         $query .= "AND project_id IN ($formatedProjects) ";
       }
 
-      $query .="ORDER BY handler_id, bug_id DESC";
+      $query .= "ORDER BY handler_id, bug_id DESC";
 
-       $result = SqlWrapper::getInstance()->sql_query($query);
-       if (!$result) {
-              echo "<span style='color:red'>ERROR: Query FAILED</span>";
-              exit;
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
       }
-      while($row = SqlWrapper::getInstance()->sql_fetch_object($result))
-      {
-        $issue = IssueCache::getInstance()->getIssue($row->id, $row);
-        $elapsed = $issue->getElapsed();
 
-        if (0 != $elapsed) {
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+         $issue = IssueCache::getInstance()->getIssue($row->id, $row);
+         $elapsed = $issue->getElapsed();
 
-        	// error
+         if (0 != $elapsed) {
+
+            // error
             $cerr = new ConsistencyError($row->id,
-                                                  $row->handler_id,
-                                                  $row->status,
-                                                  $row->last_updated,
-                                                  T_("Status should not be")." '".$statusNames[$status_new]."' (".T_("elapsed")." = ".$elapsed.")");
+               $row->handler_id,
+               $row->status,
+               $row->last_updated,
+               T_("Status should not be")." '".$statusNames[$status_new]."' (".T_("elapsed")." = ".$elapsed.")");
             $cerr->severity = T_("Error");
+
             $cerrList[] = $cerr;
-        }
+         }
       }
 
       return $cerrList;
    }
-
 
 }
 
