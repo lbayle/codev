9979128eede28a85f484b1d7104cdeee6374aca9
NSV <nicolas_pro@velin.fr>
2012-08-27 22:38:52 +0200

    FIX Load chart.js before tabs.js

--- js/chart.js
+++ js/chart.js
@@ -1,68 +1,6 @@
 // Date line plot
 jQuery(document).ready(function() {
 
-   // Libs
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/jquery.jqplot.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.dateAxisRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.cursor.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.pointLabels.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.highlighter.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.pieRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.canvasAxisLabelRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.canvasTextRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.canvasAxisTickRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-   jQuery.ajax({
-      url: "lib/jquery.jqplot/plugins/jqplot.categoryAxisRenderer.min.js",
-      dataType: "script",
-      async: false,
-      cache: true
-   });
-
    jQuery.jqplot.config.enablePlugins = false;
 
    // Chart by date
