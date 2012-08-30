8f26fe1af4221867cf8e1077b40623c018364ac1
Louis BAYLE <lbayle.trash@gmail.com>
2012-08-30 23:53:43 +0200

    tool: convert constants.php to config.ini

--- tools/convert_config_file.php
+++ tools/convert_config_file.php
@@ -18,7 +18,50 @@ include_once('../include/session.inc.php');
   You should have received a copy of the GNU General Public License
   along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
  */
+require('../path.inc.php');
 
+include_once "include/mysql_config.inc.php";
+#include_once "tools.php";
+#include_once "internal_config.inc.php";
+include_once "constants.php";
 
+require_once('classes/config.class.php');
+require_once('classes/constants.class.php');
+
+// ================ MAIN =================
+if(isset($_SESSION['userid'])) {
+
+   echo "converting constants.php to config.ini";
+
+   Constants::$codevInstall_timestamp = $codevInstall_timestamp;
+   Constants::$codevtt_logfile        = $codevtt_logfile;
+   Constants::$codevOutputDir         = Config::getInstance()->getValue(Config::id_codevReportsDir).DIRECTORY_SEPARATOR.'..';
+   Constants::$homepage_title         = $homepage_title;
+   Constants::$codevRootDir           = $codevRootDir;
+   Constants::$mantisPath             = Config::getInstance()->getValue(Config::id_mantisPath);
+   Constants::$mantisURL              = $mantisURL;
+
+   Constants::$db_mantis_host     = $db_mantis_host;
+   Constants::$db_mantis_database = $db_mantis_database;
+   Constants::$db_mantis_user     = $db_mantis_user;
+   Constants::$db_mantis_pass     = $db_mantis_pass;
+
+   Constants::$statusNames      = Config::getInstance()->getValue(Config::id_statusNames);
+   Constants::$priority_names   = Config::getInstance()->getValue(Config::id_priorityNames);
+   Constants::$resolution_names = Config::getInstance()->getValue(Config::id_resolutionNames);
+   Constants::$severity_names   = Config::getInstance()->getValue(Config::id_severityNames);
+   Constants::$bug_resolved_status_threshold = Config::getInstance()->getValue(Config::id_bugResolvedStatusThreshold);
+
+   Constants::$status_new          = $status_new;
+   Constants::$status_feedback     = $status_feedback;
+   Constants::$status_acknowledged = $status_acknowledged;
+   Constants::$status_open         = $status_open;
+   Constants::$status_closed       = $status_closed;
+
+   Constants::$resolution_fixed    = array_search('fixed',    Constants::$resolution_names);
+   Constants::$resolution_reopened = array_search('reopened',    Constants::$resolution_names);
+
+   Constants::writeConfigFile();
+}
 
 ?>
