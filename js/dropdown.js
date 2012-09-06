276efbb5c8e460da3542ad9a5f4bf4c50ece2ba5
NSV <nicolas_pro@velin.fr>
2012-09-06 21:12:34 +0200

    EVO Add submenu

---
+++ js/dropdown.js
@@ -0,0 +1,13 @@
+// Dropdown for menu
+jQuery(document).ready(function() {
+   jQuery("ul.dropdown li").hover(function() {
+      jQuery(this).addClass("hover");
+      jQuery('ul:first',this).css('visibility', 'visible');
+    }, function() {
+      jQuery(this).removeClass("hover");
+      jQuery('ul:first',this).css('visibility', 'hidden');
+    });
+
+   jQuery("ul.submenu li:has(ul)").find("a:first").append("&raquo;");
+
+});
