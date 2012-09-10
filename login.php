0798dc7218c7ef83b106a11ed0eb13a16d2b7b18
Louis BAYLE <lbayle.trash@gmail.com>
2012-09-11 01:07:20 +0200

    FIX: pb if no defaultProject defined

--- login.php
+++ login.php
@@ -43,9 +43,14 @@ function login($user, $password) {
 
         try {
             $user =  UserCache::getInstance()->getUser($row_login->id);
-            $_SESSION['teamid'] = $user->getDefaultTeam();
             $_SESSION['locale'] = $user->getDefaultLanguage();
-            $_SESSION['projectid'] = $user->getDefaultProject();
+
+            $teamid = $user->getDefaultTeam();
+            if (0 != $teamid) { $_SESSION['teamid'] = $teamid; }
+            
+            $projid = $user->getDefaultProject();
+            if (0 != $projid) { $_SESSION['projectid'] = $projid; }
+
          } catch (Exception $e) {
             $logger->debug("could not load preferences for user $row_login->id");
          }
