b447da5978024e3fbf458371faeb93c71e775694
NSV <nicolas_pro@velin.fr>
2012-10-03 09:57:53 +0200

    FIX Remove cookie usage

--- include/session.inc.php
+++ include/session.inc.php
@@ -4,9 +4,9 @@ if (!isset($_SESSION)) {
    $sname = str_replace('.', '_', $tokens[1]);
    session_name($sname);
    ini_set("session.gc_maxlifetime","83200"); // 1 day = 86400
-   session_set_cookie_params(83200); // 1 day = 86400
+   //session_set_cookie_params(83200); // 1 day = 86400
    session_start();
-   setcookie( session_name(), session_id(), time() + 83200 ); // 1 day = 86400
+   //setcookie( session_name($sname), session_id(), time() + 83200 ); // 1 day = 86400
    header('P3P: CP="NOI ADM DEV PSAi COM NAV OUR OTRo STP IND DEM"');
 }
 ?>
