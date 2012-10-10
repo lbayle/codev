218d19225a845f433f8ea47c87b2c581eadc95b7
Louis BAYLE <lbayle.work@gmail.com>
2012-10-10 17:51:47 +0200

    EVO StatusHistoryIndicator (not finished)

---
+++ indicator_plugins/StatusHistoryIndicator.class.php
@@ -0,0 +1,266 @@
+<?php
+
+/*
+  This file is part of CodevTT
+
+  CodevTT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CodevTT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Description of StatusHistoryIndicator
+ *
+ */
+class StatusHistoryIndicator implements IndicatorPlugin {
+
+   /**
+    * @var Logger The logger
+    */
+   private static $logger;
+   private $id;
+
+   private $statusData;
+
+   protected $execData;
+
+   /**
+    * Initialize complex static variables
+    * @static
+    */
+   public static function staticInit() {
+      self::$logger = Logger::getLogger(__CLASS__);
+   }
+
+   public function __construct() {
+
+   }
+
+   public function getDesc() {
+      return "Display Issue Status history";
+   }
+   public function getName() {
+      return __CLASS__;
+   }
+   public static function getSmartyFilename() {
+      return "plugin/status_history_indicator.html";
+   }
+
+
+   /**
+    *
+    * @param IssueSelection $inputIssueSel
+    * @param array $params
+    * @throws Exception
+    */
+   private function checkParams(IssueSelection $inputIssueSel, array $params = NULL) {
+
+      if (is_null($inputIssueSel)) {
+         throw new Exception("Missing IssueSelection");
+      }
+
+      if (array_key_exists('startTimestamp', $params)) {
+         $this->startTimestamp = $params['startTimestamp'];
+      } else {
+         throw new Exception("Missing parameter: startTimestamp");
+      }
+
+      if (array_key_exists('endTimestamp', $params)) {
+         $this->endTimestamp = $params['endTimestamp'];
+      } else {
+         throw new Exception("Missing parameter: endTimestamp");
+      }
+
+      if (array_key_exists('interval', $params)) {
+         $this->interval = $params['interval'];
+      } else {
+         throw new Exception("Missing parameter: interval");
+      }
+
+   }
+
+   /**
+    * @param IssueSelection $inputIssueSel
+    * @param int[] $timestampList
+    */
+   private function getStatusData(IssueSelection $inputIssueSel, array $timestampList) {
+      $this->statusData = array();
+
+      // get a snapshot of the Status at each timestamp
+      $issues = $inputIssueSel->getIssueList();
+      foreach ($timestampList as $timestamp) {
+         $statusList = array(
+             'new' => 0,
+             'feedback' => 0,
+             'ongoing' => 0,
+             'resolved' => 0,
+             'total' => 0,
+         );
+         foreach ($issues as $issue) {
+            $issueStatus = $issue->getStatus($timestamp);
+
+            // if issue exists at this date
+            if ( (-1) != $issueStatus) {
+
+               if ($issueStatus >= $issue->getBugResolvedStatusThreshold()) {
+                  $statusList['resolved'] += 1;
+               } elseif ($issueStatus == Constants::$status_feedback) {
+                  $statusList['feedback'] += 1;
+               } elseif ($issueStatus == Constants::$status_new) {
+                  $statusList['new'] += 1;
+               } else {
+                  $statusList['ongoing'] += 1;
+               }
+               $statusList['total'] += 1;
+               #echo date('Y-m-d', $timestamp)." issue ".$issue->getId()." status ".$issueStatus."<br>";
+            }
+         }
+
+         $midnight_timestamp = mktime(0, 0, 0, date('m', $timestamp), date('d', $timestamp), date('Y', $timestamp));
+         $this->statusData["$midnight_timestamp"] = $statusList;
+
+         echo date('Y-m-d', $timestamp).' '; var_dump($statusList); echo '<br>';
+      }
+      return $this->statusData;
+   }
+
+   /**
+    * @param IssueSelection $inputIssueSel
+    * @param int[] $timestampList
+    */
+   private function getStatusData2(IssueSelection $inputIssueSel, array $timestampList) {
+      $this->statusData = array();
+
+      $historyStatusNew = array();  // timestamp => nbIssues
+      $historyStatusFeedback = array();  // timestamp => nbIssues
+      $historyStatusOngoing = array();  // timestamp => nbIssues
+      $historyStatusResolved = array();  // timestamp => nbIssues
+      $historyStatusTotal = array();  // timestamp => nbIssues
+
+
+      // get a snapshot of the Status at each timestamp
+      $issues = $inputIssueSel->getIssueList();
+      foreach ($timestampList as $timestamp) {
+
+         $midnight_timestamp = mktime(0, 0, 0, date('m', $timestamp), date('d', $timestamp), date('Y', $timestamp));
+
+        // all timestamps must be defined, even if empty
+         $historyStatusNew["$midnight_timestamp"] = 0;
+         $historyStatusFeedback["$midnight_timestamp"] = 0;
+         $historyStatusOngoing["$midnight_timestamp"] = 0;
+         $historyStatusResolved["$midnight_timestamp"] = 0;
+         $historyStatusTotal["$midnight_timestamp"] = 0;
+
+         foreach ($issues as $issue) {
+            $issueStatus = $issue->getStatus($timestamp);
+
+            // if issue exists at this date
+            if ( (-1) != $issueStatus) {
+
+               if ($issueStatus >= $issue->getBugResolvedStatusThreshold()) {
+                  $historyStatusResolved["$midnight_timestamp"] += 1;
+               } else if ($issueStatus == Constants::$status_feedback) {
+                  $historyStatusFeedback["$midnight_timestamp"] += 1;
+               } else if ($issueStatus == Constants::$status_new) {
+                  $historyStatusNew["$midnight_timestamp"] += 1;
+               } else {
+                  $historyStatusOngoing["$midnight_timestamp"] += 1;
+               }
+               $historyStatusTotal["$midnight_timestamp"] += 1;
+               #echo date('Y-m-d', $timestamp)." issue ".$issue->getId()." status ".$issueStatus."<br>";
+            }
+         }
+/*
+         echo date('Y-m-d', $midnight_timestamp).
+                 ' new '.$historyStatusNew["$midnight_timestamp"].
+                 'feedback '.$historyStatusFeedback["$midnight_timestamp"].
+                 'ongoing '.$historyStatusOngoing["$midnight_timestamp"].
+                 'resolved '.$historyStatusResolved["$midnight_timestamp"].
+                 'total '.$historyStatusTotal["$midnight_timestamp"].
+                 '<br>';
+ */
+      }
+
+      $this->statusData = array(
+          'new'      => $historyStatusNew,
+          'feedback' => $historyStatusFeedback,
+          'ongoing'  => $historyStatusOngoing,
+          'resolved' => $historyStatusResolved,
+          'total'    => $historyStatusTotal);
+
+
+      return $this->statusData;
+   }
+
+
+   /**
+    *
+    *
+    * @param IssueSelection $inputIssueSel
+    * @param array $params
+    */
+   public function execute(IssueSelection $inputIssueSel, array $params = NULL) {
+
+      $this->checkParams($inputIssueSel, $params);
+
+      $startTimestamp = mktime(23, 59, 59, date('m', $params['startTimestamp']), date('d', $params['startTimestamp']), date('Y', $params['startTimestamp']));
+      $endTimestamp   = mktime(23, 59, 59, date('m', $params['endTimestamp']), date('d',$params['endTimestamp']), date('Y', $params['endTimestamp']));
+
+      //echo "StatusHistoryIndicator start ".date('Y-m-d H:i:s', $startTimestamp)." end ".date('Y-m-d H:i:s', $endTimestamp)." interval ".$params['interval']."<br>";
+      $timestampList  = Tools::createTimestampList($startTimestamp, $endTimestamp, $params['interval']);
+
+      $this->getStatusData2($inputIssueSel, $timestampList);
+
+
+
+   }
+
+
+
+   public function getSmartyObject() {
+
+      $historyStatusNew      = $this->statusData['new'];  // timestamp => nbIssues
+      $historyStatusFeedback = $this->statusData['feedback'];
+      $historyStatusOngoing  = $this->statusData['ongoing'];
+      $historyStatusResolved = $this->statusData['resolved'];
+      $historyStatusTotal    = $this->statusData['total'];
+
+
+      $xaxis        = Tools::array2plot(array_keys($historyStatusNew));
+      $jsonNew      = Tools::array2plot(array_values($historyStatusNew));
+      $jsonFeedback = Tools::array2plot(array_values($historyStatusFeedback));
+      $jsonOngoing  = Tools::array2plot(array_values($historyStatusOngoing));
+      $jsonResolved = Tools::array2plot(array_values($historyStatusResolved));
+      $jsonTotal    = Tools::array2plot(array_values($historyStatusTotal));
+
+      #echo $xaxis.'<br>';
+      echo $jsonNew.'<br>';
+      echo $jsonFeedback.'<br>';
+      echo $jsonOngoing.'<br>';
+      echo $jsonResolved.'<br>';
+      echo $jsonTotal.'<br>';
+
+      return array(
+         'xaxis'                 => Tools::array2json(array_keys($historyStatusNew)),
+         'historyStatusNew'      => Tools::array2json(array_values($historyStatusNew)),
+         'historyStatusFeedback' => Tools::array2json(array_values($historyStatusFeedback)),
+         'historyStatusOngoing'  => Tools::array2json(array_values($historyStatusOngoing)),
+         'historyStatusResolved' => Tools::array2json(array_values($historyStatusResolved)),
+         'historyStatusTotal'    => Tools::array2json(array_values($historyStatusTotal)),
+      );
+   }
+
+}
+
+// Initialize complex static variables
+StatusHistoryIndicator::staticInit();
+?>
---
+++ tests/indicator.php
@@ -0,0 +1,82 @@
+<?php
+
+require('../include/session.inc.php');
+/*
+  This file is part of CodevTT
+
+  CodevTT is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  CodevTT is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require ('../path.inc.php');
+
+class indicatorController extends Controller {
+
+   /**
+    * Initialize complex static variables
+    * @static
+    */
+   public static function staticInit() {
+      // Nothing special
+   }
+
+   protected function display() {
+      if (Tools::isConnectedUser()) {
+         $user = UserCache::getInstance()->getUser($_SESSION['userid']);
+
+         $cmd = new Command(1);
+
+
+         $cmdIssueSel = $cmd->getIssueSelection();
+
+         $startTT = $cmdIssueSel->getFirstTimetrack();
+         if ((NULL != $startTT) && (0 != $startTT->getDate())) {
+            $startTimestamp = $startTT->getDate();
+         } else {
+            $startTimestamp = $cmd->getStartDate();
+            #echo "cmd getStartDate ".date("Y-m-d", $startTimestamp).'<br>';
+            if (0 == $startTimestamp) {
+               $team = TeamCache::getInstance()->getTeam($cmd->getTeamid());
+               $startTimestamp = $team->getDate();
+               #echo "team Date ".date("Y-m-d", $startTimestamp).'<br>';
+            }
+         }
+
+         $endTimestamp =  time();
+
+
+         echo "cmd StartDate ".date("Y-m-d", $startTimestamp).'<br>';
+         echo "cmd EndDate ".date("Y-m-d", $endTimestamp).'<br>';
+
+         $params = array(
+            'startTimestamp' => $startTimestamp, // $cmd->getStartDate(),
+            'endTimestamp' => $endTimestamp,
+            'interval' => 7
+         );
+
+         $statusHistoryIndicator = new StatusHistoryIndicator();
+         $statusHistoryIndicator->execute($cmdIssueSel, $params);
+         $obj = $statusHistoryIndicator->getSmartyObject();
+
+
+         #$this->smartyHelper->assign('projects', SmartyTools::getSmartyArray($projList,$projectid));
+      }
+   }
+
+}
+
+// ========== MAIN ===========
+indicatorController::staticInit();
+$controller = new indicatorController('PAGENAME', 'MENU_NAME');
+$controller->execute();
+?>
