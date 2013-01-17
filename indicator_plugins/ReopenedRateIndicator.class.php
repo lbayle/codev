b0eec1e9be93eb36eea1c0f7da47933f70dc9eef
Louis BAYLE <lbayle.work@gmail.com>
2013-01-17 17:30:21 +0100

    EVO ReopenedRateIndicator: add nbResolved

--- indicator_plugins/ReopenedRateIndicator.class.php
+++ indicator_plugins/ReopenedRateIndicator.class.php
@@ -108,15 +108,97 @@ class ReopenedRateIndicator implements IndicatorPlugin {
 
    /**
     *
-    * CmdTotalDrift = Reestimated - (MEE + Provisions)
+    * @param type $formattedBugidList
+    * @param type $startTimestamp
+    * @param type $endTimestamp
+    * @return type
+    */
+   private function getReopened($formattedBugidList, $start, $end) {
+
+      // 1) get all reopened bugs within the timestamp
+      $query = "SELECT bug.*" .
+               "FROM `mantis_bug_table` as bug ".
+               "JOIN `mantis_bug_history_table` as history ON bug.id = history.bug_id " .
+               "WHERE bug.id IN ($formattedBugidList) " .
+               "AND history.field_name='status' " .
+               "AND history.date_modified >= $start AND history.date_modified < $end " .
+               "AND history.old_value >= get_project_resolved_status_threshold(bug.project_id) " .
+               "AND history.new_value <  get_project_resolved_status_threshold(bug.project_id) " .
+               "GROUP BY bug.id ;";
+
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
+      }
+
+      // 2) count reopened and (still) fixed issues at the end of the period
+      $bugidList = array();
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+         $bugidList[] = $row->id;
+      }
+
+      return $bugidList;
+   }
+
+   /**
+    * Returns all Issues resolved in the period and having not been re-opened
+    * 
+    * @param array $timestampList
+    */
+   private function getResolved($formattedBugidList, $start, $end, $withReopened=FALSE) {
+
+      // all bugs which status changed to 'resolved' whthin the timestamp
+      $query = "SELECT bug.* ".
+               "FROM `mantis_bug_table` as bug, `mantis_bug_history_table` as history ".
+               "WHERE bug.id IN ($formattedBugidList) " .
+               "AND bug.id = history.bug_id ".
+               "AND history.field_name='status' ".
+               "AND history.date_modified >= $start AND history.date_modified < $end ".
+               "AND history.new_value = get_project_resolved_status_threshold(project_id) ".
+               "ORDER BY bug.id DESC;";
+
+      $result = SqlWrapper::getInstance()->sql_query($query);
+      if (!$result) {
+         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+         exit;
+      }
+      $resolvedList = array();
+      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+         $issue = IssueCache::getInstance()->getIssue($row->id, $row);
+
+         if (!$withReopened) {
+            // skip if the bug has been reopened before endTimestamp
+            $latestStatus = $issue->getStatus($end);
+            if ($latestStatus < $issue->getBugResolvedStatusThreshold()) {
+               if(self::$logger->isDebugEnabled()) {
+                  $key = date('Y-m-d H:i:s', $end);
+                  self::$logger->debug("getResolved() REOPENED [$key]: bugid = ".$issue->getId().' (excluded)');
+               }
+               continue;
+            }
+         }
+         // remove duplicated values
+         if (!in_array ($issue->getId(), $resolvedList)) {
+            $resolvedList[] = $issue->getId();
+         }
+      }
+      return $resolvedList;
+   }
+
+
+   /**
+    *
+    * 
     *
     *
-    * @param Command $cmd
+    * @param IssueSelection $inputIssueSel
     * @param array $params
     */
    public function execute(IssueSelection $inputIssueSel, array $params = NULL) {
 
       $this->checkParams($inputIssueSel, $params);
+      $this->execData = array();
 
       // -------- elapsed in the period
       $startTimestamp = mktime(0, 0, 0, date('m', $this->startTimestamp), date('d', $this->startTimestamp), date('Y', $this->startTimestamp));
@@ -128,6 +210,8 @@ class ReopenedRateIndicator implements IndicatorPlugin {
 
       $reopenedList = array();
       $nbReopenedList = array();
+      $resolvedList = array();
+      $nbResolvedList = array();
 
       $formattedBugidList = implode(', ', array_keys($inputIssueSel->getIssueList()));
 
@@ -138,48 +222,45 @@ class ReopenedRateIndicator implements IndicatorPlugin {
          $lastDay = ($i + 1 < $size) ? strtotime("-1 day",$timestampList[$i]) : $timestampList[$i];
          $end   = mktime(23, 59, 59, date('m', $lastDay), date('d',$lastDay), date('Y', $lastDay));
 
-         // 1) get all reopened bugs within the timestamp
-         $query = "SELECT bug.*" .
-                  "FROM `mantis_bug_table` as bug ".
-                  "JOIN `mantis_bug_history_table` as history ON bug.id = history.bug_id " .
-                  "WHERE bug.id IN ($formattedBugidList) " .
-                  "AND history.field_name='status' " .
-                  "AND history.date_modified >= $start AND history.date_modified < $end " .
-                  "AND history.old_value >= get_project_resolved_status_threshold(bug.project_id) " .
-                  "AND history.new_value <  get_project_resolved_status_threshold(bug.project_id) " .
-                  "GROUP BY bug.id ;";
+         // -------
 
-         $result = SqlWrapper::getInstance()->sql_query($query);
-         if (!$result) {
-            echo "<span style='color:red'>ERROR: Query FAILED</span>";
-            exit;
-         }
+         $reopenedBugidList = $this->getReopened($formattedBugidList, $start, $end);
+         $resolvedBugidList = $this->getResolved($formattedBugidList, $start, $end);
 
-         // 2) count reopened and (still) fixed issues at the end of the period
-         $bugidList = array();
-         while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
-            $bugidList[] = $row->id;
-         }
+         // ---------
 
          $midnight_timestamp = mktime(0, 0, 0, date('m', $timestampList[$i]), date('d', $timestampList[$i]), date('Y', $timestampList[$i]));
          $key = Tools::formatDate('%Y-%m-%d', $midnight_timestamp);
 
-         $reopenedList[$key] = $bugidList;
-         $nbReopenedList[$key] = count($bugidList);
-         $tableData[$key] = array('reopenedDays' => $nbReopenedList[$key],
-                                  'reopenedPercent' => 0); // TODO
+         $reopenedList[$key] = $reopenedBugidList;
+         $nbReopenedList[$key] = count($reopenedBugidList);
+         $resolvedList[$key] = $resolvedBugidList;
+         $nbResolvedList[$key] = count($resolvedBugidList);
+
+         $tableData[$key] = array('nbReopened' => $nbReopenedList[$key],
+                                  'reopenedPercent' => 0, // TODO
+                                  'nbResolved' => $nbResolvedList[$key],
+                                  'resolvedPercent' => 0, // TODO
+             ); // TODO
 
          #echo "reopened[".date('Y-m-d H:i:s', $midnight_timestamp)."] (".date('Y-m-d H:i:s', $start)." - ".date('Y-m-d H:i:s', $end).") = ".count($bugidList)." reopened : ".implode(', ', $bugidList).'<br>';
       }
 
-      $this->execData = array();
       $this->execData['nbReopened'] = $nbReopenedList;
+      $this->execData['nbResolved'] = $nbResolvedList;
       $this->execData['reopenedIssues'] = $reopenedList;
+      $this->execData['resolvedIssues'] = $resolvedList;
       $this->execData['tableData'] = $tableData;
+
+
 
       return $this->execData;
    }
 
+   /**
+    *
+    * @return array $smartyVariables
+    */
    public function getSmartyObject() {
 
       $smartyVariables = array();
@@ -190,8 +271,10 @@ class ReopenedRateIndicator implements IndicatorPlugin {
       $smartyVariables['reopenedRate_plotMinDate'] = Tools::formatDate("%Y-%m-01", $startTimestamp);
       $smartyVariables['reopenedRate_plotMaxDate'] = Tools::formatDate("%Y-%m-01", $endTimestamp);
       $smartyVariables['reopenedRate_interval']    = ceil($this->interval/20);
-      $smartyVariables['reopenedRate_jqplotDaysData']  = '['.Tools::array2plot($this->execData['nbReopened']).']';
+      $smartyVariables['reopenedRate_jqplotData']  = "[".Tools::array2plot($this->execData['nbResolved']).','.Tools::array2plot($this->execData['nbReopened'])."]";
+
       $smartyVariables['reopenedRate_reopenedIssues']   = $this->execData['reopenedIssues'];
+      $smartyVariables['reopenedRate_resolvedIssues']   = $this->execData['resolvedIssues'];
       $smartyVariables['reopenedRate_tableData']   = $this->execData['tableData'];
 
       return $smartyVariables;
