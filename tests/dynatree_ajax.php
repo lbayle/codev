a27f5e9955232c9712375feedcb6c12754326674
Louis BAYLE <ext.louis.bayle@lotsys.com>
2013-06-20 16:46:24 +0200

    dynatree: load from server (not yet working)

--- tests/dynatree_ajax.php
+++ tests/dynatree_ajax.php
@@ -2,15 +2,35 @@
 require('../include/session.inc.php');
 
 require('../path.inc.php');
-if(Tools::isConnectedUser() && (isset($_GET['action']) || isset($_POST['action']))) {
-   if(isset($_GET['action'])) {
+
+if (Tools::isConnectedUser() && (isset($_GET['action']))) {
+
+   if (isset($_GET['action'])) {
+      
       if($_GET['action'] == 'saveWBS') {
+
          $jsonDynatreeDict = $_GET['jsonDynatreeDict'];
 
          $dynatreeDict = json_decode($jsonDynatreeDict);
          file_put_contents('/tmp/dynatree.txt', serialize($dynatreeDict));
 
          echo $jsonDynatreeDict;
+
+      } else if($_GET['action'] == 'loadWBS') {
+ 
+         $wbsElementId = $_GET['wbsElementId'];
+
+         $serializedDynatreeDict = file_get_contents('/tmp/dynatree.txt');
+	 
+         if (FALSE == $serializedDynatreeDict) { 
+            //echo "file not found!!!";
+            Tools::sendNotFoundAccess(); 
+         } else {
+            $dynatreeDict = unserialize($serializedDynatreeDict);
+            $jsonDynatreeDict = json_encode($dynatreeDict);
+            echo $jsonDynatreeDict;
+         }
+
       } else {
          Tools::sendNotFoundAccess();
       }
