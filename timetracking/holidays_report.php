lbayle/codev

commit 2b2214e389e94b478d00c4ce53e51a5dd1ff6876

HolidaysReport: display 'leave' task summary

timetracking/holidays_report.php
@@ -150,7 +150,7 @@ class HolidaysReportController extends Controller {
                $timestamp = mktime(0,0,0,$month,$i,$year);
 
                if (isset($externalTasks[$timestamp]) && (NULL != $externalTasks[$timestamp])) {
-
+/*
                   if ('Inactivity' == $externalTasks[$timestamp]['type']) {
                      $days[$i] = array(
                         "color" => $externalTasks[$timestamp]['color'],
@@ -159,13 +159,14 @@ class HolidaysReportController extends Controller {
                         "value" => $externalTasks[$timestamp]['duration'],
                      );
                   } elseif ($isExternalTasks) {
+ */
                      $days[$i] = array(
                         "color" => $externalTasks[$timestamp]['color'],
                         "align" => true,
                         "title" => $externalTasks[$timestamp]['title'],
                         "value" => $externalTasks[$timestamp]['duration'],
                      );
-                  }
+                  //}
                } elseif (isset($astreintes[$timestamp]) && (NULL != $astreintes[$timestamp])) {
                   $days[$i] = array(
                      "color" => $astreintes[$timestamp]['color'],

commit 58a9b4b45c712a7b3d9762e3c7cfbca55dcc0d51

FIX REGRESSION HolidaysReport: 'withExternalTasks' checkbox does not work
regression from 2b2214e389e9 display 'leave' task summary

timetracking/holidays_report.php
@@ -32,13 +32,13 @@ class HolidaysReportController extends Controller {
 
    protected function display() {
       if (Tools::isConnectedUser()) {
-          
+
         // only teamMembers & observers can access this page
         if ((0 == $this->teamid) || ($this->session_user->isTeamCustomer($this->teamid))) {
             $this->smartyHelper->assign('accessDenied', TRUE);
             return;
         }
-          
+
          $year = Tools::getSecurePOSTIntValue('year',date('Y'));
 
          $displayed_teamid = 0;
@@ -150,23 +150,18 @@ class HolidaysReportController extends Controller {
                $timestamp = mktime(0,0,0,$month,$i,$year);
 
                if (isset($externalTasks[$timestamp]) && (NULL != $externalTasks[$timestamp])) {
-/*
-                  if ('Inactivity' == $externalTasks[$timestamp]['type']) {
-                     $days[$i] = array(
-                        "color" => $externalTasks[$timestamp]['color'],
-                        "align" => true,
-                        "title" => T_('Inactivity'),
-                        "value" => $externalTasks[$timestamp]['duration'],
-                     );
-                  } elseif ($isExternalTasks) {
- */
+                  // always show inactivity tasks,
+                  // other externalTasks must only be displayed if $isExternalTasks
+                  if (('Inactivity' == $externalTasks[$timestamp]['type']) ||
+                      ($isExternalTasks)) {
+
                      $days[$i] = array(
                         "color" => $externalTasks[$timestamp]['color'],
                         "align" => true,
                         "title" => $externalTasks[$timestamp]['title'],
                         "value" => $externalTasks[$timestamp]['duration'],
                      );
-                  //}
+                  }
                } elseif (isset($astreintes[$timestamp]) && (NULL != $astreintes[$timestamp])) {
                   $days[$i] = array(
                      "color" => $astreintes[$timestamp]['color'],