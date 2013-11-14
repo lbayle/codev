0d01339ebbb1166d4d0c8e9537bf4f955a7bb108
Louis BAYLE <lbayle.trash@gmail.com>
2013-11-15 00:51:42 +0100

    WBS wbs_reader : load via Ajax

--- tests/dynatree_ajax.php
+++ tests/dynatree_ajax.php
@@ -9,41 +9,27 @@ if (Tools::isConnectedUser() && (isset($_GET['action']))) {
 
       if($_GET['action'] == 'saveWBS') {
 
+         $root_id          = $_GET['wbsRootId'];
          $jsonDynatreeDict = $_GET['jsonDynatreeDict'];
+         $nodesToDelete    = $_GET['nodesToDelete'];
 
          $dynatreeDict = json_decode($jsonDynatreeDict);
-         file_put_contents('/tmp/dynatree.txt', serialize($dynatreeDict));
+         //file_put_contents('/tmp/dynatree.txt', serialize($dynatreeDict));
 
-         file_put_contents('/tmp/tree.txt', "=== NEW ".date('Ymd')."\n");
-         //$root_id = WBSElement2::create(NULL, NULL, NULL, NULL, "root_".date('Ymd'));
-			$root_id = 1;
-         $myarray = get_object_vars($dynatreeDict);
-         $rootArray = get_object_vars($myarray['children'][0]);
-         WBSElement2::createTreeFromDynatreeData($rootArray, 1, $root_id, $root_id);
+         file_put_contents('/tmp/tree.txt', "=== NEW ".time()."\n");
+         WBSElement2::createTreeFromDynatreeData($dynatreeDict, 1, $root_id, $root_id);
 
          echo $jsonDynatreeDict;
 
       } else if($_GET['action'] == 'loadWBS') {
 
-         $root_id = $_GET['wbsElementId'];
-			//$root_id = 1;
+         $root_id = $_GET['wbsRootId'];
+         $hasDetail = ('1' == $_GET['hasDetail']) ? true : false;
 
-/*
-         $serializedDynatreeDict = file_get_contents('/tmp/dynatree.txt');
-
-         if (FALSE == $serializedDynatreeDict) {
-            //echo "file not found!!!";
-            Tools::sendNotFoundAccess();
-         } else {
-            $dynatreeDict = unserialize($serializedDynatreeDict);
-            $jsonDynatreeDict = json_encode($dynatreeDict);
-            echo $jsonDynatreeDict;
-         }
-*/
-			file_put_contents('/tmp/getDynatreeData.txt', "=== ".date('Ymd')."\n");
+			//file_put_contents('/tmp/getDynatreeData.txt', "=== ".date('Ymd')."\n");
 			$rootElement = new WBSElement2($root_id);
-			$dynatreeDict = $rootElement->getDynatreeData(true);
-			file_put_contents('/tmp/getDynatreeData.txt', serialize($dynatreeDict)."\n");
+			$dynatreeDict = $rootElement->getDynatreeData($hasDetail);
+			//file_put_contents('/tmp/getDynatreeData.txt', serialize($dynatreeDict)."\n");
 			$jsonDynatreeDict = json_encode($dynatreeDict);
 			echo $jsonDynatreeDict;
 
