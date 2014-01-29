f45136f784b4c8da0aaca0b9fc4fc79342685fc2
Kaiyue WANG <a554472@FR-6QM1CW1.ww930.my-it-solutions.net>
2014-01-29 11:11:32 +0100

    Correction activity indicator pie chart color Correction activity indicator error query failed when no user

--- indicator_plugins/ActivityIndicator/ActivityIndicator.class.php
+++ indicator_plugins/ActivityIndicator/ActivityIndicator.class.php
@@ -119,137 +119,145 @@ class ActivityIndicator implements IndicatorPlugin {
       $bugidList = array_keys($issueList);
       //$formatedBugidString = implode( ', ', array_keys($issueList));
 
-      $query = "SELECT * FROM `codev_timetracking_table` ".
-               "WHERE userid IN (".$formatedUseridString.") ";
-
-      if (isset($this->startTimestamp)) { $query .= "AND date >= $this->startTimestamp "; }
-      if (isset($this->endTimestamp))   { $query .= "AND date <= $this->endTimestamp "; }
-      $query .= " ORDER BY bugid";
-
-      $result = SqlWrapper::getInstance()->sql_query($query);
-      if (!$result) {
-         echo "<span style='color:red'>ERROR: Query FAILED</span>";
-         exit;
-      }
-      $timeTracks = array();
-      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
-         $timeTracks[$row->id] = TimeTrackCache::getInstance()->getTimeTrack($row->id, $row);
-      }
-
-      // ---
-      // un tablean de users avec repartition temps en categories: regular,external,sidetask
-      $teams = array($this->teamid);
-
-      $usersActivity = array();
-      foreach ($timeTracks as $tt) {
-
-         $issueId = $tt->getIssueId();
-         try {
-            $issue = IssueCache::getInstance()->getIssue($issueId);
-         } catch (Exception $e) {
-            self::$logger->error("execute() skip issue $issueId : ".$e->getMessage());
-            continue;
-         }
-         $userid = $tt->getUserId();
-
-         if (!array_key_exists($userid, $usersActivity)) {
-            $usersActivity[$userid] = array();
-         }
-         //$activityList = $usersActivity[$userid];
-
-         $duration = $tt->getDuration();
-         if ($extProjId == $tt->getProjectId()) {
-            #echo "external ".$tt->getIssueId()."<br>";
-            if ($extTasksCatLeave == $issue->getCategoryId()) {
-               if(array_key_exists('leave',$usersActivity[$userid])) {
-                  $usersActivity[$userid]['leave'] += $duration;
-               } else {
-                  $usersActivity[$userid]['leave'] = $duration;
-               }
-            } else {
-               if(array_key_exists('external',$usersActivity[$userid])) {
-                  $usersActivity[$userid]['external'] += $duration;
-               } else {
-                  $usersActivity[$userid]['external'] = $duration;
-               }
-            }
-
-         } else if ($issue->isSideTaskIssue($teams)) {
-            #echo "sidetask ".$tt->getIssueId()."<br>";
-
-            // if sideTask is in the IssueSelection, then it is considered as 'normal',
-            // else it should not be included
-            if (in_array($issueId, $bugidList)) {
-               $cat = $this->showSidetasks ? 'sidetask' : 'elapsed';
-               if(array_key_exists($cat,$usersActivity[$userid])) {
-                  $usersActivity[$userid][$cat] += $duration;
-               } else {
-                  $usersActivity[$userid][$cat] = $duration;
-               }
-            } else {
-               // all sideTasks are in 'other' except inactivity tasks.
-               $project = ProjectCache::getInstance()->getProject($issue->getProjectId());
-               if ($project->getCategory(Project::cat_st_inactivity) == $issue->getCategoryId()) {
-                  if(array_key_exists('leave',$usersActivity[$userid])) {
-                     $usersActivity[$userid]['leave'] += $duration;
-                  } else {
-                     $usersActivity[$userid]['leave'] = $duration;
-                  }
-               } else {
-                  if(array_key_exists('other',$usersActivity[$userid])) {
-                     $usersActivity[$userid]['other'] += $duration;
-                  } else {
-                     $usersActivity[$userid]['other'] = $duration;
-                  }
-               }
-            }
-         } else if (in_array($issueId, $bugidList)) {
-            #echo "selection ".$tt->getIssueId()."<br>";
-            if(array_key_exists('elapsed',$usersActivity[$userid])) {
-               $usersActivity[$userid]['elapsed'] += $duration;
-            } else {
-               $usersActivity[$userid]['elapsed'] = $duration;
-            }
-         } else {
-            #echo "other ".$tt->getIssueId()."<br>";
-            if(array_key_exists('other',$usersActivity[$userid])) {
-               $usersActivity[$userid]['other'] += $duration;
-            } else {
-               $usersActivity[$userid]['other'] = $duration;
-            }
-         }
+      if ($formatedUseridString != NULL && count($formatedUseridString)>0) {
+	      $query = "SELECT * FROM `codev_timetracking_table` ".
+	               "WHERE userid IN (".$formatedUseridString.") ";
+	
+	      if (isset($this->startTimestamp)) { $query .= "AND date >= $this->startTimestamp "; }
+	      if (isset($this->endTimestamp))   { $query .= "AND date <= $this->endTimestamp "; }
+	      $query .= " ORDER BY bugid";
+	
+	      $result = SqlWrapper::getInstance()->sql_query($query);
+	      if (!$result) {
+	         echo "<span style='color:red'>ERROR: Query FAILED</span>";
+	         exit;
+	      }
+	      $timeTracks = array();
+	      while($row = SqlWrapper::getInstance()->sql_fetch_object($result)) {
+	         $timeTracks[$row->id] = TimeTrackCache::getInstance()->getTimeTrack($row->id, $row);
+	      }
+	
+	      // ---
+	      // un tablean de users avec repartition temps en categories: regular,external,sidetask
+	      $teams = array($this->teamid);
+	
+	      $usersActivity = array();
+	      foreach ($timeTracks as $tt) {
+	
+	         $issueId = $tt->getIssueId();
+	         try {
+	            $issue = IssueCache::getInstance()->getIssue($issueId);
+	         } catch (Exception $e) {
+	            self::$logger->error("execute() skip issue $issueId : ".$e->getMessage());
+	            continue;
+	         }
+	         $userid = $tt->getUserId();
+	
+	         if (!array_key_exists($userid, $usersActivity)) {
+	            $usersActivity[$userid] = array();
+	         }
+	         //$activityList = $usersActivity[$userid];
+	
+	         $duration = $tt->getDuration();
+	         if ($extProjId == $tt->getProjectId()) {
+	            #echo "external ".$tt->getIssueId()."<br>";
+	            if ($extTasksCatLeave == $issue->getCategoryId()) {
+	               if(array_key_exists('leave',$usersActivity[$userid])) {
+	                  $usersActivity[$userid]['leave'] += $duration;
+	               } else {
+	                  $usersActivity[$userid]['leave'] = $duration;
+	               }
+	            } else {
+	               if(array_key_exists('external',$usersActivity[$userid])) {
+	                  $usersActivity[$userid]['external'] += $duration;
+	               } else {
+	                  $usersActivity[$userid]['external'] = $duration;
+	               }
+	            }
+	
+	         } else if ($issue->isSideTaskIssue($teams)) {
+	            #echo "sidetask ".$tt->getIssueId()."<br>";
+	
+	            // if sideTask is in the IssueSelection, then it is considered as 'normal',
+	            // else it should not be included
+	            if (in_array($issueId, $bugidList)) {
+	               $cat = $this->showSidetasks ? 'sidetask' : 'elapsed';
+	               if(array_key_exists($cat,$usersActivity[$userid])) {
+	                  $usersActivity[$userid][$cat] += $duration;
+	               } else {
+	                  $usersActivity[$userid][$cat] = $duration;
+	               }
+	            } else {
+	               // all sideTasks are in 'other' except inactivity tasks.
+	               $project = ProjectCache::getInstance()->getProject($issue->getProjectId());
+	               if ($project->getCategory(Project::cat_st_inactivity) == $issue->getCategoryId()) {
+	                  if(array_key_exists('leave',$usersActivity[$userid])) {
+	                     $usersActivity[$userid]['leave'] += $duration;
+	                  } else {
+	                     $usersActivity[$userid]['leave'] = $duration;
+	                  }
+	               } else {
+	                  if(array_key_exists('other',$usersActivity[$userid])) {
+	                     $usersActivity[$userid]['other'] += $duration;
+	                  } else {
+	                     $usersActivity[$userid]['other'] = $duration;
+	                  }
+	               }
+	            }
+	         } else if (in_array($issueId, $bugidList)) {
+	            #echo "selection ".$tt->getIssueId()."<br>";
+	            if(array_key_exists('elapsed',$usersActivity[$userid])) {
+	               $usersActivity[$userid]['elapsed'] += $duration;
+	            } else {
+	               $usersActivity[$userid]['elapsed'] = $duration;
+	            }
+	         } else {
+	            #echo "other ".$tt->getIssueId()."<br>";
+	            if(array_key_exists('other',$usersActivity[$userid])) {
+	               $usersActivity[$userid]['other'] += $duration;
+	            } else {
+	               $usersActivity[$userid]['other'] = $duration;
+	            }
+	         }
+	      }
       }
       #var_dump($usersActivity);
       $this->execData = $usersActivity;
    }
 
    public function getSmartyObject() {
-      $usersActivities = array();
-
-      $totalLeave = 0;
-      $totalExternal = 0;
-      $totalElapsed = 0;
-      $totalOther = 0;
-      $totalSidetask = 0;
-      foreach ($this->execData as $userid => $userActivity) {
-         $user = UserCache::getInstance()->getUser($userid);
-         $usersActivities[$user->getName()] = $userActivity;
-
-         if(array_key_exists('leave',$userActivity)) {
-            $totalLeave += $userActivity['leave'];
-         }
-         if(array_key_exists('external',$userActivity)) {
-            $totalExternal += $userActivity['external'];
-         }
-         if(array_key_exists('elapsed',$userActivity)) {
-            $totalElapsed += $userActivity['elapsed'];
-         }
-         if(array_key_exists('other',$userActivity)) {
-            $totalOther += $userActivity['other'];
-         }
-         if ($this->showSidetasks && array_key_exists('sidetask',$userActivity)) {
-            $totalSidetask += $userActivity['sidetask'];
-         }
+      if ($this->execData != NULL) {
+      	
+	      $usersActivities = array();
+	      	
+	      $totalLeave = 0;
+	      $totalExternal = 0;
+	      $totalElapsed = 0;
+	      $totalOther = 0;
+	      $totalSidetask = 0;
+	      
+	      foreach ($this->execData as $userid => $userActivity) {
+	         $user = UserCache::getInstance()->getUser($userid);
+	         $usersActivities[$user->getName()] = $userActivity;
+	
+	         if(array_key_exists('leave',$userActivity)) {
+	            $totalLeave += $userActivity['leave'];
+	         }
+	         if(array_key_exists('external',$userActivity)) {
+	            $totalExternal += $userActivity['external'];
+	         }
+	         if(array_key_exists('elapsed',$userActivity)) {
+	            $totalElapsed += $userActivity['elapsed'];
+	         }
+	         if(array_key_exists('other',$userActivity)) {
+	            $totalOther += $userActivity['other'];
+	         }
+	         if ($this->showSidetasks && array_key_exists('sidetask',$userActivity)) {
+	            $totalSidetask += $userActivity['sidetask'];
+	         }
+	      }
+	      
+	      ksort($usersActivities);
       }
 
       $totalActivity = array();
@@ -261,8 +269,6 @@ class ActivityIndicator implements IndicatorPlugin {
       if ($this->showSidetasks) {
          $totalActivity['sidetask'] += $totalSidetask;
       }
-
-      ksort($usersActivities);
 
       // pieChart data
       $jqplotData = array(
