b17f0f48db7f35f63b03217627eb2ed953035b54
Louis BAYLE <lbayle.trash@gmail.com>
2014-04-30 00:57:59 +0200

    LoadPerJobIndicator: remove dependency to CommandTools

--- indicator_plugins/LoadPerJobIndicator/LoadPerJobIndicator.class.php
+++ indicator_plugins/LoadPerJobIndicator/LoadPerJobIndicator.class.php
@@ -1,19 +1,19 @@
 <?php
 /*
-   This file is part of CoDev-Timetracking.
+   This file is part of CodevTT
 
-   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   CodevTT is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
-   CoDev-Timetracking is distributed in the hope that it will be useful,
+   CodevTT is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+   along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /**
