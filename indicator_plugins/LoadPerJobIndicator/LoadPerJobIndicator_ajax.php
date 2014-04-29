5a678f5b186f47d476607fec37bd4da4ec1d01df
Louis BAYLE <lbayle.trash@gmail.com>
2014-04-29 22:43:27 +0200

    LoadPerJobIndicator: move ajax php code to indicator_plugins dir
    command_info_ajax.php must NOT contain any IndicatorPlugin specific code !
    
    each IndicatorPlugin should implement getAjaxPhpFilename()

---
+++ indicator_plugins/LoadPerJobIndicator/LoadPerJobIndicator_ajax.php
@@ -0,0 +1,66 @@
+<?php
+require('../../include/session.inc.php');
+
+/*
+   This file is part of CoDev-Timetracking.
+
+   CoDev-Timetracking is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   CoDev-Timetracking is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with CoDev-Timetracking.  If not, see <http://www.gnu.org/licenses/>.
+*/
+require('../../path.inc.php');
+
+// Note: i18n is included by the Controler class, but Ajax dos not use it...
+require_once('i18n/i18n.inc.php');
+
+if(Tools::isConnectedUser() && (isset($_GET['action']) || isset($_POST['action']))) {
+   if(isset($_GET['action'])) {
+#echo "action = ".$_GET['action']."<br>";
+      $smartyHelper = new SmartyHelper();
+      if($_GET['action'] == 'getLoadPerJobIndicator') {
+         if(isset($_SESSION['cmdid'])) {
+            $cmdid = $_SESSION['cmdid'];
+            if (0 != $cmdid) {
+               $cmd = CommandCache::getInstance()->getCommand($cmdid);
+
+               $startTimestamp = Tools::date2timestamp(Tools::getSecureGETStringValue("loadPerJob_startdate"));
+               $endTimestamp = Tools::date2timestamp(Tools::getSecureGETStringValue("loadPerJob_enddate"));
+               $data = CommandTools::getLoadPerJob($cmd, $startTimestamp, $endTimestamp);
+
+               // construct the html table
+               foreach ($data as $smartyKey => $smartyVariable) {
+                  $smartyHelper->assign($smartyKey, $smartyVariable);
+                  #echo "key $smartyKey = ".var_export($smartyVariable, true)."<br>";
+               }
+               $html = $smartyHelper->fetch(LoadPerJobIndicator::getSmartySubFilename());
+               $data['loadPerJob_htmlTable'] = $html;
+
+               // return htùml & chart data
+               $jsonData = json_encode($data);
+               echo $jsonData;
+
+            } else {
+               Tools::sendBadRequest("Command equals 0");
+            }
+         } else {
+            Tools::sendBadRequest("Command not set");
+         }
+
+      } else {
+         Tools::sendNotFoundAccess();
+      }
+   }
+} else {
+   Tools::sendUnauthorizedAccess();
+}
+
+?>
--- management/command_info_ajax.php
+++ management/command_info_ajax.php
@@ -63,34 +63,6 @@ if(Tools::isConnectedUser() && (isset($_GET['action']) || isset($_POST['action']
          } else {
             Tools::sendBadRequest("Command not set");
          }
-      } else if($_GET['action'] == 'getLoadPerJobIndicator') {
-         if(isset($_SESSION['cmdid'])) {
-            $cmdid = $_SESSION['cmdid'];
-            if (0 != $cmdid) {
-               $cmd = CommandCache::getInstance()->getCommand($cmdid);
-
-               $startTimestamp = Tools::date2timestamp(Tools::getSecureGETStringValue("loadPerJob_startdate"));
-               $endTimestamp = Tools::date2timestamp(Tools::getSecureGETStringValue("loadPerJob_enddate"));
-               $data = CommandTools::getLoadPerJob($cmd, $startTimestamp, $endTimestamp);
-
-               // construct the html table
-               $smartyHelper->assign('workingDaysPerJob', $data['workingDaysPerJob']);
-               $smartyHelper->assign('loadPerJob_startDate', Tools::formatDate("%Y-%m-%d", $data['workingDaysPerJob_startTimestamp']));
-               $smartyHelper->assign('loadPerJob_endDate', Tools::formatDate("%Y-%m-%d", $data['workingDaysPerJob_endTimestamp']));
-               $html = $smartyHelper->fetch(LoadPerJobIndicator::getSmartySubFilename());
-               $data['loadPerJob_htmlTable'] = $html;
-
-               // return htùml & chart data
-               $jsonData = json_encode($data);
-               echo $jsonData;
-
-            } else {
-               Tools::sendBadRequest("Command equals 0");
-            }
-         } else {
-            Tools::sendBadRequest("Command not set");
-         }
-
       } else if($_GET['action'] == 'getProgressHistoryIndicator') {
          if(isset($_SESSION['cmdid'])) {
             $cmdid = $_SESSION['cmdid'];
