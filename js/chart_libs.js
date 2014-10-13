ff024a35d47213bd18f51bd0b1a12e317b3fedba
Louis BAYLE <lbayle.trash@gmail.com>
2014-10-13 21:29:42 +0200

    Dashboard ajax: add plugin (not yet finished)

---
+++ js/chart_libs.js
@@ -0,0 +1,73 @@
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/jquery.jqplot.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.dateAxisRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.cursor.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.pointLabels.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.highlighter.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.pieRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.canvasAxisLabelRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.canvasTextRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.canvasAxisTickRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.categoryAxisRenderer.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "lib/jquery.jqplot/plugins/jqplot.canvasOverlay.min.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+   jQuery.ajax({
+      url: "js/chart.js",
+      dataType: "script",
+      async: false,
+      cache: true
+   });
+
