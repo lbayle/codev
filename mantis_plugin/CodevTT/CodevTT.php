2955bed0abbedca5946a2cdc2344ba15f407a924
Louis BAYLE <lbayle.trash@gmail.com>
2014-10-19 01:00:51 +0200

    FIX Mantis Plugin: update SQL API
    using mysql_ is unsafe.
    
    contributed by: Paul Richards (MantisBT team)

--- mantis_plugin/CodevTT/CodevTT.php
+++ mantis_plugin/CodevTT/CodevTT.php
@@ -57,8 +57,8 @@ class CodevTTPlugin extends MantisPlugin {
       );
 
       # contributed to MantisBT 1.3
-      if (!is_null($g_event_cache['EVENT_MANAGE_PROJECT_DELETED'])) {
-         $hooks['EVENT_MANAGE_PROJECT_DELETED'] = 'projectDeleted';
+      if (!is_null($g_event_cache['EVENT_MANAGE_PROJECT_DELETE'])) {
+         $hooks['EVENT_MANAGE_PROJECT_DELETE'] = 'projectDelete';
       }
 
       return $hooks;
@@ -70,80 +70,71 @@ class CodevTTPlugin extends MantisPlugin {
     * @param array $t_bug_data
     */
    public function assignCommand($event, $t_bug_data) {
-
       #$command_ids = gpc_get_int_array( 'command_id');
-
 
       $t_bug_id = $t_bug_data->id;
 
       // delete all existing bug-command associations
       if ($event != 'EVENT_REPORT_BUG_FORM') {
-         $delete_query = "DELETE FROM `codev_command_bug_table` WHERE `bug_id` = '$t_bug_id';";
-         $delete_result = mysql_query($delete_query) or exit(mysql_error());
+         $delete_query = "DELETE FROM codev_command_bug_table WHERE bug_id=" . db_param();
+         $delete_result = db_query_bound($delete_query, array( $t_bug_id ));
       }
 
       // === create bug-command associations
       if (isset($_POST['command_id'])) {
-
          $command_ids = $_POST['command_id'];
 
          $query = "INSERT INTO `codev_command_bug_table` (`command_id`, `bug_id`) VALUES";
          $separator = "";
          //TODO test if command id is valid !!!!
          foreach ($command_ids as $command_id) {
-            #error_log ("ForEach: $command_id => $t_bug_id");
-            $query = $query . $separator . " ('$command_id', '$t_bug_id')";
+            $query = $query . $separator . " (" . db_param() . ", " . db_param() . ")";
             $separator = ",";
          }
          $query = $query . ";";
-         #error_log ("Query: $query");
-         $result = mysql_query($query) or exit(mysql_error());
+         $result = db_query_bound($query, array( $command_id, $t_bug_id ) );
 
          // === add to WBS
          // 1) get the wbs_id of this command
-         $query2 = "SELECT name, wbs_id FROM  `codev_command_table` WHERE `id` = '$command_id' ";
-         #echo "SQL query2 = $query2<br>";
-         $result2 = mysql_query($query2) or exit(mysql_error());
-         $row2 = mysql_fetch_object($result2);
-         $wbs_id = $row2->wbs_id;
-         $cmd_name = $row2->name;
+         $query2 = "SELECT name, wbs_id FROM codev_command_table WHERE id = " . db_param();
+         $result2 = db_query_bound($query2, array( $command_id ));
+		 $row2 = db_fetch_array( $result2 );
+         $wbs_id = $row2['wbs_id'];
+         $cmd_name = $row2['name'];
 
-         // 2) if wbs_id is null, the the root element must be created
+         // 2) if wbs_id is null, the root element must be created
          // (this happens only once when upgrading from 0.99.24 or below)
          $order = 1;
          if (is_null($wbs_id)) {
             #echo "Create WBS root element for Command $command_id<br>";
             // add root element
-            $query3 = "INSERT INTO `codev_wbs_table`  (`order`, `expand`, `title`) ".
-                    "VALUES ('1', '1', '$cmd_name')";
-            #echo "SQL query3 = $query3<br>";
-            $result3 = mysql_query($query3) or exit(mysql_error());
-            $wbs_id = mysql_insert_id();
+            $query3 = "INSERT INTO codev_wbs_table  (`order`, `expand`, `title`) ".
+                    "VALUES (" . db_param() . ", " . db_param() . ", " . db_param() . ")";
+			$result3 = db_query_bound($query3, array( 1, 1, $cmd_name ));
+            $wbs_id = db_insert_id();
 
-            $query4 = "UPDATE `codev_command_table` SET wbs_id = '$wbs_id' WHERE id = '$command_id';";
-            #echo "SQL query4 = $query4<br>";
-            $result4 = mysql_query($query4) or exit(mysql_error());
+            $query4 = "UPDATE codev_command_table SET wbs_id = " . db_param() . " WHERE id = " . db_param();
+            $result4 = db_query_bound($query4, array( $wbs_id, $command_id ));
 
             // 2.1) add all existing issues to the WBS
-            $query6 = "SELECT bug_id from `codev_command_bug_table` WHERE command_id = '$command_id' ORDER BY bug_id ;";
-            #echo "SQL query6 = $query6<br>";
-            $result6 = mysql_query($query6) or exit(mysql_error());
-            while ($row6 = mysql_fetch_object($result6)) {
+            $query6 = "SELECT bug_id from codev_command_bug_table WHERE command_id = " . db_param() . " ORDER BY bug_id";
+            $result6 = db_query_bound($query6, array( $command_id));
+            while ($row6 = db_fetch_array( $result6 )) {
                #echo "add issue $row6->bug_id to command $command_id<br>";
-               $query7 = "INSERT INTO `codev_wbs_table`  (`root_id`, `parent_id`, `bug_id`, `order`, `expand`) ".
-                       "VALUES ('$wbs_id', '$wbs_id', '$row6->bug_id', '$order', '0')";
+               $query7 = "INSERT INTO codev_wbs_table  (`root_id`, `parent_id`, `bug_id`, `order`, `expand`) ".
+                       "VALUES (" . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ")";
                #echo "SQL query7 = $query7<br>";
-               $result7 = mysql_query($query7) or exit(mysql_error());
+               $result7 = db_query_bound($query7, array( $wbs_id, $wbs_id, $row6['bug_id'], $order, 0));
                $order += 1;
             }
 
 
          } else {
             // 3) add bug_id to the wbs root element
-            $query5 = "INSERT INTO `codev_wbs_table`  (`root_id`, `parent_id`, `bug_id`, `order`, `expand`) ".
-                    "VALUES ('$wbs_id', '$wbs_id', '$t_bug_id', '$order', '0')";
+            $query5 = "INSERT INTO codev_wbs_table  (`root_id`, `parent_id`, `bug_id`, `order`, `expand`) ".
+                    "VALUES (" . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ")";
             #echo "SQL query5 = $query5<br>";
-            $result5 = mysql_query($query5) or exit(mysql_error());
+            $result5 = db_query_bound($query5, array( $wbs_id, $wbs_id, $t_bug_id, $order, 0 ));
          }
 
       }
@@ -151,17 +142,16 @@ class CodevTTPlugin extends MantisPlugin {
 
    public function update_bug_form($event, $t_bug_id) {
 
-      $assigned_query = "SELECT `command_id` FROM `codev_command_bug_table` WHERE `bug_id` = '$t_bug_id'";
-      $assigned_request = mysql_query($assigned_query) or exit(mysql_error());
+      $assigned_query = "SELECT `command_id` FROM `codev_command_bug_table` WHERE `bug_id` = " . db_param();
+      $assigned_request = db_query_bound($assigned_query, array( $t_bug_id ));
       $assigned_commands = array();
       $index = 0;
-      while ($row = mysql_fetch_assoc($assigned_request)) {
+      while ($row = db_fetch_array( $assigned_request )) {
          $assigned_commands[$index++] = $row['command_id'];
       }
-      mysql_free_result($assigned_request);
 
       $query = "SELECT `id`, `name`, `reference`, `team_id` FROM `codev_command_table` ORDER BY `reference`,`name`";
-      $command_request = mysql_query($query) or exit(mysql_error());
+      $command_request = db_query_bound($query);
       //TODO filter with team id
       echo'
             <tr ';
@@ -172,7 +162,7 @@ class CodevTTPlugin extends MantisPlugin {
       echo'</td>
             <td>
             <select multiple="multiple"  size="5" name="command_id[]">';
-      while ($command_array = mysql_fetch_assoc($command_request)) {
+      while ($command_array = db_fetch_array($command_request)) {
          echo '<option value="' . $command_array['id'] . '"';
          if (in_array($command_array['id'], $assigned_commands)) {
             echo ' selected="selected"';
@@ -183,7 +173,6 @@ class CodevTTPlugin extends MantisPlugin {
             </td>
             </tr>
             ';
-      mysql_free_result($command_request);
    }
 
    /**
@@ -200,18 +189,18 @@ class CodevTTPlugin extends MantisPlugin {
       $query = "SELECT DISTINCT codev_team_table.id, codev_team_table.name " .
                "FROM `codev_team_user_table`, `codev_team_table` " .
                "WHERE codev_team_user_table.team_id = codev_team_table.id ".
-               "AND   codev_team_user_table.user_id = $userid ";
+               "AND   codev_team_user_table.user_id = " . db_param();
       
       // only teams where project is defined
-      $query .= "AND 1 = is_project_in_team($project_id, codev_team_table.id) ";
+      $query .= "AND 1 = is_project_in_team(" . (int)$project_id . ", codev_team_table.id) ";
 
       $query .= "ORDER BY codev_team_table.name";
 
-      $result = mysql_query($query) or exit(mysql_error());
+      $result = db_query_bound($query, array( $userid));
       $teamidList = array();
-      while ($row = mysql_fetch_object($result)) {
-         $teamidList[] = $row->id;
-         #echo "getAvailableCommands() FOUND $row->id - $row->name<br/>";
+      while ($row = db_fetch_array($result)) {
+         $teamidList[] = $row['id'];
+         #echo "getAvailableCommands() FOUND $row['id'] - $row['name']<br/>";
       }
 
       // find team Commands
@@ -227,14 +216,12 @@ class CodevTTPlugin extends MantisPlugin {
          
          $query .= "ORDER BY reference, name";
          
-         $result = mysql_query($query) or exit(mysql_error());
+         $result = db_query_bound($query);
          $cmdList = array();
-         while ($row = mysql_fetch_object($result)) {
-            $cmdList[$row->id] = "$row->reference :: $row->name";
+         while ($row = db_fetch_array($result)) {
+            $cmdList[$row['id']] = $row['reference'] . " :: " . $row['name'];
          }
       }
-
-      mysql_free_result($result);
       return $cmdList;
    }
 
@@ -286,14 +273,14 @@ class CodevTTPlugin extends MantisPlugin {
 //select codev_command_table.* from codev_command_bug_table, codev_command_table where bug_id = '358' and codev_command_bug_table.command_id = codev_command_table.id
 
       $query  = "SELECT codev_command_table.* FROM `codev_command_bug_table`, `codev_command_table` ".
-                 "WHERE codev_command_bug_table.bug_id=$t_bug_id ".
+                 "WHERE codev_command_bug_table.bug_id=" . db_param() . " " .
                  "AND codev_command_table.id = codev_command_bug_table.command_id ".
                  "ORDER BY codev_command_table.name";
 
-      $result = mysql_query($query) or exit(mysql_error());
+      $result = db_query_bound($query, array( $t_bug_id ));
       $commandList = array();
-      while ($row = mysql_fetch_object($result)) {
-         $commandList[$row->id] = "$row->reference - $row->name";
+      while ($row = db_fetch_array( $result )) {
+         $commandList[$row['id']] = $row['reference'] . " - " . $row['name'];
       }
       if (0 != count($commandList)) {
 
@@ -317,13 +304,13 @@ class CodevTTPlugin extends MantisPlugin {
       $query = "SELECT codev_timetracking_table.date, codev_timetracking_table.userid, codev_timetracking_table.duration, ".
               "mantis_user_table.username, mantis_user_table.realname ".
               "FROM `codev_timetracking_table`, `mantis_user_table` ".
-              "WHERE codev_timetracking_table.bugid = '$bug_id' ".
+              "WHERE codev_timetracking_table.bugid = " . db_param() . " ".
               "AND codev_timetracking_table.userid = mantis_user_table.id ";
 
       $errMsg = "";
-      $result = mysql_query($query) or exit(mysql_error());
-      while ($row = mysql_fetch_object($result)) {
-         $errMsg .= date('Y-m-d',$row->date)." - $row->username ($row->realname) - duration $row->duration <br>";
+      $result = db_query_bound($query, array( $bug_id ) );
+      while ($row = db_fetch_array( $result )) {
+         $errMsg .= date('Y-m-d',$row['date'])." - " . $row['username'] . " (" . $row['realname'] . ") - duration " . $row['duration'] . " <br>";
       }
 
       if ("" != $errMsg) {
@@ -339,11 +326,11 @@ class CodevTTPlugin extends MantisPlugin {
 
       // if status changed to 'resolved' then set Backlog = 0
       #$query = "SELECT COUNT(id) FROM `mantis_bug_table` WHERE id = $bug_data->id AND status >= get_issue_resolved_status_threshold($bug_data->id)";
-      $query = "SELECT COUNT(id) FROM `mantis_bug_table` WHERE id = $bug_data->id AND $bug_data->status = get_issue_resolved_status_threshold($bug_data->id)";
-      $result = mysql_query($query) or exit(mysql_error());
-      $count = mysql_result($result, 0);
+      $query = "SELECT COUNT(id) as cnt FROM `mantis_bug_table` WHERE id = " . db_param() . " AND " . db_param() . " = get_issue_resolved_status_threshold(" . db_param() . ")";
+      $result = db_query_bound($query, array( $bug_data->id, $bug_data->status, $bug_data->id) );
+      $row = db_fetch_array( $result );
 
-      if ($count) {
+      if ($row) {
          // update backlog
          try {
             $issue = new IssueMantisPluginHelper($bug_data->id);
@@ -357,24 +344,24 @@ class CodevTTPlugin extends MantisPlugin {
    }
 
    /**
-    * Forbid project deletion if timetracks exists of project issues.
+    * Forbid project deletion if timetracks exists on project issues.
     *
     * @param type $event
     * @param type $project_id
     */
-   public function projectDeleted($event, $project_id) {
+   public function projectDelete($event, $project_id) {
 
       $query = "SELECT codev_timetracking_table.bugid, codev_timetracking_table.date, codev_timetracking_table.userid, codev_timetracking_table.duration, ".
               "mantis_user_table.username, mantis_user_table.realname ".
               "FROM `codev_timetracking_table`, `mantis_user_table` ".
               "WHERE codev_timetracking_table.userid = mantis_user_table.id ".
               "AND codev_timetracking_table.bugid IN (".
-              "   SELECT id FROM mantis_bug_table WHERE project_id = '$project_id')";
+              "   SELECT id FROM mantis_bug_table WHERE project_id = " . db_param() . ")";
 
       $errMsg = "";
-      $result = mysql_query($query) or exit(mysql_error());
-      while ($row = mysql_fetch_object($result)) {
-         $errMsg .= 'Issue '.$row->bugid.': '.date('Y-m-d',$row->date)." - $row->username ($row->realname) - duration $row->duration <br>";
+      $result = db_query_bound($query, array( $project_id ) );
+      while ($row = db_fetch_array( $result )) {
+         $errMsg .= 'Issue '.$row['bugid'].': '.date('Y-m-d',$row['date'])." - " . $row['username'] . " (" . $row['realname'] . ") - duration " . $row['duration'] ." <br>";
       }
 
       if ("" != $errMsg) {
--- mantis_plugin/CodevTT/classes/IssueMantisPluginHelper.php
+++ mantis_plugin/CodevTT/classes/IssueMantisPluginHelper.php
@@ -46,59 +46,51 @@ class IssueMantisPluginHelper {
 
       $old_backlog = NULL;
       
-      $query = "SELECT value FROM `codev_config_table` WHERE config_id = '".IssueMantisPluginHelper::id_customField_backlog."'";
-      $result = mysql_query($query);
-      if (!$result) {
-         $e = new Exception("Query FAILED: ".$query);
-         throw $e;
-      }
-      $backlogCustomField    = (0 != mysql_num_rows($result)) ? mysql_result($result, 0) : 0;
+      $query = "SELECT value FROM codev_config_table WHERE config_id = ". db_param();
+      $result = db_query_bound($query, array( IssueMantisPluginHelper::id_customField_backlog ));
+	  $row = db_fetch_array( $result );
+
+	  if( $row )
+		$backlogCustomField    = $row['value'];
+	  else
+	    $backlogCustomField    = 0;
 
       // TODO should be done only once...
-      $query = "SELECT name FROM `mantis_custom_field_table` WHERE id = '$backlogCustomField'";
-      $result = mysql_query($query);
-      if (!$result) {
-         $e = new Exception("Query FAILED: ".$query);
-         throw $e;
-      }
-      $field_name    = (0 != mysql_num_rows($result)) ? mysql_result($result, 0) : "Backlog (BL)";
+      $query = "SELECT name FROM mantis_custom_field_table WHERE id = " . db_param();
+      $result = db_query_bound($query, array($backlogCustomField));
+      $row = db_fetch_array( $result );
+
+	  if( $row )
+        $field_name    = $row['name'];
+      else
+	    $field_name = "Backlog (BL)";
 
       // check if backlog already defined for this issue
-      $query = "SELECT value FROM `mantis_custom_field_string_table` WHERE bug_id=$this->id AND field_id = '$backlogCustomField'";
-      $result = mysql_query($query);
-      if (!$result) {
-         $e = new Exception("Query FAILED: ".$query);
-         throw $e;
-      }
-      if (0 != mysql_num_rows($result)) {
-         $old_backlog = mysql_result($result, 0);
-         $query2 = "UPDATE `mantis_custom_field_string_table` SET value = '$backlog' WHERE bug_id=$this->id AND field_id = $backlogCustomField";
+      $query = "SELECT value FROM `mantis_custom_field_string_table` WHERE bug_id=" . db_param() . " AND field_id = " . db_param();
+      $result = db_query_bound($query, array( $this->id, $backlogCustomField ) );
+      $row = db_fetch_array( $result );
+	  
+      if ($row) {
+         $old_backlog = $row['value'];
+         $query2 = "UPDATE mantis_custom_field_string_table SET value = " . db_param() . " WHERE bug_id=" . db_param() . " AND field_id = " . db_param();
+		 $result2 = db_query_bound($query2, array($backlog, $this->id,$backlogCustomField));
       } else {
          $old_backlog = '';
-         $query2 = "INSERT INTO `mantis_custom_field_string_table` (`field_id`, `bug_id`, `value`) VALUES ('$backlogCustomField', '$this->id', '$backlog');";
+         $query2 = "INSERT INTO mantis_custom_field_string_table (`field_id`, `bug_id`, `value`) VALUES (" . db_param() . ", " . db_param() . ", " . db_param() . ")";
+		 $result2 = db_query_bound($query2, array( $backlogCustomField, $this->id, $backlog ));
       }
-      $result2 = mysql_query($query2);
-      if (!$result2) {
-         $e = new Exception("Query FAILED: ".$query);
-         throw $e;
-      }
+      
+
       // Add to history
       if ("$old_backlog" != "$backlog") {
          $userid = current_user_get_field( 'id' );
          $now = time();
-         $query = "INSERT INTO `mantis_bug_history_table`  (`user_id`, `bug_id`, `field_name`, `old_value`, `new_value`, `type`, `date_modified`) ".
-                  "VALUES ('$userid','$this->id','$field_name', '$old_backlog', '$backlog', '0', '".$now."');";
-         $result = mysql_query($query);
-         if (!$result) {
-            $e = new Exception("Query FAILED: ".$query);
-            throw $e;
-         }
+         $query = "INSERT INTO mantis_bug_history_table  (`user_id`, `bug_id`, `field_name`, `old_value`, `new_value`, `type`, `date_modified`) ".
+                  "VALUES (" . db_param() . "," . db_param() . "," . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ", " . db_param() . ")";
+         $result = db_query_bound($query, array( $userid, $this->id, $field_name, $old_backlog, $backlog, 0, $now  ));
       }
       // no need to update lastUpdated field
    }
-
-
-
 }
 
 ?>
