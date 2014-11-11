4d47bceec83f3101c77e55b1830facd0505aade7
Louis BAYLE <lbayle.trash@gmail.com>
2014-11-11 21:15:05 +0100

    code cleanup

--- plugins/HelloWorldIndicator/HelloWorldIndicator.class.php
+++ plugins/HelloWorldIndicator/HelloWorldIndicator.class.php
@@ -53,7 +53,7 @@ class HelloWorldIndicator extends IndicatorPluginAbstract {
          self::DOMAIN_TEAM,
          self::DOMAIN_USER,
          self::DOMAIN_PROJECT,
-         self::DOMAIN_MACRO_COMMAND,
+         self::DOMAIN_COMMAND_SET,
          self::DOMAIN_SERVICE_CONTRACT,
       );
       self::$categories = array (
