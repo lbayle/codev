6c6ef619de30635f1c3e9519d5d9806341625463
Louis BAYLE <lbayle.trash@gmail.com>
2014-11-11 02:14:29 +0100

    Plugins: PARAM_INTERVAL / OPTION_INTERVAL

--- plugins/ReopenedRateIndicator2/ReopenedRateIndicator2.class.php
+++ plugins/ReopenedRateIndicator2/ReopenedRateIndicator2.class.php
@@ -23,7 +23,7 @@
  */
 class ReopenedRateIndicator2 extends IndicatorPluginAbstract {
 
-   const OPTION_INTERVAL = 'interval';
+   const OPTION_INTERVAL = 'interval'; // defaultValue, oneWeek, twoWeeks, oneMonth
 
    private static $logger;
    private static $domains;
@@ -131,6 +131,11 @@ class ReopenedRateIndicator2 extends IndicatorPluginAbstract {
       } else {
          throw new Exception("Missing parameter: ".PluginDataProviderInterface::PARAM_END_TIMESTAMP);
       }
+      if (NULL != $pluginDataProv->getParam(PluginDataProviderInterface::PARAM_INTERVAL)) {
+         $this->interval = $pluginDataProv->getParam(PluginDataProviderInterface::PARAM_INTERVAL);
+      } else {
+         $this->interval = 30;
+      }
 
       $bugidList = array_keys($this->inputIssueSel->getIssueList());
       $this->formatedBugidList = implode(', ', $bugidList);
@@ -141,7 +146,6 @@ class ReopenedRateIndicator2 extends IndicatorPluginAbstract {
 
 
       // set default pluginSettings (not provided by the PluginDataProvider)
-      $this->interval = 7;
 
    }
 
@@ -155,7 +159,19 @@ class ReopenedRateIndicator2 extends IndicatorPluginAbstract {
       if (NULL != $pluginSettings) {
          // override default with user preferences
          if (array_key_exists(self::OPTION_INTERVAL, $pluginSettings)) {
-            $this->interval = intval($pluginSettings[self::OPTION_INTERVAL]);
+            switch ($pluginSettings[self::OPTION_INTERVAL]) {
+               case 'oneWeek':
+                  $this->interval = 7;
+                  break;
+               case 'twoWeeks':
+                  $this->interval = 14;
+                  break;
+               case 'oneMonth':
+                  $this->interval = 30;
+                  break;
+               default:
+                  self::$logger->warn('option '.self::OPTION_INTERVAL.'= '.$pluginSettings[self::OPTION_INTERVAL]." (unknown value)");
+            }
          }
       }
    }
