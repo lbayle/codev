f6f525c5db8425fcdad630097dc449e2966fc39a
Louis BAYLE <lbayle.trash@gmail.com>
2015-01-21 14:46:42 +0100

    cosmetic: 1.0.2

--- classes/config.class.php
+++ classes/config.class.php
@@ -67,8 +67,8 @@ class ConfigItem {
 class Config {
 
    // TODO Move to a more appropriate class
-   const codevVersion = "v1.0.1";
-   const codevVersionDate = "16 Aug 2014";
+   const codevVersion = "v1.0.2";
+   const codevVersionDate = "21 Jan 2015";
    const databaseVersion = 13; // used to check codev_config_table.database_version and apply upgrades.
 
    const configType_int = 1;
@@ -150,6 +150,7 @@ class Config {
       "v1.0.0 RC1" => "(13 Jan  2014) - WBS, install, log file creation if missing",
       "v1.0.0" => "(17 Mar  2014) - Relationships, timetrack duration, check_latest_version, CmdStateFilter, periodic leaves",
       "v1.0.1" => "(16 Aug  2014) - fix:Gantt+CSVimport+graphColors+WBSrootName, Hide admin menu, install:proxy+checkMantisDefaultProjectWorkflow",
+      "v1.0.2" => "(21 Jan  2015) - fix:WBS+more, MantisPlugin, FilterBugList, Jobs, Commands:totalProv, MinifyJS, EditTeam:combobox, Logs, Holidays:filters, ExportCSV",
    );
 
    /**
