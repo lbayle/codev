eb1a3c0d649ebb7ced96bdd74933118853a6365e
Louis BAYLE <lbayle.trash@gmail.com>
2016-01-14 15:20:12 +0100

    [m1352] menu: no more hover in dropdown menus (unusable on tablets)
    close menu

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,13 +1,33 @@
 // Dropdown for menu
 jQuery(document).ready(function() {
-   jQuery("ul.dropdown li").hover(function() {
-      //jQuery(this).addClass("hover");
-      jQuery('ul:first',this).css('visibility', 'visible');
-    }, function() {
-      jQuery(this).removeClass("hover");
-      jQuery('ul:first',this).css('visibility', 'hidden');
-    }
-   );
 
-   jQuery("ul.submenu li:has(ul)").find("a:first").append("&raquo;");
+   var submenus = jQuery("ul.dropdown li");
+
+   submenus.click(function(ev){
+
+      if ($(this).hasClass('dropdown-li-opened')) {
+         // close current menu
+         $(this).removeClass('dropdown-li-opened')
+      } else {
+         submenus.removeClass('dropdown-li-opened');
+         $(this).addClass('dropdown-li-opened');
+      }
+   });
+
+   jQuery(document).on('click', function(ev) {
+      // check if the click is of an element of a dropdown menu
+      var $menu = $(ev.target).parents('.dropdown'); 
+      if ($menu.length === 0) { 
+         submenus.removeClass('dropdown-li-opened'); 
+      } 
+   }); 
+
+   // la salope de librarie jquery-ui-tabs fait un 'return false' lors du click sur un tab,
+   // ce qui annulle le event bubbeling et le mecanisme de fermeture du submenu.
+   // => on s'abonne a l'evenenent du changement d'onglet pour simuler le comportement du click...
+   jQuery('.tabs').on('tabsselect', function(ev) {
+      submenus.removeClass('dropdown-li-opened'); 
+   }); 
+
+
 });
