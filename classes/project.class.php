lbayle/codev

commit 33d6c8e57c2b82d7671aa8dfe5f033f2e1ab09a4

[m1601] Mantis v1.3: use json_decode for mantis_config_table
serialized() is replaced with json_encore()

classes/project.class.php
@@ -1108,14 +1108,18 @@ class Project extends Model {
       $serialized = $row->value;
 
       if ((NULL == $serialized) || ("" == $serialized)) {
-         if(self::$logger->isDebugEnabled()) {
-            self::$logger->debug("Bad workflow defined for project $this->id");
-         }
+         self::$logger->error("Bad workflow defined for project $this->id");
          return NULL;
       }
 
-      $unserialized = unserialize($serialized);
-
+      if (Tools::isMantisV1_3()) {
+         $unserialized = json_decode( $serialized, true );
+      } else {
+         $unserialized = unserialize($serialized);
+      }
+      if (NULL == $unserialized) {
+         self::$logger->error("getWorkflowTransitions: Could not unserialize status_enum_workflow: ".$serialized);
+      }
       return $unserialized;
    }
 
@@ -1498,7 +1502,11 @@ class Project extends Model {
          if (0 != SqlWrapper::getInstance()->sql_num_rows($result)) {
             $serialized = SqlWrapper::getInstance()->sql_result($result, 0);
 
-            $unserialized = unserialize($serialized);
+            if (Tools::isMantisV1_3()) {
+               $unserialized = json_decode( $serialized, true );
+            } else {
+               $unserialized = unserialize($serialized);
+            }
             $this->issueTooltipFieldsCache[$key] = $unserialized;
          } else {
             // TODO get default value (project_id = 0)

tools.php
@@ -30,7 +30,7 @@ class Tools {
     * array id => name
     */
    private static $customFieldNames;
-
+   private static $MantisDbVersion;
 
 
    /**
@@ -51,6 +51,25 @@ class Tools {
       return (strnatcmp(phpversion(),$checkVersion) >= 0);
    }
 
+   /**
+    * @return true if mantis v1.3.x (database_version >= 201)
+    */
+   public static function isMantisV1_3() {
+
+      if (is_null(self::$MantisDbVersion)) {
+         $query = "SELECT value FROM `mantis_config_table` WHERE `config_id` = 'database_version';";
+         $result = SqlWrapper::getInstance()->sql_query($query);
+         if (!$result) {
+            echo "<span style='color:red'>ERROR: Query FAILED</span>";
+            exit;
+         }
+         if (0 != SqlWrapper::getInstance()->sql_num_rows($result)) {
+            self::$MantisDbVersion = (int)SqlWrapper::getInstance()->sql_result($result, 0);
+         }
+      }
+      // 201 is v1.3.0-rc1
+      return (self::$MantisDbVersion >= 201);
+   }
 
 
    /**

commit fa8b4d774041f9ab6f474f634d5911b6105ba94d

[m1605] REGRESSION Tooltips not displayed since v1.1.0
broken since 33d6c8e57  (Mantis v1.3)

classes/project.class.php
@@ -1506,11 +1506,7 @@ class Project extends Model {
          if (0 != SqlWrapper::getInstance()->sql_num_rows($result)) {
             $serialized = SqlWrapper::getInstance()->sql_result($result, 0);
 
-            if (Tools::isMantisV1_3()) {
-               $unserialized = json_decode( $serialized, true );
-            } else {
-               $unserialized = unserialize($serialized);
-            }
+            $unserialized = unserialize($serialized);
             $this->issueTooltipFieldsCache[$key] = $unserialized;
          } else {
             // TODO get default value (project_id = 0)