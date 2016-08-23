46efbb1a16720251b7662ccc802be2ce485219a0
Louis BAYLE <lbayle.trash@gmail.com>
2016-08-23 22:25:43 +0200

    Gantt: initial

---
+++ gantt/gantt.php
@@ -0,0 +1,63 @@
+<?php
+require('../include/session.inc.php');
+
+/*
+   This file is part of CodevTT
+
+   CodevTT is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   CodevTT is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with CodevTT.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require('../path.inc.php');
+
+class GanttController extends Controller {
+
+   /**
+    * @var Logger The logger
+    */
+   private static $logger;
+   
+   
+   // internal
+   protected $execData;
+
+   /**
+    * Initialize complex static variables
+    * @static
+    */
+   public static function staticInit() {
+      self::$logger = Logger::getLogger(__CLASS__);
+   }
+
+   protected function display() {
+      if(Tools::isConnectedUser()) {
+         // only teamMembers & observers can access this page
+         if ((0 == $this->teamid) || ($this->session_user->isTeamCustomer($this->teamid))) {
+            $this->smartyHelper->assign('accessDenied', TRUE);
+         } else {
+            $team = TeamCache::getInstance()->getTeam($this->teamid);
+
+            $schedulerManager = new SchedulerManager($this->session_userid, $this->teamid);
+
+
+         }
+      }
+   }
+   
+   
+}
+
+// ========== MAIN ===========
+GanttController::staticInit();
+$controller = new GanttController('../', 'Gantt','Planning');
+$controller->execute();
