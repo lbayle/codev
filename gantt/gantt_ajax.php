6fe9b53a11944506f04efc3cfc18ece21444fc7d
Louis BAYLE <lbayle.trash@gmail.com>
2016-08-24 22:47:36 +0200

    Gantt: tooltips

--- gantt/gantt_ajax.php
+++ gantt/gantt_ajax.php
@@ -31,13 +31,17 @@ if(Tools::isConnectedUser() && filter_input(INPUT_POST, 'action')) {
    switch ($action) {
       case 'getGanttTasks':
          try {
-            $schedulerManager = new SchedulerManager($_SESSION['userid'], $_SESSION['teamid']);
+            $teamid =  $_SESSION['teamid'];
+            $session_userid =  $_SESSION['userid'];
+            $session_user = UserCache::getInstance()->getUser($session_userid);
+            $isManager = $session_user->isTeamManager($teamid);
+
+            $schedulerManager = new SchedulerManager($session_userid, $teamid);
             $data = $schedulerManager->execute();
             $taskDates = $schedulerManager->getComputedTaskDates();
             $isExtRef = $schedulerManager->getUserOption(SchedulerManager::OPTION_isDisplayExtRef);
 
-            // TODO get tasks dependencies
-            // TODO convert $taskDates to $dxhtmlGanttTasks
+            // convert $taskDates to $dxhtmlGanttTasks
             $tasksData = array();
             $bugid_to_idx = array();
             $idx = 1;
@@ -48,13 +52,14 @@ if(Tools::isConnectedUser() && filter_input(INPUT_POST, 'action')) {
                
                if ($isExtRef) {
                   $extRef = $issue->getTcId();
-                  if (NULL == $extRef) { $extRef = 'm'.$bugid; }
-                  $griddText =$extRef.' | '.$issue->getSummary();
+                  if (empty($extRef)) { $extRef = 'm'.$bugid; }
+                  $griddText =$extRef; // .' | '.$issue->getSummary();
                   $barText = $extRef;
                } else {
-                  $griddText = $bugid.' | '.$issue->getSummary();
+                  $griddText = $bugid; // .' | '.$issue->getSummary();
                   $barText = $bugid;
                }
+               $taskTooltip = getTaskTooltip_minimal($issue, $teamid, $session_userid, $isManager);
                $tasksData[] = array(
                    'id' => $idx,
                    'text' => $griddText,
@@ -69,7 +74,7 @@ if(Tools::isConnectedUser() && filter_input(INPUT_POST, 'action')) {
                    #'readonly' => true
                    // custom:
                    'barText' => $barText,
-                   'tooltipHtml' => 'TODO',        // TODO
+                   'tooltipHtml' => $taskTooltip,        // TODO
                    'assignedTo' => 'toto, titi',   // TODO
                );
                $bugid_to_idx[$bugid] = $idx;
@@ -78,6 +83,7 @@ if(Tools::isConnectedUser() && filter_input(INPUT_POST, 'action')) {
 
             //$ganttAjaxLogger->error($tasksData);
 
+            // TODO get tasks dependencies
             $tasksLinks = array();
 /*
         {id:1, source:1, target:2, type:"1"},
@@ -109,5 +115,87 @@ if(Tools::isConnectedUser() && filter_input(INPUT_POST, 'action')) {
    }
 } else {
    Tools::sendUnauthorizedAccess();
+}
+
+function getTaskTooltip_minimal($issue, $teamid, $session_userid, $isManager) {
+   $finalTooltipAttr = array();
+
+   $extRef = $issue->getTcId();
+   if (!empty($extRef)) {
+      $finalTooltipAttr[T_('Task')] = $extRef.' (m'.$issue->getId().')';
+   } else {
+      $finalTooltipAttr[T_('Task')] = $issue->getId();
+   }
+   $finalTooltipAttr[T_('Summary')] = $issue->getSummary();
+   
+   if ($issue->getDeadline() > 0) {
+      $finalTooltipAttr[T_('Deadline')] = date(T_("Y-m-d"), $issue->getDeadline());
+   }
+
+   $htmlTooltip =
+              '<table style="margin:0;border:0;padding:0;background-color:white;"><tbody>';
+   foreach ($finalTooltipAttr as $key => $value) {
+      $htmlTooltip .= '<tr>'.
+         '<td valign="top" style="color:blue;width:35px;">'.$key.'</td>'.
+         '<td>'.nl2br(htmlspecialchars($value)).'</td>'.
+         '</tr>';
+   }
+   $htmlTooltip .= '</tbody></table>';
+   return $htmlTooltip;
+}
+
+function getTaskTooltip($issue, $teamid, $session_userid, $isManager) {
+
+   $finalTooltipAttr = array();
+
+   $tooltipAttr = $issue->getTooltipItems($teamid, $session_userid, $isManager);
+   //unset($tooltipAttr[T_('Project')]);
+   //unset($tooltipAttr[T_('Type')]);
+   //unset($tooltipAttr[T_('Category')]);
+   unset($tooltipAttr[T_('Status')]);
+   unset($tooltipAttr[T_('Priority')]);
+   unset($tooltipAttr[T_('Severity')]);
+   unset($tooltipAttr[T_('External ID')]);
+   unset($tooltipAttr[T_('Backlog')]);
+   unset($tooltipAttr[T_('Deadline')]);
+
+   // insert in front
+   $extRef = $issue->getTcId();
+
+   $finalTooltipAttr[T_('Summary')] = $issue->getSummary();
+   $finalTooltipAttr += $tooltipAttr;
+
+   if ($issue->getDeadline() > 0) {
+      $finalTooltipAttr[T_('Deadline')] = date(T_("Y-m-d"), $issue->getDeadline());
+   }
+
+   // HTML
+   $htmlTooltip =
+              '<table style="margin:0;border:0;padding:0;background-color:white;">'.
+              '<tbody>';
+   $driftColor = NULL;
+   $driftMgrColor = NULL;
+   if (array_key_exists('DriftColor', $finalTooltipAttr)) {
+      $driftColor = $finalTooltipAttr['DriftColor'];
+      unset($finalTooltipAttr['DriftColor']);
+   }
+   if (array_key_exists('DriftMgrColor', $finalTooltipAttr)) {
+      $driftMgrColor = $finalTooltipAttr['DriftMgrColor'];
+      unset($finalTooltipAttr['DriftMgrColor']);
+   }
+   foreach ($finalTooltipAttr as $key => $value) {
+      $htmlTooltip .= '<tr>'.
+                  '<td valign="top" style="color:blue;width:35px;">'.$key.'</td>';
+      if ($driftColor != NULL && $key == T_('Drift')) {
+         $htmlTooltip .= '<td><span style="background-color:#'.$driftColor.'">&nbsp;&nbsp;'.$value.'&nbsp;&nbsp;</span></td>';
+      } else if (!is_null($driftMgrColor) && $key == T_('DriftMgr')) {
+         $htmlTooltip .= '<td><span style="background-color:#'.$driftMgrColor.'">&nbsp;&nbsp;'.$value.'&nbsp;&nbsp;</span></td>';
+      } else {
+         $htmlTooltip .= '<td>'.nl2br(htmlspecialchars($value)).'</td>';
+      }
+      $htmlTooltip .= '</tr>';
+   }
+   $htmlTooltip .= '</tbody></table>';
+   return $htmlTooltip;
 }
 
