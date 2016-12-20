lbayle/codev

commit 606def9b37413e0090f109710ae2f34a19bb8bd9

i18n FIX strftime UTF-8

i18n/i18n.inc.php
@@ -17,30 +17,30 @@ putenv('LC_MESSAGES='.$locale);
 
 if($locale === "fr") {
    // Try many values because OS doesn't have the same constants
-   $phpLocale = setlocale(LC_ALL,"fr_FR","fr","fra","French");
+   $phpLocale = setlocale(LC_ALL,"fr_FR.UTF-8","fr.UTF-8","fra.UTF-8","French");
 } elseif($locale === "en") {
    // Try many values because OS doesn't have the same constants
-   $phpLocale = setlocale(LC_ALL,"en_US","us","usa","en","eng","English");
+   $phpLocale = setlocale(LC_ALL,"en_US.UTF-8","us.UTF-8","usa.UTF-8","en.UTF-8","English");
 } elseif($locale === "pt_BR") {
-   $phpLocale = setlocale(LC_ALL,"pt_BR","pt","ptb","Portuguese");
+   $phpLocale = setlocale(LC_ALL,"pt_BR.UTF-8","pt.UTF-8","ptb.UTF-8","Portuguese");
 } elseif($locale === "de_DE") {
-   $phpLocale = setlocale(LC_ALL,"de_DE","de","German");
+   $phpLocale = setlocale(LC_ALL,"de_DE.UTF-8","de.UTF-8","German");
 } elseif($locale === "it_IT") {
-   $phpLocale = setlocale(LC_ALL,"it_IT","it","Italian");
+   $phpLocale = setlocale(LC_ALL,"it_IT.UTF-8","it.UTF-8","Italian");
 } elseif($locale === "es_ES") {
-   $phpLocale = setlocale(LC_ALL,"es_ES","es","Spanish");
+   $phpLocale = setlocale(LC_ALL,"es_ES.UTF-8","es.UTF-8","Spanish");
 } elseif($locale === "nl_NL") {
-   $phpLocale = setlocale(LC_ALL,"nl_NL","nl","Netherlands");
+   $phpLocale = setlocale(LC_ALL,"nl_NL.UTF-8","nl.UTF-8","Netherlands");
 } elseif($locale === "zh_CN") {
-   $phpLocale = setlocale(LC_ALL,"zh_CN","cn","Chinese");
+   $phpLocale = setlocale(LC_ALL,"zh_CN.UTF-8","cn.UTF-8","Chinese");
 } elseif($locale === "zh_TW") {
-   $phpLocale = setlocale(LC_ALL,"zh_TW","tw", "cht", "Taiwan", "chinese-traditional");
+   $phpLocale = setlocale(LC_ALL,"zh_TW.UTF-8","tw.UTF-8", "cht.UTF-8", "Taiwan", "chinese-traditional");
 } elseif($locale === "lt") {
-   $phpLocale = setlocale(LC_ALL,"lt_LT","lt","lithuanian");
+   $phpLocale = setlocale(LC_ALL,"lt_LT.UTF-8","lt.UTF-8","lithuanian");
 } elseif($locale === "ko") {
-   $phpLocale = setlocale(LC_ALL,"ko", "ko_KR","korean");
+   $phpLocale = setlocale(LC_ALL,"ko_KR.UTF-8", "ko.UTF-8","korean");
 } elseif($locale === "ar") {
-   $phpLocale = setlocale(LC_ALL,"ar", "ar_DZ","arab");
+   $phpLocale = setlocale(LC_ALL,"ar_DZ.UTF-8", "ar.UTF-8","arab");
 } else {
    // No locale set, it's because visitors modify the url, so forbidden reply
    header('HTTP/1.1 403 Forbidden');

smarty_tools.php
@@ -124,7 +124,7 @@ class SmartyTools {
          
          $weeks[] = array(
             'id' => $i,
-            'value' => utf8_encode(ucwords($monday)." - ".ucwords($friday)),
+            'value' => $monday." - ".$friday,
             'selected' => $i == $weekid
          );
       }

tools.php
@@ -464,13 +464,17 @@ class Tools {
 
    /**
     * Format the date in locale
+    * Note: to return in UTF8, the locale must be specified with ".utf8"
+    * example: setlocale(LC_ALL,"ar.utf8", "ar_DZ.utf8","arab");
+    *
     * @static
     * @param string $pattern The pattern to user
     * @param int $timestamp The timestamp to format
     * @return string The localized date
     */
    public static function formatDate($pattern, $timestamp) {
-      return utf8_encode(ucwords(strftime($pattern, $timestamp)));
+      // return utf8_encode(ucwords(strftime($pattern, $timestamp)));
+      return strftime($pattern, $timestamp);
    }
 
    /**

commit bc35caca6a30bc72ed316be0a2c264a16db5efc0

i18n: setLocale + fix regression introduced in 606def9b3741

classes/controller.class.php
@@ -82,7 +82,7 @@ abstract class Controller {
             $this->smartyHelper->assign('isAdmin', true);
          }
 
-         $locale = getLocale();
+         $locale = codevGetLocale();
          $this->smartyHelper->assign('locale', $locale);
       }
 
@@ -101,4 +101,3 @@ abstract class Controller {
 
 }
 
-?>

i18n/i18n.inc.php
@@ -6,77 +6,103 @@ require_once('gettext.inc');
 
 # REM: run "locale -a" to see locales installed on the system
 
-$locale = getLocale();
+// WARNING http://php.net/manual/en/function.setlocale.php
+//The locale information is maintained per process, not per thread.
+//If you are running PHP on a multithreaded server API like IIS, HHVM or Apache on Windows,
+//you may experience sudden changes in locale settings while a script is running,
+//though the script itself never called setlocale().
+//This happens due to other scripts running in different threads of the same process at the same time,
+//changing the process-wide locale using setlocale().
 
-// WORKAROUND : Windows hacks
-putenv('LANGUAGE='.$locale);
-putenv('LANG='.$locale);
-putenv('LC_ALL='.$locale);
-putenv('LC_MESSAGES='.$locale);
-// END WORKAROUND
+// display current value:
+//echo "<br>current Locale=".T_setlocale(LC_ALL, 0).'<br>';
 
-if($locale === "fr") {
-   // Try many values because OS doesn't have the same constants
-   $phpLocale = setlocale(LC_ALL,"fr_FR.UTF-8","fr.UTF-8","fra.UTF-8","French");
-} elseif($locale === "en") {
-   // Try many values because OS doesn't have the same constants
-   $phpLocale = setlocale(LC_ALL,"en_US.UTF-8","us.UTF-8","usa.UTF-8","en.UTF-8","English");
-} elseif($locale === "pt_BR") {
-   $phpLocale = setlocale(LC_ALL,"pt_BR.UTF-8","pt.UTF-8","ptb.UTF-8","Portuguese");
-} elseif($locale === "de_DE") {
-   $phpLocale = setlocale(LC_ALL,"de_DE.UTF-8","de.UTF-8","German");
-} elseif($locale === "it_IT") {
-   $phpLocale = setlocale(LC_ALL,"it_IT.UTF-8","it.UTF-8","Italian");
-} elseif($locale === "es_ES") {
-   $phpLocale = setlocale(LC_ALL,"es_ES.UTF-8","es.UTF-8","Spanish");
-} elseif($locale === "nl_NL") {
-   $phpLocale = setlocale(LC_ALL,"nl_NL.UTF-8","nl.UTF-8","Netherlands");
-} elseif($locale === "zh_CN") {
-   $phpLocale = setlocale(LC_ALL,"zh_CN.UTF-8","cn.UTF-8","Chinese");
-} elseif($locale === "zh_TW") {
-   $phpLocale = setlocale(LC_ALL,"zh_TW.UTF-8","tw.UTF-8", "cht.UTF-8", "Taiwan", "chinese-traditional");
-} elseif($locale === "lt") {
-   $phpLocale = setlocale(LC_ALL,"lt_LT.UTF-8","lt.UTF-8","lithuanian");
-} elseif($locale === "ko") {
-   $phpLocale = setlocale(LC_ALL,"ko_KR.UTF-8", "ko.UTF-8","korean");
-} elseif($locale === "ar") {
-   $phpLocale = setlocale(LC_ALL,"ar_DZ.UTF-8", "ar.UTF-8","arab");
-} else {
-   // No locale set, it's because visitors modify the url, so forbidden reply
-   header('HTTP/1.1 403 Forbidden');
-   exit;
-}
-#file_put_contents("/data/codevtt_tmp_log.txt", "phpLocale=<$phpLocale>\n", FILE_APPEND);
-T_setlocale(LC_ALL, $phpLocale);
+$codevLocale = codevGetLocale();
+//echo "locale = ".$locale."<br>";
+
+codevSetLocale($codevLocale);
 
 try {
-   if (($_SESSION['locale'] != $locale) && Tools::isConnectedUser() && isset($_SESSION['teamid'])) {
+   if (($_SESSION['locale'] != $codevLocale) && Tools::isConnectedUser() && isset($_SESSION['teamid'])) {
       $user =  UserCache::getInstance()->getUser($_SESSION['userid']);
-      $user->setDefaultLanguage($locale);
+      $user->setDefaultLanguage($codevLocale);
    }
 } catch (Exception $e) {
    #$logger->error('could not setDefaultLanguage ('.$locale.') for user '.$_SESSION['userid']);
 }
 
-$_SESSION['locale'] = $locale;
+$_SESSION['locale'] = $codevLocale;
 
-// we want 3.5 always to be displayed '3.5' and not '3,5'
-$phpLocale = setlocale(LC_NUMERIC,"en_US","us","usa","en","eng","English");
-T_setlocale(LC_NUMERIC,$phpLocale);
 
-$locales_dir = (TRUE === file_exists('./i18n/locale')) ? './i18n/locale' : $locales_dir = '../i18n/locale';
+// ===========================================================================
+/**
+ * convert CodevTT localse string to system UTF-8 string
+ * and set locale usion gettext
+ * 
+ * @param type $codevLocale
+ * @return type
+ */
+function codevSetLocale($codevLocale) {
+   if($codevLocale === "fr") {
+      // Try many values because OS doesn't have the same constants
+      // setlocale() always return FALSE on windows, use gettext T_setlocale
+      $phpLocale = T_setlocale(LC_ALL, "fr_FR.UTF-8","fr_FR.utf8","fr.UTF-8","fra.UTF-8","French.UTF-8");
+   } elseif($codevLocale === "en") {
+      $phpLocale = T_setlocale(LC_ALL,"en_US.UTF-8","us.UTF-8","usa.UTF-8","en.UTF-8","English");
+   } elseif($codevLocale === "pt_BR") {
+      $phpLocale = T_setlocale(LC_ALL,"pt_BR.UTF-8","pt.UTF-8","ptb.UTF-8","Portuguese");
+   } elseif($codevLocale === "de_DE") {
+      $phpLocale = T_setlocale(LC_ALL,"de_DE.UTF-8","de.UTF-8","German");
+   } elseif($codevLocale === "it_IT") {
+      $phpLocale = T_setlocale(LC_ALL,"it_IT.UTF-8","it.UTF-8","Italian");
+   } elseif($codevLocale === "es_ES") {
+      $phpLocale = T_setlocale(LC_ALL,"es_ES.UTF-8","es.UTF-8","Spanish");
+   } elseif($codevLocale === "nl_NL") {
+      $phpLocale = T_setlocale(LC_ALL,"nl_NL.UTF-8","nl.UTF-8","Netherlands");
+   } elseif($codevLocale === "zh_CN") {
+      $phpLocale = T_setlocale(LC_ALL, "zh_CN.UTF-8","zh_CN.utf8","cn.UTF-8","Chinese.UTF-8");
+   } elseif($codevLocale === "zh_TW") {
+      $phpLocale = T_setlocale(LC_ALL,"zh_TW.UTF-8","tw.UTF-8", "cht.UTF-8", "Taiwan.UTF-8", "chinese-traditional.UTF-8");
+   } elseif($codevLocale === "lt") {
+      $phpLocale = T_setlocale(LC_ALL,"lt_LT.UTF-8","lt.UTF-8","lithuanian");
+   } elseif($codevLocale === "ko") {
+      $phpLocale = T_setlocale(LC_ALL,"ko_KR.UTF-8", "ko.UTF-8","korean");
+   } elseif($codevLocale === "ar") {
+      $phpLocale = T_setlocale(LC_ALL,"ar_DZ.UTF-8", "ar.UTF-8","arab");
+   } else {
+      // set to system default:
+      T_setlocale(LC_ALL, NULL);
+   }
+   //echo "phpLocale = ".var_export($phpLocale, true)."<br>";
+   #file_put_contents("/data/codevtt_tmp_log.txt", "phpLocale=<$phpLocale>\n", FILE_APPEND);
 
-$textdomain = "codev";
+   // WORKAROUND : Windows hacks
+   putenv('LANGUAGE='.$phpLocale);
+   putenv('LANG='.$phpLocale);
+   //putenv('LC_ALL='.$phpLocale);
+   //putenv('LC_MESSAGES='.$phpLocale);
+   // END WORKAROUND
 
-T_bindtextdomain($textdomain,$locales_dir);
-T_bind_textdomain_codeset($textdomain, 'UTF-8');
-T_textdomain($textdomain);
+   // we want 3.5 always to be displayed '3.5' and not '3,5'
+   //T_setlocale(LC_NUMERIC,"en_US.utf8","us.utf8","usa.utf8","en.utf8","eng.utf8","English");
 
-$availableEncoding = mb_detect_order();
-if(!array_search("ISO-8859-1", $availableEncoding)) {
-   $newEnconding = array();
-   $newEnconding[] = "ISO-8859-1";
-   mb_detect_order(array_merge($availableEncoding,$newEnconding));
+   $locales_dir = (TRUE === file_exists('./i18n/locale')) ? './i18n/locale' : $locales_dir = '../i18n/locale';
+
+   $textdomain = "codev";
+   T_bindtextdomain($textdomain,$locales_dir);
+   T_bind_textdomain_codeset($textdomain, 'UTF-8');
+   T_textdomain($textdomain);
+
+   // I don't remember why...
+   $availableEncoding = mb_detect_order();
+   if(!array_search("ISO-8859-1", $availableEncoding)) {
+      $newEnconding = array();
+      $newEnconding[] = "ISO-8859-1"; // Latin-1
+      mb_detect_order(array_merge($availableEncoding,$newEnconding)); // array ( 0 => 'ASCII', 1 => 'UTF-8', 2 => 'ISO-8859-1', )
+   }
+
+   //echo "<br>current Locale=".T_setlocale(LC_ALL, 0).'<br>';
+   return T_setlocale(LC_ALL, 0);
 }
 
 /**
@@ -86,7 +112,7 @@ if(!array_search("ISO-8859-1", $availableEncoding)) {
  * -> The prefered existing locale of the browser
  * @return string The locale
  */
-function getLocale() {
+function codevGetLocale() {
 
    $locale = 'en'; // default language
 

smarty_tools.php
@@ -121,7 +121,13 @@ class SmartyTools {
          $mondayTimestamp = strtotime($date_string);
          $monday = T_('W').$i.' | '.strftime('%d %b', strtotime("Monday",$mondayTimestamp));
          $friday = strftime("%d %b", strtotime("Friday",$mondayTimestamp));
-         
+
+/*      if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+         $monday = utf8_encode($monday);
+         $friday = utf8_encode($friday);
+      }
+*/
+
          $weeks[] = array(
             'id' => $i,
             'value' => $monday." - ".$friday,

tools.php
@@ -473,8 +473,11 @@ class Tools {
     * @return string The localized date
     */
    public static function formatDate($pattern, $timestamp) {
-      // return utf8_encode(ucwords(strftime($pattern, $timestamp)));
-      return strftime($pattern, $timestamp);
+      //if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+      //   return utf8_encode(ucwords(strftime($pattern, $timestamp)));
+      //} else {
+         return strftime($pattern, $timestamp);
+      //}
    }
 
    /**