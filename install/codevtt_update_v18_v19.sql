375de9ceb87c5958f02a962a3459d415c543a2e2
Louis BAYLE <lbayle.trash@gmail.com>
2019-04-07 11:21:52 +0200

    version info

--- install/codevtt_update_v18_v19.sql
+++ install/codevtt_update_v18_v19.sql
@@ -8,6 +8,8 @@ SET SQL_MODE='ANSI';
 CREATE INDEX key1 ON codev_blog_table ("dest_team_id", "dest_user_id", "date_submitted");
 CREATE INDEX key1 ON codev_blog_activity_table ("blog_id", "user_id", "action");
 
+INSERT INTO "codev_blog_table" ("id", "date_submitted", "src_user_id", "dest_user_id", "dest_project_id", "dest_team_id", "severity", "category", "summary", "content", "date_expire", "color") VALUES
+(1, 1526228606, 0, 0, 0, 0, 2, '3', 'Exchange messages with your team !', 'Hi,\nthis plugins allows to share notifications within the teams.\n\nExemples:\n\"The time spent on the March 3rd meeting must be counted on task 34001\"\n\"The Mantis/CodevTT server will be unavailable for maintenance on May 13th\"\n\nUse the <img src=\"images/b_add.png\"/> button on your right to add messages.\nTo hide this message, click <img src="images/b_markAsRead.png"/> then <img src="images/b_ghost.png"/>\n\nBest regards', 0, '0');
 
 -- leader_id is replaced by a list of administrators '123,543,98,43'
 ALTER TABLE "codev_team_table" ADD "administrators" varchar(255) DEFAULT NULL AFTER "description";
