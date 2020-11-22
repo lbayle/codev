744baac520ed4f948c2d80e8402c5a91a588e991
Louis BAYLE <lbayle.trash@gmail.com>
2020-11-22 15:08:46 +0100

    backup 7z

--- tools/backup_cronjob.sh
+++ tools/backup_cronjob.sh
@@ -18,7 +18,10 @@ DB=bugtracker
 DIR_BACKUP=/tmp/codevtt/backups
 
 DATE=$(date +%Y%m%d)
+
+# delete the file from 3 days back, so we have always 3 days of backups (3600*24*3 = 259200)
 DATE_3_DAYS_AGO=$(date --date @$(( $(date +%s) - 259200 )) +%Y%m%d)
+DATE_5_DAYS_AGO=$(date --date @$(( $(date +%s) - 432000 )) +%Y%m%d)
 
 # delete the file from 3 days back, so we have always 3 days of backups
 file_to_delete=${DIR_BACKUP}/${DB}_${DATE_3_DAYS_AGO}.sql
@@ -30,10 +33,18 @@ if [ -f ${file_to_delete}.gz ];
 then
    rm ${file_to_delete}.gz
 fi
+if [ -f ${file_to_delete}.7z ];
+then
+   rm ${file_to_delete}.7z
+fi
 
-# backup
+# create backup file
 mkdir -p $DIR_BACKUP
+date
 mysqldump --opt -u${USER} -p${PASSWD} ${DB} > ${DIR_BACKUP}/${DB}_${DATE}.sql
-gzip ${DIR_BACKUP}/${DB}_${DATE}.sql
+date
+gzip -f ${DIR_BACKUP}/${DB}_${DATE}.sql
+#7za a -sdel ${DIR_BACKUP}/${DB}_${DATE}.sql.7z ${DIR_BACKUP}/${DB}_${DATE}.sql
+date
 
 # end.
